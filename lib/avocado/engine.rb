f39526e4afddd199a726db68db8279a3d9fec247
Adrian Marin <adrian@adrianthedev.com>
2020-05-09 18:28:06 +0300

    refactor: precompile assets

--- lib/avocado/engine.rb
+++ lib/avocado/engine.rb
@@ -25,16 +25,21 @@ end
 
     initializer "webpacker.proxy" do |app|
       insert_middleware = begin
-                          Avocado.webpacker.config.dev_server.present?
-                        rescue
-                          nil
-                        end
+                            Avocado.webpacker.config.dev_server.present?
+                          rescue
+                            nil
+                          end
       next unless insert_middleware
 
       app.middleware.insert_before(
         0, Webpacker::DevServerProxy,
         ssl_verify_none: true,
         webpacker: Avocado.webpacker
+      )
+
+      config.app_middleware.use(
+        Rack::Static,
+        urls: ["/avocado-packs"], root: "avocado/public"
       )
     end
   end
