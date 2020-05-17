775d580000680a54f06713682f3d99ac3006e0b1
Adrian Marin <adrian@adrianthedev.com>
2020-05-17 21:32:57 +0300

    refactor: minor tweaks

--- lib/avocado/app/fields/number_field.rb
+++ lib/avocado/app/fields/number_field.rb
@@ -9,24 +9,22 @@ module Avocado
         @component = 'number-field'
         @sortable = true
 
-
         @min = args[:min].present? ? args[:min].to_i : nil
         @max = args[:max].present? ? args[:max].to_i : nil
         @step = args[:step].present? ? args[:step].to_i : nil
       end
-    end
 
-    def fetch_for_resource(model, view = :index)
-      fields = super(model, view)
+      def fetch_for_resource(model, view = :index)
+        fields = super(model, view)
 
-      abort self.inspect if id == 'age'
-      fields[:value] = @block.call model, self if @block.present?
+        fields[:value] = @block.call model, self if @block.present?
 
-      fields[:min] = @min
-      fields[:max] = @max
-      fields[:step] = @step
+        fields[:min] = @min
+        fields[:max] = @max
+        fields[:step] = @step
 
-      fields
+        fields
+      end
     end
   end
 end
--- spec/dummy/app/services/avocado/resources/user.rb
+++ spec/dummy/app/services/avocado/resources/user.rb
@@ -10,7 +10,7 @@ module Avocado
         id
         text :Name, required: true, only_on: [:forms, :index]
         text :email, name: 'User Email', required: true, hide_on: :edit
-        number :age, min: 1, max: 120, step: 2
+        number :age, min: 0, max: 120, step: 1
         password :password, name: 'User Password', required: true, except_on: :forms
         password :password_confirmation, name: 'Password confirmation', required: true
         text 'Is Writer' do |model, resource|
