8d60ddc98099a3bc2a5c5074639fc796690237b4
Adrian Marin <adrian@adrianthedev.com>
2020-06-07 13:53:18 +0300

    chore: wip

--- scripts/build-2.sh
+++ scripts/build-2.sh
@@ -6,6 +6,7 @@ BUMP=$1
 
 gem bump $BUMP
 VERSION=$(bundle exec rails runner 'puts Avocado::VERSION')
-git add Gemfile.lock
-git tag v$VERSION
+gem tag
+# git add Gemfile.lock
+# git tag v$VERSION
 git push --tags
