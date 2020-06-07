0e39a30bef0709baa273e89ccd613aa6e1137840
Adrian Marin <adrian@adrianthedev.com>
2020-06-07 13:57:35 +0300

    ci: try new build

--- scripts/build-2.sh
+++ scripts/build-2.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+NAME=Avocado
 BUMP=$1
 
 gem bump $BUMP
+gem bump --no-commit
 VERSION=$(bundle exec rails runner 'puts Avocado::VERSION')
+bundle
+git commit -m "Bump $NAME to $VERSION"
 gem tag
 # git add Gemfile.lock
 # git tag v$VERSION
