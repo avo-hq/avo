8e82f5eb02d3ce228adb1be4e53ab78003325eb4
Adrian Marin <adrian@adrianthedev.com>
2020-06-07 13:59:49 +0300

    chore: git add files before

--- scripts/build-2.sh
+++ scripts/build-2.sh
@@ -9,6 +9,7 @@ gem bump $BUMP
 gem bump --no-commit
 VERSION=$(bundle exec rails runner 'puts Avocado::VERSION')
 bundle install --quiet
+git add .
 git commit -m "Bump $NAME to $VERSION"
 gem tag
 # git add Gemfile.lock
