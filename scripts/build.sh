5df52d863b167d00b0d8c4cb4d7af5b31a523597
Adrian Marin <adrian@adrianthedev.com>
2020-06-07 15:13:43 +0300

    chore: tweak again

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,9 @@ gem bump $BUMP --no-commit
 VERSION=$(bundle exec rails runner 'puts Avocado::VERSION')
 bundle install --quiet
 git add .
-# git commit -m "Bump $NAME to $VERSION"
-# gem tag
-git tag -am "tag v$VERSION" v$VERSION
+git commit -m "Bump $NAME to $VERSION"
+gem tag
+# git tag -am "tag v$VERSION" v$VERSION
 # git add Gemfile.lock
 # git tag v$VERSION
 git push --tags
