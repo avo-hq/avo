6b7fc175f71d78d22c32219f613386d4f3390a1a
Adrian Marin <adrian@adrianthedev.com>
2020-06-10 09:57:59 +0300

    style: minor tweaks (#51)

--- lib/avocado.rb
+++ lib/avocado.rb
@@ -1,5 +1,5 @@
-require "avocado/engine"
-require "zeitwerk"
+require 'avocado/engine'
+require 'zeitwerk'
 require_relative 'avocado/configuration'
 
 loader = Zeitwerk::Loader.for_gem
@@ -7,13 +7,13 @@ loader.push_dir(Pathname.new(File.join(__dir__, 'avocado')))
 loader.setup
 
 module Avocado
-  ROOT_PATH = Pathname.new(File.join(__dir__, ".."))
+  ROOT_PATH = Pathname.new(File.join(__dir__, '..'))
 
   class << self
     def webpacker
       @webpacker ||= ::Webpacker::Instance.new(
         root_path: ROOT_PATH,
-        config_path: ROOT_PATH.join("config/webpacker.yml")
+        config_path: ROOT_PATH.join('config/webpacker.yml')
       )
     end
 
