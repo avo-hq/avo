21b6666af2d410077f08f11730e0b40f77a094f6
Adrian Marin <adrian@adrianthedev.com>
2020-06-26 23:11:57 +0300

    refactor: bring back the field

--- app/frontend/js/components/Show/HeadingField.vue
+++ app/frontend/js/components/Show/HeadingField.vue
@@ -2,8 +2,8 @@
   <div class="flex items-start py-2 leading-tight border-t bg-gray-200">
     <div class="p-4 h-full w-full">
       <template v-if="field.name">
-          <p v-if="!this.field.as_html" v-text="field.name"></p>
-          <div v-else v-html="field.name"></div>
+        <p v-if="!this.field.as_html" v-text="field.name"></p>
+        <div v-else v-html="field.name"></div>
       </template>
       <p v-else>-</p>
     </div>
