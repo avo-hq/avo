679b9163b75b0ebf86b3d708f133ce6a3c407d38
Adrian Marin <adrian@adrianthedev.com>
2020-07-03 12:34:09 +0300

    test: fail tests on console error

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,4 +1,19 @@
 RSpec.configure do |config|
+  config.after(:each, type: :system) do
+    errors = page.driver.browser.manage.logs.get(:browser)
+    if errors.present?
+      aggregate_failures 'javascript errors' do
+        errors.each do |error|
+          expect(error.level).not_to eq('SEVERE'), error.message
+          next unless error.level == 'WARNING'
+          STDERR.puts 'WARN: javascript warning'
+          STDERR.puts error.message
+        end
+      end
+    end
+  end
+
+
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
