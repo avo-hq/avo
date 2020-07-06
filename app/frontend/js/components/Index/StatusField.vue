101931b757b119863394707c1b737875a1e1a0ee
mihaimdm <mihai.mdm2007@gmail.com>
2020-07-06 12:10:25 +0300

    fix: export status mixin

--- app/frontend/js/components/Index/StatusField.vue
+++ app/frontend/js/components/Index/StatusField.vue
@@ -1,36 +1,21 @@
 <template>
   <index-field-wrapper :field="field">
     <StatusComponent
-      v-if="value"
+      v-if="field.value"
       :status="status"
-      :label="value"
+      :label="field.value"
     />
     <empty-dash v-else />
   </index-field-wrapper>
 </template>
 
 <script>
+import IsStatusField from '@/js/mixins/is-status-field'
 import StatusComponent from '@/js/components/StatusComponent.vue'
 
 export default {
   props: ['field'],
   components: { StatusComponent },
-  computed: {
-    status() {
-      if (this.isFailing) return 'fail'
-      if (this.isLoading) return 'load'
-
-      return 'success'
-    },
-    isFailing() {
-      return this.field.failed_when.indexOf(this.field.value) > -1
-    },
-    isLoading() {
-      return this.field.loading_when.indexOf(this.field.value) > -1
-    },
-    value() {
-      return this.field.value
-    },
-  },
+  mixins: [IsStatusField],
 }
 </script>
--- app/frontend/js/components/Show/StatusField.vue
+++ app/frontend/js/components/Show/StatusField.vue
@@ -10,24 +10,12 @@
 </template>
 
 <script>
+import IsStatusField from '@/js/mixins/is-status-field'
 import StatusComponent from '@/js/components/StatusComponent.vue'
 
 export default {
   props: ['field', 'index'],
   components: { StatusComponent },
-  computed: {
-    status() {
-      if (this.isFailing) return 'fail'
-      if (this.isLoading) return 'load'
-
-      return 'success'
-    },
-    isFailing() {
-      return this.field.failed_when.indexOf(this.field.value) > -1
-    },
-    isLoading() {
-      return this.field.loading_when.indexOf(this.field.value) > -1
-    },
-  },
+  mixins: [IsStatusField],
 }
 </script>
