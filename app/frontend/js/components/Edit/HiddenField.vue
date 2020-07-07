ad96aae892406d1a45ce7a5103d044de0e8209c5
Mihai David Marin <mihai.mdm2007@gmail.com>
2020-07-07 14:55:43 +0300

    feature: Hidden field (#71)
    Co-authored-by: Adrian Marin <adrian@adrianthedev.com>

---
+++ app/frontend/js/components/Edit/HiddenField.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :field="field" :errors="errors" :index="index">
+    <input type="hidden" :value="value" />
+  </div>
+</template>
+
+<script>
+import FormField from '@/js/mixins/form-field'
+
+export default {
+  mixins: [FormField],
+}
+</script>
---
+++ lib/avocado/app/fields/hidden_field.rb
@@ -0,0 +1,17 @@
+require_relative 'text_field'
+
+module Avocado
+  module Fields
+    class HiddenField < TextField
+      def initialize(name, **args, &block)
+        @defaults = {
+          component: 'hidden-field',
+        }
+
+        super(name, **args, &block)
+
+        only_on :edit
+      end
+    end
+  end
+end
