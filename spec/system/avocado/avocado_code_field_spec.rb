4b85d2e4c4d33e1354b8aeea6c246e52898e13f1
mihaimdm <mihai.mdm2007@gmail.com>
2020-07-08 13:50:48 +0300

    test: still wip

--- spec/system/avocado/avocado_code_field_spec.rb
+++ spec/system/avocado/avocado_code_field_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'CodeField', type: :system do
   describe 'without value' do
-    let!(:project) { create :project , code_snippet: ''}
+    let!(:user) { create :user , custom_css: ''}
 
     context 'show' do
       it 'displays the projects empty code_snippet (dash)' do
-        visit "/avocado/resources/projects/#{project.id}"
+        visit "/avocado/resources/users/#{user.id}"
 
-        expect(find_field_value_element('code_snippet')).to have_text empty_dash
+        expect(find_field_value_element('custom_css')).to have_text empty_dash
       end
     end
 
     context 'edit' do
       it 'has the projects code_snippet label and empty code editor' do
-        visit "/avocado/resources/projects/#{project.id}/edit"
+        visit "/avocado/resources/users/#{user.id}/edit"
 
-        code_snippet_element = find_field_element('code_snippet')
+        custom_css_element = find_field_element('custom_css')
 
-        expect(code_snippet_element).to have_text 'Code snippet'
+        expect(custom_css_element).to have_text 'Custom css'
 
-        expect(code_snippet_element).to have_css '.vue-codemirror'
+        expect(custom_css_element).to have_css '.vue-codemirror'
       end
     end
   end
