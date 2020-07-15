a5516dc2a2b5bd3454ec9108d416887fbff805b8
mihaimdm <mihai.mdm2007@gmail.com>
2020-07-15 12:00:56 +0300

    feat: refactor a bit

--- spec/system/avocado/nullable_field_spec.rb
+++ spec/system/avocado/nullable_field_spec.rb
@@ -2,81 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'NullableField', type: :system do
   describe 'without input (specifying null_values: ["", "0", "null", "nil"])' do
-    let!(:team) { create :team, description: nil }
-
-    context 'show' do
-      it 'displays the teams empty description (dash)' do
-        visit "/avocado/resources/team/#{team.id}"
-
-        expect(find_field_value_element('description')).to have_text empty_dash
-      end
-    end
-
-    context 'edit' do
-      it 'has the teams description empty' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        expect(find_field('description').value).to eq ''
-      end
-    end
-  end
-
-  describe 'with regular input (specifying null_values: ["", "0", "null", "nil"])' do
-    let!(:team) { create :team, description: 'descr' }
-
-    context 'edit' do
-      it 'has the teams description prefilled' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        expect(find_field('description').value).to eq 'descr'
-      end
-      it 'changes the teams description to null ("" - empty string)' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        fill_in 'description', with: ''
-        click_on 'Save'
-        wait_for_loaded
-
-        expect(current_path).to eql "/avocado/resources/teams/#{team.id}"
-        expect(find_field_value_element('description')).to have_text empty_dash
-      end
-
-      it 'changes the teams description to null ("0")' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        fill_in 'description', with: '0'
-        click_on 'Save'
-        wait_for_loaded
-
-        expect(current_path).to eql "/avocado/resources/teams/#{team.id}"
-        expect(find_field_value_element('description')).to have_text empty_dash
-      end
-
-      it 'changes the teams description to null ("nil")' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        fill_in 'description', with: 'nil'
-        click_on 'Save'
-        wait_for_loaded
-
-        expect(current_path).to eql "/avocado/resources/teams/#{team.id}"
-        expect(find_field_value_element('description')).to have_text empty_dash
-      end
-
-      it 'changes the users description to null ("null")' do
-        visit "/avocado/resources/teams/#{team.id}/edit"
-
-        fill_in 'description', with: 'null'
-        click_on 'Save'
-        wait_for_loaded
-
-        expect(current_path).to eql "/avocado/resources/teams/#{team.id}"
-        expect(find_field_value_element('description')).to have_text empty_dash
-      end
-    end
-  end
-
-  describe 'without input (without specifying null_values)' do
     let!(:post) { create :post, body: nil }
 
     context 'show' do
@@ -96,14 +21,14 @@ RSpec.describe 'NullableField', type: :system do
     end
   end
 
-  describe 'with regular input (without specifying null_values)' do
-    let!(:post) { create :post, body: 'Story begins here!' }
+  describe 'with regular input (specifying null_values: ["", "0", "null", "nil"])' do
+    let!(:post) { create :post, body: 'descr' }
 
     context 'edit' do
       it 'has the posts body prefilled' do
         visit "/avocado/resources/posts/#{post.id}/edit"
 
-        expect(find_field('body').value).to eq 'Story begins here!'
+        expect(find_field('body').value).to eq 'descr'
       end
       it 'changes the posts body to null ("" - empty string)' do
         visit "/avocado/resources/posts/#{post.id}/edit"
@@ -114,6 +39,81 @@ RSpec.describe 'NullableField', type: :system do
 
         expect(current_path).to eql "/avocado/resources/posts/#{post.id}"
         expect(find_field_value_element('body')).to have_text empty_dash
+      end
+
+      it 'changes the posts body to null ("0")' do
+        visit "/avocado/resources/posts/#{post.id}/edit"
+
+        fill_in 'body', with: '0'
+        click_on 'Save'
+        wait_for_loaded
+
+        expect(current_path).to eql "/avocado/resources/posts/#{post.id}"
+        expect(find_field_value_element('body')).to have_text empty_dash
+      end
+
+      it 'changes the posts body to null ("nil")' do
+        visit "/avocado/resources/posts/#{post.id}/edit"
+
+        fill_in 'body', with: 'nil'
+        click_on 'Save'
+        wait_for_loaded
+
+        expect(current_path).to eql "/avocado/resources/posts/#{post.id}"
+        expect(find_field_value_element('body')).to have_text empty_dash
+      end
+
+      it 'changes the postss body to null ("null")' do
+        visit "/avocado/resources/posts/#{post.id}/edit"
+
+        fill_in 'body', with: 'null'
+        click_on 'Save'
+        wait_for_loaded
+
+        expect(current_path).to eql "/avocado/resources/posts/#{post.id}"
+        expect(find_field_value_element('body')).to have_text empty_dash
+      end
+    end
+  end
+
+  describe 'without input (without specifying null_values)' do
+    let!(:project) { create :project, status: nil }
+
+    context 'show' do
+      it 'displays the projects empty status (dash)' do
+        visit "/avocado/resources/project/#{project.id}"
+
+        expect(find_field_value_element('status')).to have_text empty_dash
+      end
+    end
+
+    context 'edit' do
+      it 'has the projects status empty' do
+        visit "/avocado/resources/projects/#{project.id}/edit"
+
+        expect(find_field('status').value).to eq ''
+      end
+    end
+  end
+
+  describe 'with regular input (without specifying null_values)' do
+    let!(:project) { create :project, status: 'rejected' }
+
+    context 'edit' do
+      it 'has the projects status prefilled' do
+        visit "/avocado/resources/projects/#{project.id}/edit"
+
+        expect(find_field('status').value).to eq 'rejected'
+      end
+      it 'changes the projects status to null ("" - empty string)' do
+        visit "/avocado/resources/projects/#{project.id}/edit"
+
+        fill_in 'status', with: ''
+        click_on 'Save'
+        wait_for_loaded
+
+        expect(current_path).to eql "/avocado/resources/projects/#{project.id}"
+        expect(find_field_value_element('status')).to have_text empty_dash
       end
     end
   end
