286150b2f85f9b35707a9239412290669b0420dd
Adrian Marin <adrian@adrianthedev.com>
2020-07-19 12:13:33 +0300

    refactor: minor cleanup

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,14 +6,14 @@ import Avo from '@/js/Avo'
 document.addEventListener('DOMContentLoaded', Avo.init)
 
 Mousetrap.bind('r r r', () => Avo.reload())
-// import '@/js/bootstrap'
-// import '@/js/vue'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 const images = require.context('../images', true)
+// eslint-disable-next-line no-unused-vars
 const imagePath = (name) => images(name, true)
 const svgs = require.context('../svgs', true)
+// eslint-disable-next-line no-unused-vars
 const svgPath = (name) => svgs(name, true)
