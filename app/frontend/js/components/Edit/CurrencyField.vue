463d3e5b03ad17f83bb82accd7d6fd31cce26ba6
mihaimdm <mihai.mdm2007@gmail.com>
2020-07-22 20:05:04 +0300

    refactor: modify ifelse also in edit

--- app/frontend/js/components/Edit/CurrencyField.vue
+++ app/frontend/js/components/Edit/CurrencyField.vue
@@ -30,8 +30,11 @@ export default {
   },
   methods: {
     setInitialValue() {
-      if (this.field.value) this.value = Number(this.field.value, 10)
-      else this.value = null
+      if (this.field.value) {
+        this.value = Number(this.field.value, 10)
+      } else {
+        this.value = null
+      }
     },
     focus() {
       if (this.$refs['field-input']) this.$refs['field-input'].$el.focus()
