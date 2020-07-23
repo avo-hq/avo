45ed6e2c70f5363e736edcfae2c7ec50763f7b38
Adrian Marin <adrian@adrianthedev.com>
2020-07-23 17:52:37 +0300

    chore: reinstall avocado-js

--- app/frontend/js/components/Edit/BooleanField.vue
+++ app/frontend/js/components/Edit/BooleanField.vue
@@ -1,14 +1,14 @@
 <template>
-<edit-field-wrapper :field="field" :errors="errors" :index="index">
-  <div class="h-8 flex items-center">
-    <input type="checkbox"
-      ref="field-input"
-      :id="field.id"
-      :disabled="disabled"
-      class="w-4 h-4"
-      v-model="value"
-    >
-  </div>
+  <edit-field-wrapper :field="field" :errors="errors" :index="index">
+    <div class="h-8 flex items-center">
+      <input type="checkbox"
+        ref="field-input"
+        :id="field.id"
+        :disabled="disabled"
+        class="w-4 h-4"
+        v-model="value"
+      >
+    </div>
   </edit-field-wrapper>
 </template>
 
