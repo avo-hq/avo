1379e441a4bdaaa2436e5619885e93889cc57425
Adrian Marin <adrian@adrianthedev.com>
2020-08-11 12:14:56 +0300

    chore: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-IMAGE_NAME=avo-dev
-WORKSPACE_DIR=$1	BUMP=${1:-'patch'}
-TAG=$2
-OS=$(node -r os -e 'console.log(os.platform())')
-
-cd $WORKSPACE_DIR
-
-docker build -t $IMAGE_NAME -f docker/Dockerfile .
-
-CID=$(docker create $IMAGE_NAME)
-
-docker cp ${CID}:/avo/pkg/. $WORKSPACE_DIR/pkg
-docker rm ${CID}
+rm -rf public/avo-packs
+bin/webpack
+gem build avo --output pkg/avo.gem
