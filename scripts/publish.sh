9896ef7022277ba9d622b726fbd7258ee002fa15
Adrian Marin <adrian@adrianthedev.com>
2020-09-25 10:56:39 +0300

    chore: update release script to publish to registry (#131)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,4 +5,4 @@ set -e
 VERSION=$(bundle exec rails runner 'puts Avo::VERSION')
 
 gem build avo.gemspec
-gem push --key github --host https://rubygems.pkg.github.com/avo-hq ./avo-$VERSION.gem
+gem push --key github --host https://rubygems.pkg.github.com/avo-hq ./pkg/avo-$VERSION.gem
