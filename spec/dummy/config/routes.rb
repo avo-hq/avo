6b7e494647bb2eadcb6fc585eca65902403796d2
Adrian Marin <adrian@adrianthedev.com>
2020-10-06 17:34:07 +0300

    fix: select field doesn't show the right values in some cases (#138)

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root to: redirect('/avo')
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
