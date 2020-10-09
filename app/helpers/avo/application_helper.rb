4b258899ff1e8b5d0b652e877e06656abc2fd097
Adrian Marin <adrian@adrianthedev.com>
2020-10-09 20:31:58 +0300

    feature: allow logo customization

--- app/helpers/avo/application_helper.rb
+++ app/helpers/avo/application_helper.rb
@@ -5,5 +5,9 @@ module Avo
     def current_webpacker_instance
       Avo.webpacker
     end
+
+    def render_logo_partial
+      render partial: 'vendor/avo/partials/logo' rescue image_pack_tag 'logo.png', class: 'h-full', title: 'Avo'
+    end
   end
 end
