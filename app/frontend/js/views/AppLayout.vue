a05f06ae435860a8c30dfcff76769b0b902f24ac
Adrian Marin <adrian@adrianthedev.com>
2020-10-18 16:20:13 +0300

    chore: remove useless content from app layout

--- app/frontend/js/views/AppLayout.vue
+++ app/frontend/js/views/AppLayout.vue
@@ -1,17 +1,5 @@
-<template>
-  <div>
-    AppLayout
-  </div>
-</template>
-
 <script>
 export default {
-  data: () => ({}),
   props: ['routerKey', 'layout'],
-  computed: {},
-  methods: {},
-  mounted() {},
 }
 </script>
-
-<style lang="postcss"></style>
