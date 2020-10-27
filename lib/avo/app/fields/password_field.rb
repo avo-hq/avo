348fb53725347e4e1cc553f8f2ee0a31a4dfd2ca
mihaimdm <mihai.mdm2007@gmail.com>
2020-10-27 18:32:47 +0200

    feat: solve only_on not working

--- lib/avo/app/fields/password_field.rb
+++ lib/avo/app/fields/password_field.rb
@@ -10,7 +10,13 @@ module Avo
 
         super(name, **args, &block)
 
-        only_on :forms
+        if (args[:only_on].present? || args[:hide_on].present?) && (args[:only_on] == :create || args[:hide_on] == :edit)
+          only_on :create
+        elsif (args[:only_on].present? || args[:hide_on].present?) && (args[:only_on] == :edit || args[:hide_on] == :create)
+          only_on :edit
+        else
+          only_on :forms
+        end
       end
     end
   end
