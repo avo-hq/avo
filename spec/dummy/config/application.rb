d99385098d69f0383be5f8c6fc77327a410e6ff9
Adrian Marin <adrian@adrianthedev.com>
2020-11-09 19:01:26 +0200

    ci: add application rb i18n middleware

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -17,5 +17,7 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.use I18n::JS::Middleware
   end
 end
