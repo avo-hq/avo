ebc2b661c0202e8c7e39957a90786ea6278cd9fd
mihaimdm <mihai.mdm2007@gmail.com>
2020-12-07 20:19:06 +0200

    test wip

--- spec/system/avo/filters_spec.rb
+++ spec/system/avo/filters_spec.rb
@@ -203,4 +203,107 @@ RSpec.describe 'Filters', type: :system do
       end
     end
   end
+
+  describe 'Date filter' do
+    let!(:january_project) { create :project, name: 'January project', started_at: '2020-01-01 00:00:00' }
+    let!(:february_project) { create :project, name: 'February project', started_at: '2020-02-01 00:00:00' }
+    let!(:march_project) { create :project, name: 'March project', started_at: '2020-03-01 00:00:00' }
+
+    let(:url) { '/avo/resources/projects' }
+
+    context 'without default value' do
+      it 'displays the filter' do
+        visit url
+        find('[data-button="resource-filters"]').click
+
+        expect(page).to have_text 'Started after filter'
+        expect(page).to have_field('avo_filters_started_filter', placeholder: 'Select a date', type: 'hidden')
+        expect(page).to have_text 'January project'
+        expect(page).to have_text 'February project'
+        expect(page).to have_text 'March project'
+        expect(page).to have_button('Reset filters', disabled: true)
+      end
+
+      it 'changes the query' do
+        visit url
+        find('[data-button="resource-filters"]').click
+
+        fill_in_date_picker('Started after filter', with: '1969-07-20')
+        wait_for_loaded
+
+        expect(page).to have_text 'Started after filter'
+        expect(page).to have_field('avo_filters_started_filter', placeholder: 'Select a date', type: 'hidden')
+        expect(page).to have_text 'January project'
+        expect(page).to have_text 'February project'
+        expect(page).to have_text 'March project'
+        expect(page).to have_button('Reset filters', disabled: true)
+      end
+    end
+  end
+
+  # def fill_in_date_manually(label_text, with:)
+  #   with_open_flatpickr(label_text) do |field|
+  #     fill_in field[:id], with: with
+  #   end
+  # end
+
+  def fill_in_date_picker(label_text, with:)
+    within_open_flatpickr(label_text) do
+      within_flatpickr_months do
+        select_flatpickr_month(with.split('-')[1])
+
+        fill_in_flatpickr_year(with.split('-')[0])
+
+        click_on_flatpickr_day(with.split('-')[2])
+      end
+    end
+  end
+
+  def with_open_flatpickr(label_text)
+
+    field_label = find(:label, text: label_text)
+
+    date_field = field_label.sibling('.flatpickr-input')
+    date_field.click # Open the widget
+
+    yield(date_field)
+
+    date_field.send_keys :tab # Close the date picker widget
+  end
+
+  def within_open_flatpickr(label_text)
+    with_open_flatpickr(label_text) do
+      within find(:xpath, "/html/body/div[contains(@class, 'flatpickr-calendar')]") { yield }
+    end
+  end
+
+  def within_flatpickr_months
+    within find('.flatpickr-months .flatpickr-month .flatpickr-current-month') { yield }
+  end
+
+  def select_flatpickr_month(month)
+    find("select.flatpickr-monthDropdown-months > option:nth-child(#{month.to_i})").select_option
+  end
+
+  def fill_in_flatpickr_year(year)
+    find('input.cur-year').set(year)
+  end
+
+  def click_on_flatpickr_day(day)
+    within_flatpickr_days do
+      find('span', text: day).click
+    end
+  end
+
+  def within_flatpickr_days
+    within find('.flatpickr-innerContainer > .flatpickr-rContainer > .flatpickr-days') { yield }
+  end
+
+  # def fill_in_date_with_js(label_text, with:)
+  #   date_field = find(:label, text: label_text).sibling('.flatpickr-input')
+
+  #   script = "document.querySelector('##{date_field[:id]}').flatpickr().setDate('#{with}');"
+
+  #   page.execute_script(script)
+  # end
 end
