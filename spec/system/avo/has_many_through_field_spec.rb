3078a284ee170d8497d426be19fb3bb8816c8090
Adrian Marin <adrian@adrianthedev.com>
2021-01-19 14:14:34 +0200

    chore: comment out old specs

--- spec/system/avo/has_many_through_field_spec.rb
+++ spec/system/avo/has_many_through_field_spec.rb
@@ -1,57 +1,57 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'HasManyField', type: :system do
-  let!(:user) { create :user }
-  let!(:project) { create :project }
+# RSpec.describe 'HasManyField', type: :system do
+#   let!(:user) { create :user }
+#   let!(:project) { create :project }
 
-  subject { visit url; page }
+#   subject { visit url; page }
 
-  context 'show' do
-    let(:url) { "/avo/resources/users/#{user.id}" }
+#   context 'show' do
+#     let(:url) { "/avo/resources/users/#{user.id}" }
 
-    describe 'without a related project' do
-      it 'attaches a project' do
-        visit url
+#     describe 'without a related project' do
+#       it 'attaches a project' do
+#         visit url
 
-        wait_for_loaded
+#         wait_for_loaded
 
-        click_on 'Attach project'
+#         click_on 'Attach project'
 
-        expect(page).to have_text 'Choose project'
+#         expect(page).to have_text 'Choose project'
 
-        select project.name, from: 'options'
+#         select project.name, from: 'options'
 
-        click_on 'Attach'
-        wait_for_loaded
+#         click_on 'Attach'
+#         wait_for_loaded
 
-        expect(page).to have_text 'Project attached'
-        expect(page).not_to have_text 'Choose project'
-        expect(page).to have_text project.name
+#         expect(page).to have_text 'Project attached'
+#         expect(page).not_to have_text 'Choose project'
+#         expect(page).to have_text project.name
 
-        expect(user.projects.pluck('id')).to include project.id
-      end
-    end
-  end
+#         expect(user.projects.pluck('id')).to include project.id
+#       end
+#     end
+#   end
 
-  context 'show' do
-    let(:url) { "/avo/resources/users/#{user.id}" }
+#   context 'show' do
+#     let(:url) { "/avo/resources/users/#{user.id}" }
 
-    describe 'with a related project' do
-      it 'detaches the project' do
-        user.projects << project
+#     describe 'with a related project' do
+#       it 'detaches the project' do
+#         user.projects << project
 
-        visit url
-        wait_for_loaded
+#         visit url
+#         wait_for_loaded
 
-        find("tr[resource-id='#{project.id}'] a[data-control='detach']").click
+#         find("tr[resource-id='#{project.id}'] a[data-control='detach']").click
 
-        expect(page).to have_text 'Detach project'
+#         expect(page).to have_text 'Detach project'
 
-        click_on 'Confirm'
-        wait_for_loaded
+#         click_on 'Confirm'
+#         wait_for_loaded
 
-        expect(page).not_to have_text project.name
-      end
-    end
-  end
-end
+#         expect(page).not_to have_text project.name
+#       end
+#     end
+#   end
+# end
