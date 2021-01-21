cd23388ff34671f411b826ece92bcab22c59ba4a
mihaimdm <mihai.mdm2007@gmail.com>
2021-01-21 13:38:05 +0200

    wip

--- spec/dummy/app/avo/resources/user.rb
+++ spec/dummy/app/avo/resources/user.rb
@@ -31,7 +31,7 @@ module Avo
         # heading '<div class="text-gray-300 uppercase font-bold">DEV</div>', as_html: true
         # code :custom_css, theme: 'dracula', language: 'css', help: "This enables you to edit the user's custom styles.", height: '125px'
 
-        # hidden :team_id, default: 0 # For testing purposes
+        hidden :team_id, default: 0 # For testing purposes
 
         # has_and_belongs_to_many :projects
         # has_many :posts
---
+++ spec/features/avo/hidden_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'hidden', type: :feature do
+
+  context 'edit' do
+    let(:url) { "/avo/resources/users/#{user.id}/edit" }
+
+    describe 'without input' do
+      let!(:user) { create :user }
+      it 'has the hidden field empty' do
+        visit url
+
+        expect(find("[data-field-id='team_id'] input[type='hidden']", visible: false).value).to be_nil
+      end
+    end
+
+    describe 'with input' do
+      let!(:user) { create :user, team_id: 10 }
+
+      context 'edit' do
+        it 'has the hidden field' do
+          visit "/avo/resources/users/#{user.id}/edit"
+
+          expect(find("[data-field-id='team_id'] input[type='hidden']", visible: false).value).to eq '10'
+        end
+      end
+    end
+  end
+
+  context 'create' do
+    let(:url) { '/avo/resources/users/new' }
+
+    it 'has the hidden field prefilled with default' do
+      visit url
+
+      # expect(find("[data-field-id='team_id'] input[type='hidden']", visible: false).value).to eq 0
+    end
+  end
+
+end
