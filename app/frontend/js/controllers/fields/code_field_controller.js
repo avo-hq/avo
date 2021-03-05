29e155930bd49e560363fa4f8bc7ff007ed5996f
Adrian Marin <adrian@adrianthedev.com>
2021-03-05 12:27:17 +0200

    fix: code field syntax highlight broken

--- app/frontend/js/controllers/fields/code_field_controller.js
+++ app/frontend/js/controllers/fields/code_field_controller.js
@@ -1,6 +1,20 @@
 import { Controller } from 'stimulus'
 import { castBoolean } from '@/js/helpers/cast_boolean'
 import CodeMirror from 'codemirror'
+import 'codemirror/mode/css/css.js'
+import 'codemirror/mode/css/css.js'
+import 'codemirror/mode/dockerfile/dockerfile.js'
+import 'codemirror/mode/htmlmixed/htmlmixed.js'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/mode/markdown/markdown.js'
+import 'codemirror/mode/nginx/nginx.js'
+import 'codemirror/mode/php/php.js'
+import 'codemirror/mode/ruby/ruby.js'
+import 'codemirror/mode/sass/sass.js'
+import 'codemirror/mode/shell/shell.js'
+import 'codemirror/mode/sql/sql.js'
+import 'codemirror/mode/vue/vue.js'
+import 'codemirror/mode/xml/xml.js'
 
 export default class extends Controller {
   static targets = ['element']
