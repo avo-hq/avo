0ef7f1e7b3e9c88267ecec3fd35a6f7845ba20cb
Adrian Marin <adrian@adrianthedev.com>
2021-03-08 20:09:00 +0200

    chore: update localization file and add generator

---
+++ lib/generators/avo/locales_generator.rb
@@ -0,0 +1,12 @@
+require 'rails/generators/base'
+
+class InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path('templates', __dir__)
+
+  namespace 'avo:locales'
+  desc 'Creates an Avo initializer adds the route to the routes file.'
+
+  def create_files
+    template 'locales/avo.en.yml', 'config/locales/avo.en.yml'
+  end
+end
