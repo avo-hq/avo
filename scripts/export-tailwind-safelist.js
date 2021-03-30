4ef3599663c24a48ab496c094c846670f81f3054
Adrian Marin <adrian@adrianthedev.com>
2021-03-30 14:05:29 +0300

    fix: add missing button border

--- scripts/export-tailwind-safelist.js
+++ scripts/export-tailwind-safelist.js
@@ -5,7 +5,7 @@ const colors = require('tailwindcss/colors')
 
 // Dynamically built buttons need these classes in prod.
 // eslint-disable-next-line max-len
-const buttonClasses = ['hover:border-${color}-700', 'hover:border-${color}-600', 'bg-${color}-500', 'hover:bg-${color}-600', 'disabled:bg-${color}-300', 'hover:text-${color}-700', 'text-${color}-600']
+const buttonClasses = ['hover:border-${color}-700', 'border-${color}-600', 'bg-${color}-500', 'hover:bg-${color}-600', 'disabled:bg-${color}-300', 'hover:text-${color}-700', 'text-${color}-600']
 const ignoredButtonClasses = Object.keys(colors).map((color) => buttonClasses.map((buttonClass) => buttonClass.replace('${color}', kebabCase(color))))
 
 // Write them into a safelist.txt file until @tailwindcss/jit supports PurgeCSS options
