e3badc9d74776ce07309787f136d321b5239d2e0
Adrian Marin <adrian@adrianthedev.com>
2021-04-02 19:20:18 +0300

    chore: one more disabled rule

--- app/components/avo/edit/fields/belongs_to_field_component.rb
+++ app/components/avo/edit/fields/belongs_to_field_component.rb
@@ -4,6 +4,7 @@ class Avo::Edit::Fields::BelongsToFieldComponent < Avo::Edit::Fields::FieldCompo
   def disabled
     return true if @field.readonly
     return true if @field.target_resource.model_class.name == params[:via_resource_class]
+    return true if @field.id.to_s == params[:via_relation].to_s
 
     false
   end
