1d93c4110cb5ef497565ebede17d83415621a45d
Adrian Marin <adrian@adrianthedev.com>
2021-04-02 18:11:52 +0300

    chore: new release script (#310)

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,22 @@ set -e
 NAME=Avo
 BUMP=${1:-'patch'}
 
-bundle exec bump $BUMP --tag
+# Initial bump
+gem bump --no-commit
+
+# update the appraisal gemspecs
+bundle exec appraisal install
+
+# Get the version
 VERSION=$(bundle exec rails runner 'puts Avo::VERSION')
+MESSAGE="Bump $NAME to v$VERSION"
+TAG="v$VERSION"
+
+git add ./gemfiles
+git add ./lib/avo/version.rb
+git add ./Gemfile.lock
+
+git commit -m "$MESSAGE"
+git tag -a -m "$MESSAGE" $TAG
 
 git push --follow-tags
