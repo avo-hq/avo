0ac0a5c479a138bad86e9141b58b29f318c68d4f
Adrian Marin <adrian@adrianthedev.com>
2021-04-08 17:44:26 +0300

    test: add tests

---
+++ spec/features/avo/license_tinkering_in_application_controller_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.feature "LicenseTinkeringInApplicationController", type: :feature do
+  describe "check_avo_license" do
+    context "without tinkering" do
+      it "renders the response" do
+        expect {
+          visit "/avo/dashboard"
+        }.not_to raise_error
+      end
+    end
+
+    context "without tinkering" do
+      before do
+        Avo::ToolsController.define_method :check_avo_license do
+          ""
+        end
+      end
+
+      after do
+        Avo.send(:remove_const, :ToolsController)
+
+        class Avo::ToolsController < Avo::ApplicationController
+          def dashboard
+            @page_title = "Dashboard"
+            add_breadcrumb "Dashboard"
+          end
+        end
+      end
+
+      it "with tinkering" do
+        expect {
+          visit "/avo/dashboard"
+        }.to raise_error Avo::LicenseTinkeredError
+      end
+    end
+  end
+end
