78a8968bc6d018121966d8840ceab712dbac1aa4
Adrian Marin <adrian@adrianthedev.com>
2021-04-12 11:55:56 +0300

    fix: invalid license erros on /avo

--- app/controllers/avo/home_controller.rb
+++ app/controllers/avo/home_controller.rb
@@ -3,11 +3,10 @@ require_dependency "avo/application_controller"
 module Avo
   class HomeController < ApplicationController
     def index
-      @page_title = "Get started"
-
       if Avo.configuration.home_path.present?
         redirect_to Avo.configuration.home_path
       elsif !Rails.env.development?
+        @page_title = "Get started"
         redirect_to resources_path Avo::App.get_resources.min_by { |resource| resource.route_key }.model_class
       end
     end
