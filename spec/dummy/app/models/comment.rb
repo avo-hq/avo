838614abb96a5526e3be081463ee17d4fd0fbc54
Adrian Marin <adrian@adrianthedev.com>
2021-05-21 16:50:42 +0300

    chore: add tiny_name to comments

--- spec/dummy/app/models/comment.rb
+++ spec/dummy/app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true, optional: true
   belongs_to :user, optional: true
+
+  def tiny_name
+    ActionView::Base.full_sanitizer.sanitize(body).truncate 30
+  end
 end
