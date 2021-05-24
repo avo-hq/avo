87fd5caf45fdd11c43f1a51e3e9203ccb856e938
mihaimdm <mihai.mdm2007@gmail.com>
2021-05-24 22:32:23 +0300

    update project policy and fix badge symbol

--- spec/dummy/app/policies/project_policy.rb
+++ spec/dummy/app/policies/project_policy.rb
@@ -1,4 +1,32 @@
 class ProjectPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def destroy?
+    true
+  end
+
   class Scope < Scope
     def resolve
       scope.all
