4181ecd803c738850773c0028bff46e758d29973
Adrian Marin <adrian@adrianthedev.com>
2021-06-03 14:11:19 +0300

    chore: explain whay we use the env for rails version

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,6 +10,7 @@ Spring.application_root = "."
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    # We're going to load the defaults from the env because we're using appraisal and differently versioned gems.
     config.load_defaults ENV["RAILS_VERSION"] || 6.1
 
     # Settings in config/environments/* take precedence over those specified here.
