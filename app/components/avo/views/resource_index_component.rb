84e3f43d44e8cd6525fb1c6f006acf6afeae8f52
David Mihai Marin <mihai.mdm2007@gmail.com>
2021-06-23 20:59:59 +0300

    fix: allow readonly on resources (#442)
    * fix: unscope only order for table
    
    * fix: allow readonly for relations
    
    * implement comments
    
    * fix tests

--- app/components/avo/views/resource_index_component.rb
+++ app/components/avo/views/resource_index_component.rb
@@ -45,18 +45,35 @@ class Avo::Views::ResourceIndexComponent < Avo::ResourceComponent
   end
 
   def can_create?
-    @resource.authorization.authorize_action(:create, raise_exception: false) && simple_relation?
+    @resource.authorization.authorize_action(:create, raise_exception: false) && simple_relation? && !has_reflection_and_is_read_only
   end
 
   def can_attach?
     klass = @reflection
     klass = @reflection.through_reflection if klass.is_a? ::ActiveRecord::Reflection::ThroughReflection
 
-    @reflection.present? && klass.is_a?(::ActiveRecord::Reflection::HasManyReflection)
+    @reflection.present? && klass.is_a?(::ActiveRecord::Reflection::HasManyReflection) && !has_reflection_and_is_read_only
   end
 
   def can_detach?
-    @reflection.present? && @reflection.is_a?(::ActiveRecord::Reflection::HasOneReflection) && @models.present?
+    @reflection.present? && @reflection.is_a?(::ActiveRecord::Reflection::HasOneReflection) && @models.present? && !has_reflection_and_is_read_only
+  end
+
+  def has_reflection_and_is_read_only
+    if @reflection.present? && @reflection.active_record.name && @reflection.name
+      fields = ::Avo::App.get_resource_by_model_name(@reflection.active_record.name).get_field_definitions
+      filtered_fields = fields.filter{ |f| f.id == @reflection.name}
+    else
+      return false
+    end
+
+    if filtered_fields
+      is_field_read_only = filtered_fields.filter{ |f| f.id == @reflection.name}[0].readonly
+    else
+      is_field_read_only = false
+    end
+
+    is_field_read_only
   end
 
   def create_path
