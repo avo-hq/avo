26a110c44e99a0acb0740c9b56d3a2e40453d300
Adrian Marin <adrian@adrianthedev.com>
2021-06-23 23:52:20 +0300

    feature: add standalone action generator

--- lib/generators/avo/action_generator.rb
+++ lib/generators/avo/action_generator.rb
@@ -4,10 +4,20 @@ module Generators
   module Avo
     class ActionGenerator < Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
+      class_option :standalone, type: :boolean
+
       namespace "avo:action"
 
       def create_resource_file
-        template "action.tt", "app/avo/actions/#{singular_name}.rb"
+        type = "resource"
+
+        type = "standalone" if options[:standalone]
+
+        if type == "standalone"
+          template "standalone_action.tt", "app/avo/actions/#{singular_name}.rb"
+        else
+          template "action.tt", "app/avo/actions/#{singular_name}.rb"
+        end
       end
     end
   end
