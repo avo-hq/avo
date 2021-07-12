dc45036cd32569905bf2f06e3978db26de97544f
Chris Parsons <chris@thinkcodelearn.com>
2021-07-12 15:29:17 +0100

    feature: Select all on controller (#473)
    Works like a charm! Thanks for the contribution 🔥

--- app/helpers/avo/resources_helper.rb
+++ app/helpers/avo/resources_helper.rb
@@ -48,8 +48,21 @@ module Avo
     def item_selector_input(floating: false, size: :md)
       "<input type='checkbox'
         class='mx-3 #{"absolute inset-auto left-0 mt-2 z-10 hidden group-hover:block checked:block" if floating} #{size.to_sym == :lg ? "w-5 h-5" : "w-4 h-4"}'
-        data-action='input->item-selector#toggle'
+        data-action='input->item-selector#toggle input->item-select-all#update'
+        data-item-select-all-target='itemCheckbox'
+        name='#{t "avo.select_item"}'
         title='#{t "avo.select_item"}'
+        data-tippy='tooltip'
+      />"
+    end
+
+    def item_select_all_input
+      "<input type='checkbox'
+        class='mx-3 w-4 h-4'
+        data-action='input->item-select-all#toggle'
+        data-item-select-all-target='checkbox'
+        name='#{t "avo.select_all"}'
+        title='#{t "avo.select_all"}'
         data-tippy='tooltip'
       />"
     end
---
+++ app/packs/js/controllers/item_select_all_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = [ "itemCheckbox", "checkbox" ]
+
+  connect() {
+    this.resourceName = this.element.dataset.resourceName
+  }
+
+  toggle(event) {
+    var value = !!event.target.checked
+    document.querySelectorAll(`[data-controller="item-selector"][data-resource-name="${this.resourceName}"] input[type=checkbox]`)
+      .forEach((checkbox) => checkbox.checked != value && checkbox.click())
+  }
+
+  update() {
+    var allSelected = true
+    this.itemCheckboxTargets.forEach((checkbox) => allSelected = allSelected && checkbox.checked)
+    this.checkboxTarget.checked = allSelected
+  }
+}
