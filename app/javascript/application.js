25ef673de310e67b76b6d97b77e353766d96d59d
Adrian Marin <adrian@adrianthedev.com>
2022-02-01 22:51:07 +0200

    wip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,17 +4,14 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import * as ActiveStorage from '@rails/activestorage'
 import * as Mousetrap from 'mousetrap'
-// import { Application } from 'stimulus'
 import { Turbo } from '@hotwired/turbo-rails'
-// import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import Rails from '@rails/ujs'
 import tippy from 'tippy.js'
 
 // Toastr alerts
 import './js/active-storage'
-import './js/controllers.js'
+import './js/controllers'
 import './js/toastr'
-import {application} from './js/application'
 
 Rails.start()
 
@@ -37,12 +34,6 @@ window.initTippy = initTippy
 
 ActiveStorage.start()
 
-// const context = require.context('./../js/controllers', true, /\.js$/)
-// application.load(definitionsFromContext(context))
-
-// const fieldsContext = require.context('./../js/controllers/fields', true, /\.js$/)
-// application.load(definitionsFromContext(fieldsContext))
-
 document.addEventListener('turbo:load', () => {
   document.body.classList.remove('turbo-loading')
   initTippy()
@@ -59,10 +50,3 @@ document.addEventListener('turbo:submit-start', () => document.body.classList.ad
 document.addEventListener('turbo:before-cache', () => {
   document.querySelectorAll('[data-turbo-remove-before-cache]').forEach((element) => element.remove())
 })
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
