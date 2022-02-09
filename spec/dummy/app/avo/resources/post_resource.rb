d40cccc3ed7e58242a0b4adfe3f531c48a1b925e
Adrian Marin <adrian@adrianthedev.com>
2022-02-09 18:58:34 +0200

    wip

--- spec/dummy/app/avo/resources/post_resource.rb
+++ spec/dummy/app/avo/resources/post_resource.rb
@@ -11,7 +11,6 @@ class PostResource < Avo::BaseResource
   field :name, as: :text, required: true
   field :body, as: :trix, placeholder: "Enter text", always_show: false, attachment_key: :attachments, hide_attachment_url: true, hide_attachment_filename: true, hide_attachment_filesize: true
   field :cover_photo, as: :file, is_image: true, as_avatar: :rounded
-  field :user, as: :belongs_to, placeholder: "—"
   field :excerpt, as: :text, hide_on: :all, as_description: true do |model|
     ActionView::Base.full_sanitizer.sanitize(model.body).truncate 130
   rescue
@@ -22,6 +21,7 @@ class PostResource < Avo::BaseResource
   field :is_published, as: :boolean do |model|
     model.published_at.present?
   end
+  field :user, as: :belongs_to, placeholder: "—"
   field :status, as: :select, enum: ::Post.statuses, display_value: false
   field :comments, as: :has_many
 
