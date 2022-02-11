e06e29ce0050651b4b3ae97dd97b7b17289509cc
Jonas Reif <reif.jonas@gmx.de>
2022-02-11 16:33:28 +0100

    Feature: Add is_audio option to file fields to show a html audio_tag (#650)
    Co-authored-by: Adrian Marin <adrian@adrianthedev.com>
    
    Thank you for your contribution!

--- lib/avo/fields/file_field.rb
+++ lib/avo/fields/file_field.rb
@@ -4,6 +4,7 @@ module Avo
       attr_accessor :link_to_resource
       attr_accessor :is_avatar
       attr_accessor :is_image
+      attr_accessor :is_audio
       attr_accessor :direct_upload
 
       def initialize(id, **args, &block)
@@ -12,6 +13,7 @@ module Avo
         @link_to_resource = args[:link_to_resource].present? ? args[:link_to_resource] : false
         @is_avatar = args[:is_avatar].present? ? args[:is_avatar] : false
         @is_image = args[:is_image].present? ? args[:is_image] : @is_avatar
+        @is_audio = args[:is_audio].present? ? args[:is_audio] : false
         @direct_upload = args[:direct_upload].present? ? args[:direct_upload] : false
       end
 
--- spec/dummy/app/avo/resources/post_resource.rb
+++ spec/dummy/app/avo/resources/post_resource.rb
@@ -11,6 +11,7 @@ class PostResource < Avo::BaseResource
   field :name, as: :text, required: true
   field :body, as: :trix, placeholder: "Enter text", always_show: false, attachment_key: :attachments, hide_attachment_url: true, hide_attachment_filename: true, hide_attachment_filesize: true
   field :cover_photo, as: :file, is_image: true, as_avatar: :rounded
+  field :audio, as: :file, is_audio: true
   field :excerpt, as: :text, hide_on: :all, as_description: true do |model|
     ActionView::Base.full_sanitizer.sanitize(model.body).truncate 130
   rescue
