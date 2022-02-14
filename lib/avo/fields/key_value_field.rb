08b0cacb1d10d9c662a2e0b48e96ba71ddca1ffa
Adrian Marin <adrian@adrianthedev.com>
2022-02-14 16:48:48 +0200

    use present? where available

--- lib/avo/fields/key_value_field.rb
+++ lib/avo/fields/key_value_field.rb
@@ -11,10 +11,10 @@ module Avo
 
         hide_on :index
 
-        @key_label = args[:key_label].to_s if args[:key_label].present?
-        @value_label = args[:value_label].to_s if args[:value_label].present?
-        @action_text = args[:action_text].to_s if args[:action_text].present?
-        @delete_text = args[:delete_text].to_s if args[:delete_text].present?
+        @key_label = args[:key_label] if args[:key_label].present?
+        @value_label = args[:value_label] if args[:value_label].present?
+        @action_text = args[:action_text] if args[:action_text].present?
+        @delete_text = args[:delete_text] if args[:delete_text].present?
 
         @disable_editing_keys = args[:disable_editing_keys].present? ? args[:disable_editing_keys] : false
         # disabling editing keys also disables adding rows (doesn't take into account the value of disable_adding_rows)
@@ -29,25 +29,25 @@ module Avo
       end
 
       def key_label
-        return @key_label if @key_label && !@key_label.empty?
+        return @key_label if @key_label.present?
 
         I18n.translate('avo.key_value_field.key')
       end
 
       def value_label
-        return @value_label if @value_label && !@value_label.empty?
+        return @value_label if @value_label.present?
 
         I18n.translate('avo.key_value_field.value')
       end
 
       def action_text
-        return @action_text if @action_text && !@action_text.empty?
+        return @action_text if @action_text.present?
 
         I18n.translate('avo.key_value_field.add_row')
       end
 
       def delete_text
-        return @delete_text if @delete_text && !@delete_text.empty?
+        return @delete_text if @delete_text.present?
 
         I18n.translate('avo.key_value_field.delete_row')
       end
