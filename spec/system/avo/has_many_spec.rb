7ef43db8082197a7a043ed9b3e277f77a419b521
Adrian Marin <adrian@adrianthedev.com>
2022-02-18 12:42:32 +0200

    different sleeps

--- spec/system/avo/has_many_spec.rb
+++ spec/system/avo/has_many_spec.rb
@@ -47,9 +47,10 @@ RSpec.feature "HasManyField", type: :system do
         expect {
           find("[data-resource-id='#{comments.first.id}'] [data-control='destroy']").click
           page.driver.browser.switch_to.alert.accept
+          sleep 0.1
           find("[data-resource-id='#{comments.third.id}'] [data-control='destroy']").click
           page.driver.browser.switch_to.alert.accept
-          sleep 0.2
+          sleep 0.1
         }.to change(Comment, :count).by(-2)
 
         expect(page).to have_current_path url
