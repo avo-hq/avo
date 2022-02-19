030f2fc914756c9fe5e3c5095d60fdf88c39aca9
Ricky Chilcott <ricky@missionmet.com>
2022-02-19 16:55:45 -0500

    feature: Allow Avo::NavigationLinkComponent to accept an optional target param (#685)

--- app/components/avo/navigation_link_component.rb
+++ app/components/avo/navigation_link_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Avo::NavigationLinkComponent < ViewComponent::Base
-  def initialize(label: nil, path: nil, active: :inclusive, size: :md)
+  def initialize(label: nil, path: nil, active: :inclusive, size: :md, target: "_self")
     @label = label
     @path = path
     @active = active
     @size = size
+    @target = target
   end
 end
