6e1a862f4cda6806a12b535011cfb605991b7232
Adrian Marin <adrian@adrianthedev.com>
2022-03-10 16:05:36 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -154,14 +154,6 @@ module.exports = {
     screens: {
       xs: '495px',
       ...defaultTheme.screens,
-      // 'sm': '640px',
-      // 'tablet': '768px',
-      // 'laptop': '1024px',
-      // 'desktop': '1280px',
-      // // => @media (min-width: 1280px) { ... }
-
-      // '2xl': '1536px',
-      // // => @media (min-width: 1536px) { ... }
     },
   },
   variants: {
