ffce0603a79361baa79097abd460a0ec1491f7dc
Adrian Marin <adrian@adrianthedev.com>
2022-03-11 15:37:52 +0200

    wip

--- spec/components/avo/empty_state_component_spec.rb
+++ spec/components/avo/empty_state_component_spec.rb
@@ -1,8 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Avo::EmptyStateComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   # it "renders something useful" do
   #   expect(
   #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
