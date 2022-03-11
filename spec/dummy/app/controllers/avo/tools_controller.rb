3f100176e3f8ea4c338511e6ded085e5f2ca27e6
Adrian Marin <adrian@adrianthedev.com>
2022-03-11 15:09:23 +0200

    wip

--- spec/dummy/app/controllers/avo/tools_controller.rb
+++ spec/dummy/app/controllers/avo/tools_controller.rb
@@ -1,7 +1,5 @@
 class Avo::ToolsController < Avo::ApplicationController
   def dashboard
-    flash.now[:notice] = "This is a very long notice notiifcation to dusplay on mobile"
-    flash.now[:error] = "This is a very long error notiifcation to dusplay on mobile"
     @page_title = "Dashboard"
   end
 end
