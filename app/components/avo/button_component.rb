a16382517a261b6ab84b392e557a402fb7e285ad
Adrian Marin <adrian@adrianthedev.com>
2022-03-11 15:16:25 +0200

    codeclimate

--- app/components/avo/button_component.rb
+++ app/components/avo/button_component.rb
@@ -40,28 +40,10 @@ class Avo::ButtonComponent < ViewComponent::Base
 
     classes += " space-x-1" if content.present? && @icon.present?
 
-    classes += case @style
-    when :primary
-      " bg-primary-500 text-white border-primary-500 hover:bg-primary-600 hover:border-primary-600 active:border-primary-700 active:outline-primary-700 active:bg-primary-600"
-    when :outline
-      " bg-white text-#{@color}-500 border-#{@color}-500 hover:bg-#{@color}-100 active:bg-#{@color}-100 active:border-#{@color}-500 active:outline-#{@color}-500"
-    else
-      ""
-    end
+    classes += style_classes
 
     classes += @compact ? " px-1" : " px-4" # Same horizontal padding on all sizes
-    classes += case @size.to_sym
-    when :xs
-      " py-0"
-    when :sm
-      " py-1"
-    when :md
-      " py-2"
-    when :lg
-      " py-3"
-    else
-      ""
-    end
+    classes += spacing_classes
 
     classes
   end
@@ -73,27 +55,7 @@ class Avo::ButtonComponent < ViewComponent::Base
   def full_content
     result = ""
     icon_classes = @icon_class
-
-    case @size
-    when :xs
-      icon_classes += " h-4"
-      # When icon is solo we need to add an offset
-      icon_classes += " my-1" if content.blank?
-    when :sm
-      icon_classes += " h-4"
-      # When icon is solo we need to add an offset
-      icon_classes += " my-1" if content.blank?
-    when :md
-      icon_classes += " h-5"
-      # When icon is solo we need to add an offset
-      icon_classes += " my-0.5" if content.blank?
-    when :lg
-      icon_classes += " h-6"
-    else
-      ""
-    end
-
-    # icon_classes += content.present? ? " mr-1" : ""
+    icon_classes = full_content_icon_classes
 
     result += helpers.svg(@icon, class: icon_classes) if @icon.present?
     result += "<span>#{content}</span>" if content.present?
@@ -125,5 +87,56 @@ class Avo::ButtonComponent < ViewComponent::Base
         full_content
       end
     end
+  end
+
+  private
+
+  def spacing_classes
+    case @size.to_sym
+    when :xs
+      " py-0"
+    when :sm
+      " py-1"
+    when :md
+      " py-2"
+    when :lg
+      " py-3"
+    else
+      ""
+    end
+  end
+
+  def style_classes
+    case @style
+    when :primary
+      " bg-primary-500 text-white border-primary-500 hover:bg-primary-600 hover:border-primary-600 active:border-primary-700 active:outline-primary-700 active:bg-primary-600"
+    when :outline
+      " bg-white text-#{@color}-500 border-#{@color}-500 hover:bg-#{@color}-100 active:bg-#{@color}-100 active:border-#{@color}-500 active:outline-#{@color}-500"
+    else
+      ""
+    end
+  end
+
+  def full_content_icon_classes
+    icon_classes = ""
+
+    case @size
+    when :xs
+      icon_classes += " h-4"
+      # When icon is solo we need to add an offset
+      icon_classes += " my-1" if content.blank?
+    when :sm
+      icon_classes += " h-4"
+      # When icon is solo we need to add an offset
+      icon_classes += " my-1" if content.blank?
+    when :md
+      icon_classes += " h-5"
+      # When icon is solo we need to add an offset
+      icon_classes += " my-0.5" if content.blank?
+    when :lg
+      icon_classes += " h-6"
+    end
+
+    icon_classes
   end
 end
