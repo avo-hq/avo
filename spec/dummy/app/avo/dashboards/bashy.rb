6c3edc59007f962d43eb76fa4b9fa89832fbd93e
Adrian Marin <adrian@adrianthedev.com>
2022-03-25 12:05:27 +0200

    wip

--- spec/dummy/app/avo/dashboards/bashy.rb
+++
@@ -1,9 +0,0 @@
-class Bashy < Avo::Dashboards::BaseDashboard
-  self.id = 'bashy'
-  self.name = 'Bashy'
-  self.description = 'Tiny dashboard description'
-  self.grid_cols = 6
-
-  # cards go here
-  # card UsersSomething
-end
