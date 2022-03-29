bb3daf7ac75370c25964cb7985ff1e4d18e8e6f2
Adrian Marin <adrian@adrianthedev.com>
2022-03-29 15:50:09 +0200

    fix: show errors for critical operations

---
+++ spec/features/avo/destroy_record_spec_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "DestroyRecordSpecs", type: :system do
+  describe "foreign key constraints" do
+    let!(:user) { create :user }
+    let!(:comment) { create :comment, user: user }
+
+    it "raises error" do
+      url = "/admin/resources/users/#{user.slug}"
+      visit url
+
+      expect {
+        click_on "Delete"
+        wait_for_loaded
+      }.not_to change(User, :count)
+
+      expect(current_path).to eq url
+      expect(page).to have_text "PG::ForeignKeyViolation"
+    end
+  end
+end
