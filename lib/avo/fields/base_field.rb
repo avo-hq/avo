5aaf1ff405ec0d74626ad80fecd9119e8c7e9d9a
Adrian Marin <adrian@adrianthedev.com>
2022-03-30 13:58:00 +0300

    fix: has_many turbo frame crash (#758)

--- lib/avo/fields/base_field.rb
+++ lib/avo/fields/base_field.rb
@@ -190,8 +190,12 @@ module Avo
         "#{type.camelize}Field"
       end
 
+      # Try and build the component class or fallback to a blank one
       def component_for_view(view = :index)
-        "Avo::Fields::#{view_component_name}::#{view.to_s.camelize}Component".safe_constantize
+        component_class = "::Avo::Fields::#{view_component_name}::#{view.to_s.camelize}Component"
+        component_class.constantize
+      rescue
+        ::Avo::BlankFieldComponent
       end
 
       def model_errors
