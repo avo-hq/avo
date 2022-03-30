c1c92183bd3b0a7047d12cde67f3d2e8fe85eb2f
Adrian Marin <adrian@adrianthedev.com>
2022-03-30 16:59:52 +0200

    revert

--- app/components/avo/panel_component.rb
+++ app/components/avo/panel_component.rb
@@ -3,8 +3,6 @@
 class Avo::PanelComponent < ViewComponent::Base
   attr_reader :title
 
-  RIGHT_ALIGNED_BUTTONS_BREAKPOINT = "xl"
-
   renders_one :tools
   renders_one :body
   renders_one :bare_content
@@ -39,9 +37,5 @@ class Avo::PanelComponent < ViewComponent::Base
 
   def render_header?
     @title.present? || description.present? || tools.present? || display_breadcrumbs?
-  end
-
-  def bp
-    RIGHT_ALIGNED_BUTTONS_BREAKPOINT
   end
 end
