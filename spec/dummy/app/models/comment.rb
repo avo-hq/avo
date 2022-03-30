431dc5428aa41c3a73f8493b30452ebccc5551a5
Adrian Marin <adrian@adrianthedev.com>
2022-03-31 00:00:38 +0300

    fix: wrong redirect after validation failed when wediting a has many association (#762)

--- spec/dummy/app/models/comment.rb
+++ spec/dummy/app/models/comment.rb
@@ -1,12 +1,20 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
+  validate :body_different
 
   belongs_to :commentable, polymorphic: true, optional: true
   belongs_to :user, optional: true
 
-  scope :starts_with, -> (prefix) { where('LOWER(body) LIKE ?', "#{prefix}%") }
+  scope :starts_with, ->(prefix) { where("LOWER(body) LIKE ?", "#{prefix}%") }
 
   def tiny_name
     ActionView::Base.full_sanitizer.sanitize(body.to_s).truncate 60
+  end
+
+  def body_different
+    possible_comment = Comment.where(body: body).where.not(id: id).first
+    if possible_comment.present?
+      errors.add :body, message: "exists in another Comment."
+    end
   end
 end
