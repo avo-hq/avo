5797e0a1bd417f9ee3926dea346f3bcbb20eb52e
Adrian Marin <adrian@adrianthedev.com>
2022-04-09 13:58:53 +0300

    eager load unless prod

--- lib/avo/dynamic_router.rb
+++ lib/avo/dynamic_router.rb
@@ -1,7 +1,7 @@
 module Avo
   module DynamicRouter
     def self.routes(router)
-      Rails.application.eager_load! if Rails.env.development?
+      Rails.application.eager_load! unless Rails.env.production?
 
       BaseResource.descendants
         .select do |resource|
