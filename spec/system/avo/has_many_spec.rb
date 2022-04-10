f2e3ac404490e761b7b46ecd8d93d0ee1b08d3f8
Adrian Marin <adrian@adrianthedev.com>
2022-04-10 15:45:47 +0300

    wip

--- spec/system/avo/has_many_spec.rb
+++ spec/system/avo/has_many_spec.rb
@@ -59,7 +59,7 @@ RSpec.feature "HasManyField", type: :system do
         expect(page).not_to have_text comments.third.tiny_name.to_s
         expect(page).to have_text comments.second.tiny_name.to_s
 
-        sleep 0.2
+        sleep 0.5
         expect(page).to have_text("Resource destroyed").twice
       end
     end
