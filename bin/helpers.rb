eb9706e473edb05c8b6b13e08640f4694e08629c
Jon Frisby <jfrisby@mrjoy.com>
2022-04-10 08:43:02 -0700

    Fix/init input handling (#804)

--- bin/helpers.rb
+++ bin/helpers.rb
@@ -22,8 +22,14 @@ end
 
 def ask(question:, valid_answers: [])
   puts "\n#{question} (#{valid_answers.join('/')})"
+  # An uppercase option is treated as a default answer.  Otherwise, we disregard case, and always
+  # return the answer in lowercase.
+  default_answer = valid_answers.select { |val| val == val.upcase }.first&.downcase
 
-  input = gets.chomp
+  valid_answers.map!(&:downcase)
+
+  input = gets.downcase.chomp
+  input = default_answer if input == ''
 
   while !valid_answers.include?(input)
     puts 'Invalid input, please try again.'
