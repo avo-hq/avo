8e3a71b7c163c652813e9563f48e4774fefa9b9c
Adrian Marin <adrian@adrianthedev.com>
2022-04-14 02:01:52 +0300

    wip

--- spec/dummy/config/initializers/avo.rb
+++ spec/dummy/config/initializers/avo.rb
@@ -29,7 +29,7 @@ Avo.configure do |config|
       end
     end
 
-    section "Resources" do
+    section "Resources", icon: "heroicons/outline/academic-cap" do
       group "Academia" do
         resource :course
         resource :course_link
