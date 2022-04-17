15664cb15c553122eb69e3ed66051e6597b1ac35
Adrian Marin <adrian@adrianthedev.com>
2022-04-17 22:16:03 +0300

    refactor: use default cllapsed state for menus

--- app/components/avo/sidebar/heading_component.rb
+++ app/components/avo/sidebar/heading_component.rb
@@ -2,12 +2,14 @@
 
 class Avo::Sidebar::HeadingComponent < ViewComponent::Base
   attr_reader :collapsable
+  attr_reader :collapsed
   attr_reader :icon
   attr_reader :key
   attr_reader :label
 
-  def initialize(label: nil, icon: nil, collapsable: false, key: nil)
+  def initialize(label: nil, icon: nil, collapsable: false, collapsed: false, key: nil)
     @collapsable = collapsable
+    @collapsed = collapsed
     @icon = icon
     @key = key
     @label = label
