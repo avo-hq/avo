b7ed012acdf2744cbaf846db7113bd0d3a38b642
Adrian Marin <adrian@adrianthedev.com>
2022-04-19 15:22:11 +0300

    chore: add annotate gem (#855)

--- spec/dummy/app/models/spouse.rb
+++ spec/dummy/app/models/spouse.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#  person_id  :bigint
+#
 class Spouse < Person
   belongs_to :person, foreign_key: :marriage_person_id, optional: true
 end
