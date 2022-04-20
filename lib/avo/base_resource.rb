527108e3bfcd6d9f2fd0d9f12e22cc4b3992b927
Adrian Marin <adrian@adrianthedev.com>
2022-04-20 23:06:29 +0300

    mark tags as pro

--- lib/avo/base_resource.rb
+++ lib/avo/base_resource.rb
@@ -135,6 +135,12 @@ module Avo
         end
       end
 
+      if Avo::App.license.lacks_with_trial(:advanced_fields)
+        fields = fields.reject do |field|
+          field.type == 'tags'
+        end
+      end
+
       fields
     end
 
