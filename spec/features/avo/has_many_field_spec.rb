522ca70135c3571347294974f70635b751807d3b
Adrian Marin <adrian@adrianthedev.com>
2022-04-29 17:13:57 +0300

    specs

--- spec/features/avo/has_many_field_spec.rb
+++ spec/features/avo/has_many_field_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "HasManyField", type: :feature do
-  let!(:user) { create :user }
+  let!(:user) { create :user, first_name: "Alicia" }
 
   subject do
     visit url
