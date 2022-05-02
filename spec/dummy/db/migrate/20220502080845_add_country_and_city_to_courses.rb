3004897b1b188ae5eff622356c95f43456512cf5
Adrian Marin <adrian@adrianthedev.com>
2022-05-02 13:56:32 +0300

    migration file 6.0

--- spec/dummy/db/migrate/20220502080845_add_country_and_city_to_courses.rb
+++ spec/dummy/db/migrate/20220502080845_add_country_and_city_to_courses.rb
@@ -1,4 +1,4 @@
-class AddCountryAndCityToCourses < ActiveRecord::Migration[6.1]
+class AddCountryAndCityToCourses < ActiveRecord::Migration[6.0]
   def change
     add_column :courses, :country, :string
     add_column :courses, :city, :string
