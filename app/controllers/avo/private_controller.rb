94c9ab05dcc9a4dcc4cc248dacf4207511007777
Adrian Marin <adrian@adrianthedev.com>
2022-05-11 11:57:39 +0300

    refactor: cleaner design (#884)

--- app/controllers/avo/private_controller.rb
+++ app/controllers/avo/private_controller.rb
@@ -3,6 +3,7 @@ require_dependency "avo/application_controller"
 module Avo
   class PrivateController < ApplicationController
     def design
+      @page_title = "Design [Private]"
     end
   end
 end
--- spec/features/avo/create_through_record_spec.rb
+++ spec/features/avo/create_through_record_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'create_through_record', type: :feature do
+RSpec.feature "create_through_record", type: :feature do
   # create post on user
-  describe 'belongs_to' do
+  describe "belongs_to" do
     let(:url) { "/admin/resources/posts/new?via_relation=user&via_relation_class=User&via_resource_id=#{admin.id}" }
 
-    it 'attaches the user to the post' do
+    it "attaches the user to the post" do
       expect(Post.count).to eq 0
       visit url
 
-      fill_in 'post_name', with: 'post name'
-      click_on 'Save'
+      fill_in "post_name", with: "post name"
+      click_on "Save"
 
       wait_for_loaded
 
@@ -20,15 +20,15 @@ RSpec.feature 'create_through_record', type: :feature do
     end
   end
 
-  describe 'belongs_to comment' do
+  describe "belongs_to comment" do
     let(:url) { "/admin/resources/comments/new?via_relation=user&via_relation_class=User&via_resource_id=#{admin.id}" }
 
-    it 'attaches the user to the team' do
+    it "attaches the user to the team" do
       expect(Comment.count).to eq 0
       visit url
 
-      fill_in 'comment_body', with: 'Comment body'
-      click_on 'Save'
+      fill_in "comment_body", with: "Comment body"
+      click_on "Save"
 
       wait_for_loaded
 
@@ -39,17 +39,17 @@ RSpec.feature 'create_through_record', type: :feature do
   end
 
   # create person on spouse
-  describe 'has_many' do
+  describe "has_many" do
     let!(:person) { create :person }
     let(:url) { "/admin/resources/spouses/new?via_relation=spouses&via_relation_class=Person&via_resource_id=#{person.id}" }
 
-    it 'attaches the spouse to a person' do
+    it "attaches the spouse to a person" do
       expect(Person.count).to eq 1
       expect(person.spouses.first).to be nil
       visit url
 
-      fill_in 'person_name', with: 'Mary'
-      click_on 'Save'
+      fill_in "person_name", with: "Mary"
+      click_on "Save"
 
       wait_for_loaded
 
@@ -60,20 +60,20 @@ RSpec.feature 'create_through_record', type: :feature do
   end
 
   # create user on team
-  describe 'has_many through' do
+  describe "has_many through" do
     let!(:team) { create :team }
     let(:url) { "/admin/resources/users/new?via_relation=teams&via_relation_class=Team&via_resource_id=#{team.id}" }
 
-    it 'attaches the user to the team' do
+    it "attaches the user to the team" do
       expect(User.count).to eq 1 # the admin is there
       visit url
 
-      fill_in 'user_first_name', with: 'first'
-      fill_in 'user_last_name', with: 'last'
-      fill_in 'user_email', with: 'user@email.com'
-      fill_in 'user_password', with: 'passpasspass'
-      fill_in 'user_password_confirmation', with: 'passpasspass'
-      click_on 'Save'
+      fill_in "user_first_name", with: "first"
+      fill_in "user_last_name", with: "last"
+      fill_in "user_email", with: "user@email.com"
+      fill_in "user_password", with: "passpasspass"
+      fill_in "user_password_confirmation", with: "passpasspass"
+      click_on "Save"
 
       wait_for_loaded
 
@@ -84,16 +84,16 @@ RSpec.feature 'create_through_record', type: :feature do
   end
 
   # create project on user
-  describe 'has_and_belongs_to_many' do
+  describe "has_and_belongs_to_many" do
     let(:url) { "/admin/resources/projects/new?via_relation=users&via_relation_class=User&via_resource_id=#{admin.id}" }
 
-    it 'attaches the user to the team' do
+    it "attaches the user to the team" do
       expect(Project.count).to eq 0
       visit url
 
-      fill_in 'project_name', with: 'Projy'
-      fill_in 'project_users_required', with: '15'
-      click_on 'Save'
+      fill_in "project_name", with: "Projy"
+      fill_in "project_users_required", with: "15"
+      click_on "Save"
 
       wait_for_loaded
 
@@ -104,16 +104,16 @@ RSpec.feature 'create_through_record', type: :feature do
   end
 
   # create create comment on user
-  describe 'polymorphic' do
+  describe "polymorphic" do
     let!(:project) { create :project }
     let(:url) { "/admin/resources/comments/new?via_relation=commentable&via_relation_class=Project&via_resource_id=#{project.id}" }
 
-    it 'attaches the user to the team' do
+    it "attaches the user to the team" do
       expect(Comment.count).to eq 0
       visit url
 
-      fill_in 'comment_body', with: 'Comment body'
-      click_on 'Save'
+      fill_in "comment_body", with: "Comment body"
+      click_on "Save"
 
       wait_for_loaded
 
