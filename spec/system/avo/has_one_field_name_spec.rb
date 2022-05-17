3b11a81e066d02c66bcce2f2ad14221f17c1f53b
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2022-05-17 13:01:05 +0100

    return field name if field is has_one (#906)
    * return field name if field is has_one
    
    * name coherence
    
    * test
    
    * test rubocop trigger
    
    * attach and detach test

---
+++ spec/system/avo/has_one_field_name_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'HasOneFieldName', type: :system do
+  let!(:user) { create :user }
+  let!(:post) { create :post }
+
+  subject {
+    visit url
+    page 
+  }
+
+  context 'show' do
+    let(:url) { "/admin/resources/users/#{user.id}" }
+
+    describe 'without a related post' do
+      it 'attaches and detaches a post' do
+        visit url
+        expect(page).to have_text 'Attach Main post'
+
+        click_on 'Attach Main post'
+        wait_for_loaded
+        expect(page).to have_text 'Choose post'
+
+        expect(page).to have_select 'fields_related_id', selected: "Choose an option"
+        select post.name, from: 'fields_related_id'
+
+        click_on 'Attach'
+        wait_for_loaded
+
+        expect(page).to have_text 'Post attached.'
+        expect(page).not_to have_text 'Choose post'
+        expect(page).to have_text post.name
+
+        expect(user.posts.pluck('id')).to include post.id
+
+        expect(page).to have_text 'Main post'
+        expect(page).to have_text 'Detach main post'
+
+        accept_alert do
+          click_on 'Detach main post'
+        end
+        wait_for_loaded
+
+        expect(page).to have_text 'Post detached.'
+        expect(page).not_to have_text 'Detach main post'
+        expect(page).to have_text 'Attach Main post'
+        expect(user.posts.pluck('id')).not_to include post.id
+      end
+    end
+  end
+end
