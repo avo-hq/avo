0a9c5cd4bde4ca03b8f772abdf1b1c9a48373698
Paul Bob <paul.ionut.bob@gmail.com>
2022-05-28 12:03:42 +0100

    undo course_resource.rb

--- spec/dummy/app/avo/resources/course_resource.rb
+++ spec/dummy/app/avo/resources/course_resource.rb
@@ -12,7 +12,7 @@ class CourseResource < Avo::BaseResource
   field :country, as: :select, options: Course.countries.map { |country| [country, country] }.to_h
   field :city, as: :select, options: Course.cities.values.flatten.map { |city| [city, city] }.to_h
   field :links, as: :has_many, searchable: true, placeholder: "Click to choose a link"
-  
+
   filter CourseCountryFilter
   filter CourseCityFilter
 end
