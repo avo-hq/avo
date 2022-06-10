6fcb1d1442d3c685b5f7336dc78579be493220c9
Adrian Marin <adrian@adrianthedev.com>
2022-06-11 00:39:54 +0300

    fix: use resource name for url instead of model name

--- spec/dummy/app/avo/resources/membership_resource.rb
+++ spec/dummy/app/avo/resources/membership_resource.rb
@@ -1,4 +1,4 @@
-class TeamMembershipResource < Avo::BaseResource
+class MembershipResource < Avo::BaseResource
   self.title = :name
   self.includes = [:user, :team]
   self.visible_on_sidebar = false
@@ -6,9 +6,11 @@ class TeamMembershipResource < Avo::BaseResource
     scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   end
   self.hide_from_global_search = true
+  self.model_class = ::TeamMembership
 
   field :id, as: :id
   field :id, as: :number, only_on: :edit
+
   field :level, as: :select, as_description: true, options: ->(**args) { {Beginner: :beginner, Intermediate: :intermediate, Advanced: :advanced, "#{args[:model].id}": "model_id", "#{args[:resource].name}": "resource_name", "#{args[:view]}": "view", "#{args[:field].id}": "field"} }, display_value: true, default: -> { Time.now.hour < 12 ? "advanced" : "beginner" }
 
   field :user, as: :belongs_to, searchable: false, attach_scope: -> {
