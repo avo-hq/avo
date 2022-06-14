5b2fafca17bbf7cc6afb61b6c0f418b0852006ed
Adrian Marin <adrian@adrianthedev.com>
2022-06-14 12:56:49 +0300

    wip

--- app/components/avo/views/resource_edit_component.rb
+++ app/components/avo/views/resource_edit_component.rb
@@ -4,9 +4,11 @@ class Avo::Views::ResourceEditComponent < Avo::ResourceComponent
   include Avo::ResourcesHelper
   include Avo::ApplicationHelper
 
-  def initialize(resource: nil)
+  attr_reader :view
+
+  def initialize(resource: nil, view: :edit)
     @resource = resource
-    @view = :edit
+    @view = view
 
     split_panel_fields
   end
@@ -17,7 +19,11 @@ class Avo::Views::ResourceEditComponent < Avo::ResourceComponent
     elsif via_index?
       helpers.resources_path(resource: @resource)
     else # via resource show page
-      helpers.resource_path(model: @resource.model, resource: @resource)
+      if is_edit?
+        helpers.resource_path(model: @resource.model, resource: @resource)
+      else
+        helpers.resources_path(resource: @resource)
+      end
     end
   end
 
@@ -25,6 +31,26 @@ class Avo::Views::ResourceEditComponent < Avo::ResourceComponent
   # The update? method should be called only when the user clicks the Save button so the developer gets access to the params from the form.
   def can_see_the_save_button?
     @resource.authorization.authorize_action :edit, raise_exception: false
+  end
+
+  def is_edit?
+    view == :edit
+  end
+
+  def form_url
+    if is_edit?
+      helpers.resource_path(
+        model: @resource.model,
+        resource: @resource
+      )
+    else
+      helpers.resources_path(
+        resource: @resource,
+        via_relation_class: params[:via_relation_class],
+        via_relation: params[:via_relation],
+        via_resource_id: params[:via_resource_id]
+      )
+    end
   end
 
   private
