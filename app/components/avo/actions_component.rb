8401b6d27496a571d7e49195e57a83e306db258f
Adrian Marin <adrian@adrianthedev.com>
2022-06-20 13:29:45 +0300

    feature: show actions on edit view

--- app/components/avo/actions_component.rb
+++ app/components/avo/actions_component.rb
@@ -3,12 +3,50 @@
 class Avo::ActionsComponent < ViewComponent::Base
   include Avo::ApplicationHelper
 
-  def initialize(actions: [], resource: nil)
+  def initialize(actions: [], resource: nil, view: nil)
     @actions = actions
     @resource = resource
+    @view = view
   end
 
   def render?
     @actions.present?
+  end
+
+  # When running an action for one record we should do it on a special path.
+  # We do that so we get the `model` param inside the action so we can prefill fields.
+  def action_path(id)
+    return many_records_path(id) unless @resource.has_model_id?
+
+    if on_record_page?
+      single_record_path id
+    else
+      many_records_path id
+    end
+  end
+
+  # How should the action be displayed by default
+  def is_disabled?(action)
+    return false if action.standalone
+
+    on_index_page?
+  end
+
+  private
+
+  def on_record_page?
+    @view.in?([:show, :edit, :new])
+  end
+
+  def on_index_page?
+    !on_record_page?
+  end
+
+  def single_record_path(id)
+    "#{@resource.record_path}/actions/#{id}"
+  end
+
+  def many_records_path(id)
+    "#{@resource.records_path}/actions/#{id}"
   end
 end
