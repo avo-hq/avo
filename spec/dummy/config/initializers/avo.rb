0d1dd1a3742d9c1d7fd3f78be4e256e44344cf5e
Adrian <adrian@adrianthedev.com>
2022-07-22 17:06:08 +0300

    revert path config

--- spec/dummy/config/initializers/avo.rb
+++ spec/dummy/config/initializers/avo.rb
@@ -14,7 +14,6 @@ Avo.configure do |config|
     }
   end
   config.home_path = -> { avo.dashboard_path(:dashy) }
-  config.home_path = -> { avo.resources_teams_path }
   config.set_initial_breadcrumbs do
     add_breadcrumb "Dashboard", "/admin/dashboards/dashy"
   end
