255a6dadef3df5eb13c6b9a7186007d2df7e84f6
Paul Bob <paul.ionut.bob@gmail.com>
2022-07-26 23:14:03 +0100

    can hide pagination select when only 1 page and choose to show/hide counter

--- lib/avo/fields/has_many_field.rb
+++ lib/avo/fields/has_many_field.rb
@@ -1,9 +1,14 @@
 module Avo
   module Fields
     class HasManyField < HasBaseField
+      attr_accessor :hide_per_page_options_on_few_items
+      attr_accessor :hide_per_page_counter
+
       def initialize(id, **args, &block)
         args[:updatable] = false
 
+        @hide_per_page_options_on_few_items = args[:hide_per_page_options_on_few_items] || false
+        @hide_per_page_counter = args[:hide_per_page_counter] || false
         hide_on :all
         show_on :show
 
