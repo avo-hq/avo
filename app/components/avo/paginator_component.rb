219fc1c5aa1a9f457b7136e1f5394a6e1620984e
Paul Bob <paul.ionut.bob@gmail.com>
2022-07-27 22:51:16 +0100

    pass discreet pagination instead field to paginator component

--- app/components/avo/paginator_component.rb
+++ app/components/avo/paginator_component.rb
@@ -6,14 +6,14 @@ class Avo::PaginatorComponent < ViewComponent::Base
   attr_reader :index_params
   attr_reader :resource
   attr_reader :parent_model
-  attr_reader :field
+  attr_reader :discreet_pagination
 
-  def initialize(resource: nil, parent_model: nil, pagy: nil, turbo_frame: nil, index_params: nil, field: nil)
+  def initialize(resource: nil, parent_model: nil, pagy: nil, turbo_frame: nil, index_params: nil, discreet_pagination: nil)
     @pagy = pagy
     @turbo_frame = turbo_frame
     @index_params = index_params
     @resource = resource
     @parent_model = parent_model
-    @field = field
+    @discreet_pagination = discreet_pagination
   end
 end
