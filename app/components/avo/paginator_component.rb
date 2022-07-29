4ffd73b15460b05f4c8146841f35a5585520b1f7
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2022-07-29 23:26:22 +0100

    improvement/hide_pagination_when_few_items (#1106)

--- app/components/avo/paginator_component.rb
+++ app/components/avo/paginator_component.rb
@@ -6,12 +6,14 @@ class Avo::PaginatorComponent < ViewComponent::Base
   attr_reader :index_params
   attr_reader :resource
   attr_reader :parent_model
+  attr_reader :discreet_pagination
 
-  def initialize(resource: nil, parent_model: nil, pagy: nil, turbo_frame: nil, index_params: nil)
+  def initialize(resource: nil, parent_model: nil, pagy: nil, turbo_frame: nil, index_params: nil, discreet_pagination: nil)
     @pagy = pagy
     @turbo_frame = turbo_frame
     @index_params = index_params
     @resource = resource
     @parent_model = parent_model
+    @discreet_pagination = discreet_pagination
   end
 end
