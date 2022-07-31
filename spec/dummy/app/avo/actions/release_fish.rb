f6a769e16cc8e01841e12d38f3ce34280c6ace3c
Adrian <adrian@adrianthedev.com>
2022-07-31 23:00:18 +0300

    lint

--- spec/dummy/app/avo/actions/release_fish.rb
+++ spec/dummy/app/avo/actions/release_fish.rb
@@ -3,9 +3,7 @@ class ReleaseFish < Avo::BaseAction
   self.message = "Are you sure you want to release this fish?"
 
   def handle(**args)
-    models, fields, current_user, resource = args.values_at(:models, :fields, :current_user, :resource)
-
-    models.each do |model|
+    args[:models].each do |model|
       model.release
     end
   end
