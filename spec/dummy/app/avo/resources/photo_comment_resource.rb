3ffa5b7cb64295dce01abb5c3a8434e0fb9b60d5
Paul Bob <paul.ionut.bob@gmail.com>
2022-08-02 02:41:21 +0100

    add user to photo comments

--- spec/dummy/app/avo/resources/photo_comment_resource.rb
+++ spec/dummy/app/avo/resources/photo_comment_resource.rb
@@ -25,8 +25,8 @@ class PhotoCommentResource < Avo::BaseResource
     end
   end
   field :tiny_name, as: :text, only_on: :index, as_description: true
-
   field :photo, as: :file, is_image: true, as_avatar: :rounded, full_width: true, hide_on: [], accept: "image/*"
+  field :user, as: :belongs_to
 
   field :commentable_type, as: :hidden, default: "Post"
   field :commentable_id, as: :hidden, default: -> { Avo::App.params[:via_resource_id] }
