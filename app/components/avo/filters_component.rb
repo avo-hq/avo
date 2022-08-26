d2742284d23def5bfab6589832ab58d12919ad54
Paul Bob <paul.ionut.bob@gmail.com>
2022-08-26 22:57:13 +0100

    fix

--- app/components/avo/filters_component.rb
+++ app/components/avo/filters_component.rb
@@ -3,13 +3,24 @@
 class Avo::FiltersComponent < ViewComponent::Base
   include Avo::ApplicationHelper
 
-  def initialize(filters: [], resource: nil, applied_filters: [])
+  def initialize(filters: [], resource: nil, applied_filters: [], parent_model: nil, parent_resource: nil)
     @filters = filters
     @resource = resource
     @applied_filters = applied_filters
+    @parent_model = parent_model
+    @parent_resource = parent_resource
   end
 
   def render?
     @filters.present?
+  end
+
+  def reset_path
+    # If come from a association page
+    if @parent_model.present? && @parent_resource.present?
+      helpers.related_resources_path(@parent_model, @parent_model, filters: nil, keep_query_params: true)
+    else
+      helpers.resources_path(resource: @resource, filters: nil, keep_query_params: true)
+    end
   end
 end
