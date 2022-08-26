c2ad7b5da7932cb4cd9abba3fd216386d83980bb
Adrian Marin <adrian@adrianthedev.com>
2022-08-26 10:00:01 +0300

    chore: add testing for the release fish action (#1169)

---
+++ spec/features/avo/release_fish_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature ReleaseFish, type: :feature do
+  let(:fish) { create :fish }
+  let(:current_user) { admin }
+  let(:resource) { UserResource.new.hydrate model: fish }
+
+  it "tests the dummy action" do
+    args = {
+      fields: {
+        message: "Bye fishy!"
+      },
+      current_user: current_user,
+      resource: resource,
+      models: [fish]
+    }
+
+    action = described_class.new(model: fish, resource: resource, user: current_user, view: :edit)
+
+    expect(action).to receive(:succeed).with "1 fish released with message 'Bye fishy!'."
+    expect(fish).to receive(:release)
+
+    action.handle(**args)
+  end
+end
