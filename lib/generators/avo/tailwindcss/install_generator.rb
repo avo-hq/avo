4b00ef99060068ef821b37925b5c57435d334679
Adrian <adrian@adrianthedev.com>
2022-08-31 11:49:39 +0300

    feature: automate tailwind installation to Avo

---
+++ lib/generators/avo/tailwindcss/install_generator.rb
@@ -0,0 +1,45 @@
+require_relative "../base_generator"
+
+module Generators
+  module Avo
+    module Tailwindcss
+      class InstallGenerator < BaseGenerator
+        source_root File.expand_path("../templates", __dir__)
+
+        namespace "avo:tailwindcss:install"
+        desc "Add Tailwindcss to your Avo project."
+
+        def create_files
+          unless Rails.root.join("app/assets/stylesheets/avo.tailwind.css").exist?
+            say "Add default app/assets/stylesheets/avo.tailwind.css"
+            copy_file template_path("avo.tailwind.css"), "app/assets/stylesheets/avo.tailwind.css"
+          end
+
+          if Rails.root.join("Procfile.dev").exist?
+            append_to_file "Procfile.dev", "css: bin/rails avo:tailwindcss:watch\n"
+          else
+            say "Add default Procfile.dev"
+            copy_file template_path("Procfile.dev"), "Procfile.dev"
+
+            say "Ensure foreman is installed"
+            run "gem install foreman"
+          end
+
+          append_to_file "Procfile.dev", "css: bin/rails tailwindcss:watch\n"
+
+          say "Ejecting the _head.html.erb partial"
+          Rails::Generators.invoke("avo:eject", [":head", "--no-avo-version"], {destination_root: Rails.root})
+
+          say "Adding the CSS asset to the partial"
+          prepend_to_file Rails.root.join("app", "views", "avo", "partials", "_head.html.erb"), "<%= stylesheet_link_tag \"avo.tailwind.css\", media: \"all\" %>"
+        end
+
+        no_tasks do
+          def template_path(filename)
+            Pathname.new(__dir__).join("..", "templates", "tailwindcss", filename).to_s
+          end
+        end
+      end
+    end
+  end
+end
