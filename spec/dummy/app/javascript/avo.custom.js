67e9dd35e18d62e4c6ee536a0589ffe84d013d7c
Adrian <adrian@adrianthedev.com>
2022-09-01 13:16:57 +0300

    chore: minor assets refactor

--- spec/dummy/app/javascript/avo.custom.js
+++ spec/dummy/app/javascript/avo.custom.js
@@ -1,8 +1,13 @@
 import { Application } from '@hotwired/stimulus'
 import CourseResourceController from './avo_custom/course_resource_controller'
 
-const application = Application.start()
+// const application = Application.start()
+console.log('window.Stimulus->', window.Stimulus)
+const application = window.Stimulus
 
 // Configure Stimulus development experience
 application.debug = window?.localStorage.getItem('avo.debug')
 application.register('course-resource', CourseResourceController)
+
+// eslint-disable-next-line no-console
+console.log('Hi from Avo custom JS 👋')
