5dc61c5b288efd3f578e820bd0bda8b27a68e093
Oleksandr Yakovlyev <olex@pop-os.localdomain>
2022-09-13 15:49:34 +0100

    add date-time filter

---
+++ app/javascript/js/controllers/date_time_filter_controller.js
@@ -0,0 +1,17 @@
+import BaseFilterController from './filter_controller'
+
+export default class extends BaseFilterController {
+  static targets = ['startDate', 'endDate']
+  static values = { class: String }
+
+  getFilterValue() {
+    return {
+      start: this.startDateTarget.value,
+      end: this.endDateTarget.value
+    }
+  }
+
+  getFilterClass() {
+    return this.classValue
+  }
+}
