7c516301cd47b067cead0cf9f38a194dc791d0e5
Paul Bob <paul.ionut.bob@gmail.com>
2022-09-15 07:55:25 +0100

    i18n

--- app/avo/actions/pre_made_action_batch_delete.rb
+++ app/avo/actions/pre_made_action_batch_delete.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Actions
   class PreMadeActionBatchDelete < Avo::BaseAction
     self.name = I18n.t('avo.delete').capitalize
@@ -5,14 +7,17 @@ module Actions
     self.confirm_button_label = I18n.t('avo.delete').capitalize
 
     def handle(**args)
-      models, _, _, _ = args.values_at(:models, :fields, :current_user, :resource)
+      models, = args.values_at(:models, :fields, :current_user, :resource)
 
       models.each(&:destroy!)
 
-      silent
-
-    rescue => exception
-      fail exception
+      inform I18n.t(
+        'avo.resource_batch_destroyed',
+        records: I18n.t('avo.record', count: models.size),
+        destroyed: I18n.t('avo.destroyed', count: models.size)
+      )
+    rescue StandardError => e
+      fail e
     end
   end
 end
