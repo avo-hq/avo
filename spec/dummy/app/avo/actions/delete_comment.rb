da417dbf62871a03e6220324bcff193f288502d0
Adrian <adrian@adrianthedev.com>
2022-09-27 10:26:17 +0300

    chore: linters

--- spec/dummy/app/avo/actions/delete_comment.rb
+++ spec/dummy/app/avo/actions/delete_comment.rb
@@ -1,6 +1,7 @@
 class DeleteComment < Avo::BaseAction
   self.name = 'Delete'
 
+
   def handle(**args)
     models, _fields, _current_user, _resource = args.values_at(:models, :fields, :current_user, :resource)
 
