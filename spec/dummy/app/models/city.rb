0e9f2317aefe269dd8d10582ba44623b04ad5d41
Adrian <adrian@adrianthedev.com>
2022-10-02 13:09:16 +0300

    wip

--- spec/dummy/app/models/city.rb
+++ spec/dummy/app/models/city.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id               :bigint           not null, primary key
+#  name             :string
+#  population       :integer
+#  is_capital       :boolean
+#  features         :json
+#  metadata         :json
+#  image_url        :string
+#  description      :text
+#  status           :string
+#  tiny_description :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class City < ApplicationRecord
   enum status: {Open: "open", Closed: "closed"}
 
--- spec/dummy/app/models/product.rb
+++ spec/dummy/app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  price       :integer
+#  status      :string
+#  category    :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
   enum category: [
     "Music players",
