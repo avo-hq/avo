1aa31ee3dfd3a1e0fa7e44481bb0ed819d5d9bb1
Paul Bob <paul.ionut.bob@gmail.com>
2022-10-07 21:48:16 +0100

    --without_resource option

--- lib/generators/model_generator.rb
+++ lib/generators/model_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators/rails/model/model_generator'
 module Rails
   module Generators
     class ModelGenerator
-      hook_for :avo_resource, type: :boolean, default: true
+      hook_for :avo_resource, type: :boolean, default: true unless ARGV.include?("--without_resource")
     end
   end
 end
