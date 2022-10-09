b0e61a297587b53b5eb61fd91478793b704eca72
Adrian <adrian@adrianthedev.com>
2022-10-09 17:36:31 +0300

    chore: minor style tweak

--- lib/avo/reloader.rb
+++ lib/avo/reloader.rb
@@ -20,40 +20,41 @@ class Avo::Reloader
   end
 
   private
-    def updater
-      @updater ||= config.file_watcher.new(files, directories) { reload! }
+
+  def updater
+    @updater ||= config.file_watcher.new(files, directories) { reload! }
+  end
+
+  def files
+    # we want to watch some files no matter what
+    paths = [
+      Rails.root.join("config", "initializers", "avo.rb"),
+    ]
+
+    # we want to watch some files only in Avo development
+    if reload_lib?
+      paths += []
     end
 
-    def files
-      # we want to watch some files no matter what
-      paths = [
-        Rails.root.join("config", "initializers", "avo.rb"),
-      ]
+    paths
+  end
 
-      # we want to watch some files only in Avo development
-      if reload_lib?
-        paths += []
-      end
+  def directories
+    dirs = {}
 
-      paths
+    # watch the lib directory in Avo development
+    if reload_lib?
+      dirs[Avo::Engine.root.join("lib", "avo").to_s] = ["rb"]
     end
 
-    def directories
-      dirs = {}
+    dirs
+  end
 
-      # watch the lib directory in Avo development
-      if reload_lib?
-        dirs[Avo::Engine.root.join("lib", "avo").to_s] = ["rb"]
-      end
+  def config
+    Rails.application.config
+  end
 
-      dirs
-    end
-
-    def config
-      Rails.application.config
-    end
-
-    def reload_lib?
-      Avo::IN_DEVELOPMENT || ENV['AVO_RELOAD_LIB_DIR']
-    end
+  def reload_lib?
+    Avo::IN_DEVELOPMENT || ENV["AVO_RELOAD_LIB_DIR"]
+  end
 end
