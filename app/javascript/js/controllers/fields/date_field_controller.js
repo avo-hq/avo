feb2c155c5827798b7817ec479b35073b72fc643
Adrian <adrian@adrianthedev.com>
2022-10-20 12:57:56 +0300

    stop execution if the initial value is empty

--- app/javascript/js/controllers/fields/date_field_controller.js
+++ app/javascript/js/controllers/fields/date_field_controller.js
@@ -136,18 +136,25 @@ export default class extends Controller {
     // Hide calendar and only keep time picker.
     options.noCalendar = this.noCalendarValue
 
-    // Enable timezone display
-    if (this.enableTimeValue && this.relativeValue) {
-      options.defaultDate = this.parsedValue.setZone(this.displayTimezone).toISO()
+    if (this.initialValue) {
+      // Enable timezone display
+      if (this.enableTimeValue && this.relativeValue) {
+        options.defaultDate = this.parsedValue.setZone(this.displayTimezone).toISO()
 
-      options.dateFormat = 'Y-m-d H:i:S'
-    } else {
-      // Because the browser treats the date like a timestamp and updates it at 00:00 hour, when on a western timezone the date will be converted with one day offset.
-      // Ex: 2022-01-30 will render as 2022-01-29 on an American timezone
-      options.defaultDate = universalTimestamp(this.initialValue)
+        options.dateFormat = 'Y-m-d H:i:S'
+      } else {
+        // Because the browser treats the date like a timestamp and updates it at 00:00 hour, when on a western timezone the date will be converted with one day offset.
+        // Ex: 2022-01-30 will render as 2022-01-29 on an American timezone
+        options.defaultDate = universalTimestamp(this.initialValue)
+      }
     }
 
     this.flatpickrInstance = flatpickr(this.fakeInputTarget, options)
+
+    // Don't try to parse the value if the input is empty.
+    if (!this.initialValue) {
+      return
+    }
 
     let value
     switch (this.fieldTypeValue) {
