4a60a36c86658160c5915f7462525adeba001a4c
Paul Bob <paul.ionut.bob@gmail.com>
2022-10-29 17:45:37 +0100

    code for manual test

--- spec/dummy/app/avo/resources/spouse_resource.rb
+++ spec/dummy/app/avo/resources/spouse_resource.rb
@@ -9,4 +9,34 @@ class SpouseResource < Avo::BaseResource
 
   field :id, as: :id
   field :name, as: :text
+
+  # Uncomment this to test visibility inside tabs, panels and sidebar
+  # tabs do
+  #   field :hidden_field_inside_tabs, as: :text, visible: -> (resource:) { false }
+
+  #   tab "hidden testing visibility inside tabs" do
+  #     field :hidden_field_inside_tabs_inside_tab, as: :text, visible: -> (resource:) { false }
+
+  #     panel do
+  #       field :hidden_field_inside_tabs_inside_tab_inside_panel, as: :text, visible: -> (resource:) { false }
+  #     end
+  #   end
+  # end
+
+  # sidebar do 
+  #   field :hidden_field_inside_sidebar, as: :text, visible: -> (resource:) { false }
+  #   field :field_inside_sidebar, as: :text
+  # end
+
+  # tabs do
+  #   field :test_field_inside_tabs, as: :text
+
+  #   tab "testing visibility inside tabs" do
+  #     field :test_field_inside_tabs_inside_tab, as: :text
+
+  #     panel do
+  #       field :test_field_inside_tabs_inside_tab_inside_panel, as: :text
+  #     end
+  #   end
+  # end
 end
