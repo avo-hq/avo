fb21f385fa51135a457dc99884f073a2fba71780
Adrian <adrian@adrianthedev.com>
2022-10-30 23:22:17 +0200

    refactored spec fields

--- spec/dummy/app/avo/resources/spouse_resource.rb
+++ spec/dummy/app/avo/resources/spouse_resource.rb
@@ -9,33 +9,4 @@ class SpouseResource < Avo::BaseResource
 
   field :id, as: :id
   field :name, as: :text
-
-  # For RSpec tabs_panels_and_sidebar_visibility_spec.rb
-  tabs do
-    field :hidden_field_inside_tabs, as: :text, visible: -> (resource:) {
-      name = self.items_holder.items.first.items_holder.items.first.items.first.resource.model.name
-      name == "RSpec TabsPanelAndSidebarVisibility"
-    }
-
-    tab "Hidden tab inside tabs" do
-      field :hidden_field_inside_tabs_inside_tab, as: :text, visible: -> (resource:) {
-        name = self.items_holder.items.first.resource.model.name
-        name == "RSpec TabsPanelAndSidebarVisibility"
-      }
-
-      panel do
-        field :hidden_field_inside_tabs_inside_tab_inside_panel, as: :text, visible: -> (resource:) {
-          name = self.items_holder.items.first.resource.model.name
-          name == "RSpec TabsPanelAndSidebarVisibility"
-        }
-      end
-    end
-  end
-
-  sidebar do
-    field :hidden_field_inside_sidebar, as: :text, visible: -> (resource:) {
-      name = self.items.first.resource.model.name
-      name == "RSpec TabsPanelAndSidebarVisibility"
-    }
-  end
 end
