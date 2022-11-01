27833c14df03d69b6a3ba6ed5370f820f35121f2
Nathan Kriege <nkriege@gmail.com>
2022-11-01 17:24:08 -0500

    comment

--- app/javascript/js/controllers/search_controller.js
+++ app/javascript/js/controllers/search_controller.js
@@ -110,6 +110,8 @@ export default class extends Controller {
   }
 
   disconnect() {
+    // Don't leave open autocompletes around when disconnected. Otherwise it will still
+    // be visible when navigating back to this page.
     if (this.destroyMethod) {
       this.destroyMethod();
       this.destroyMethod = nil;
