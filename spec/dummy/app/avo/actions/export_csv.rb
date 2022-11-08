85bbfcfe81252e6c8333d07370605d7ba4a6a27a
Adrian <adrian@adrianthedev.com>
2022-11-08 12:33:31 +0200

    minor tweaks

--- spec/dummy/app/avo/actions/export_csv.rb
+++ spec/dummy/app/avo/actions/export_csv.rb
@@ -1,20 +1,23 @@
 class ExportCsv < Avo::BaseAction
-  self.name = "Export csv"
-  self.no_confirmation = false
+  self.name = "Export CSV"
   self.may_download_file = true
 
-  def handle(**args)
-    models, resource = args.values_at(:models, :resource)
+  # Add more fields here for custo user-selected columns
+  field :id, as: :boolean
+  field :created_at, as: :boolean
+
+  def handle(models:, resource:, fields:, **)
+    columns = models.first.class.columns_hash.keys
+    # Uncomment below to use the user-selected fields
+    # columns = get_columns_from_fields(fields)
 
     return fail "No record selected" if models.blank?
 
-    attributes = get_attributes models.first
-
     file = CSV.generate(headers: true) do |csv|
-      csv << attributes
+      csv << columns
 
       models.each do |record|
-        csv << attributes.map do |attr|
+        csv << columns.map do |attr|
           record.send(attr)
         end
       end
@@ -23,9 +26,7 @@ class ExportCsv < Avo::BaseAction
     download file, "#{resource.plural_name}.csv"
   end
 
-  def get_attributes(record)
-    # return ["id", "created_at"] # uncomment this and fill in for custom model properties
-
-    record.class.columns_hash.keys
+  def get_columns_from_fields(fields)
+    fields.select { |key, value| value }.keys
   end
 end
