d504fff07332f38128ca2e92119eec8edc989d9c
ali-haider-eqt <ali@equipetechnique.com>
2022-11-12 01:55:40 +0500

    editor menu expandable on whole link bar

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -213,6 +213,13 @@ ActiveRecord::Schema.define(version: 2022_10_11_150126) do
     t.string "color"
   end
 
+  create_table "types", force: :cascade do |t|
+    t.string "title"
+    t.bigint "fish_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "first_name"
