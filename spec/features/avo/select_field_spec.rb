9e6ee5b231fc0530c89ae95cae9edc7560b2be54
Paul Bob <paul.ionut.bob@gmail.com>
2022-11-14 21:06:34 +0000

    select field spec reforced

--- spec/features/avo/select_field_spec.rb
+++ spec/features/avo/select_field_spec.rb
@@ -35,4 +35,44 @@ RSpec.feature "Selects", type: :feature do
       end
     end
   end
+
+  describe "when options are enum hash" do
+    context "create and show" do
+      it "show key but save value" do
+        CityResource.with_temporary_items do
+          field :population, as: :select, options: {
+            'Zero': 0,
+            'Low': 50000,
+            'Medium': 100000,
+            'High': 250000,
+          }
+        end
+
+        visit avo.new_resources_city_path
+
+        expect(page).to have_select "city_population", selected: nil, options: ["Zero", "Low", "Medium", "High"]
+
+        select "Medium", from: "city_population"
+        save
+        expect(page).to have_text "Medium"
+        expect(City.last.population).to eq 100000
+      end
+    end
+  end
+
+  describe "when options are enum array" do
+    context "create and show" do
+      it "show key but save value" do
+        visit avo.new_resources_post_path
+
+        expect(page).to have_select "post_status", selected: "draft", options: ["draft", "published", "archived"]
+
+        fill_in "post_name", with: "Published post =)"
+        select "published", from: "post_status"
+        save
+        expect(page).to have_text "published"
+        expect(Post.last.status).to eq "published"
+      end
+    end
+  end
 end
