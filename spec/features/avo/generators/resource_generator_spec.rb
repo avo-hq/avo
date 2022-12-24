a145b67207b168a7ebb681472de342eaf0c6b15f
Alex Dent <aldent95@hotmail.com>
2022-12-24 14:00:32 +1300

    Adding new test to cover bug

--- spec/features/avo/generators/resource_generator_spec.rb
+++ spec/features/avo/generators/resource_generator_spec.rb
@@ -12,4 +12,21 @@ RSpec.feature "resource generator", type: :feature do
 
     check_files_and_clean_up files
   end
+
+  context 'when generating resources from a DB model' do
+    it 'generates with the correct date_time field type' do
+      files = [
+        Rails.root.join("app", "avo", "resources", "event_resource.rb").to_s,
+        Rails.root.join("app", "controllers", "avo", "events_controller.rb").to_s
+      ]
+
+      Rails::Generators.invoke("avo:resource", ["event", "-q"], {destination_root: Rails.root})
+
+      expect(File.read(files[0])).to include('field :event_time, as: :date_time')
+
+      check_files_and_clean_up files
+    end
+  end
+
+
 end
