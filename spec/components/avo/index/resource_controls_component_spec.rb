2d4c0e30f4d834db19fabaef9a33efb60284e1ba
Jeremias Ramirez <jeremias@external.twentyeighthealth.com>
2023-02-10 10:23:25 -0300

    feat(can_reorder?): move logic and adds basic test

--- spec/components/avo/index/resource_controls_component_spec.rb
+++ spec/components/avo/index/resource_controls_component_spec.rb
@@ -1,12 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Avo::Index::ResourceControlsComponent, type: :component do
+  describe "#can_reorder?" do
+    context "when @reflection is present" do
+      it "returns the result of authorize_association_for" do
+        component = described_class.new(reflection: true)
+        allow(component).to receive(:authorize_association_for).with(:reorder).and_return(true)
+        expect(component.can_reorder?).to eq(true)
+      end
+    end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+    context "when @reflection is not present" do
+      it "returns the result of authorize_action" do
+        authorization = double(:authorization, authorize_action: true)
+        component = described_class.new(resource: double(:resource, authorization: authorization))
+        expect(component.can_reorder?).to eq(true)
+      end
+    end
+  end
 end
