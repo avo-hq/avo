a40823bf44ecb0c3b30a0649bd6dfd23ad92b311
Adrian <adrian@adrianthedev.com>
2023-02-20 12:27:28 +0200

    wip

--- spec/features/avo/generators/locales_generator_spec.rb
+++ spec/features/avo/generators/locales_generator_spec.rb
@@ -9,6 +9,10 @@ RSpec.feature "locales generator", type: :feature do
       Rails.root.join("config", "locales", "avo.#{locale}.yml").to_s
     end
 
+    ["nn", "ro"].each do |locale|
+      files << Rails.root.join("config", "locales", "pagy", "#{locale}.yml").to_s
+    end
+
     Rails::Generators.invoke("avo:locales", ["-q"], {destination_root: Rails.root})
 
     check_files_and_clean_up files
