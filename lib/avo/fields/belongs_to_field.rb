7c5e45d05892471c337bf5e21927db82b999ca3d
Paul Bob <paul.ionut.bob@gmail.com>
2023-03-11 11:58:14 +0000

    use resource on belongs to

--- lib/avo/fields/belongs_to_field.rb
+++ lib/avo/fields/belongs_to_field.rb
@@ -57,6 +57,8 @@ module Avo
     # - is_disabled?
 
     class BelongsToField < BaseField
+      include Avo::Fields::Concerns::UseResource
+
       attr_accessor :target
 
       attr_reader :polymorphic_as
@@ -79,6 +81,7 @@ module Avo
         @attach_scope = args[:attach_scope]
         @polymorphic_help = args[:polymorphic_help]
         @target = args[:target]
+        @use_resource = args[:use_resource] || nil
       end
 
       def searchable
@@ -222,6 +225,8 @@ module Avo
       end
 
       def target_resource
+        return use_resource if use_resource.present?
+
         if is_polymorphic?
           if value.present?
             return App.get_resource_by_model_name(value.class)
