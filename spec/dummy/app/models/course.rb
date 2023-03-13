a951257ec7244b285a9761a5637a13f5b1ffc61c
Paul Lemus <paullemus@protonmail.com>
2023-03-13 11:39:17 -0700

    chore: ran annotate on all dummy models

--- spec/dummy/app/models/course.rb
+++ spec/dummy/app/models/course.rb
@@ -2,14 +2,14 @@
 #
 # Table name: courses
 #
-#  id           :bigint           not null, primary key
-#  name         :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  skills       :text             default([]), is an Array
-#  country      :string
-#  city         :string
-#  starting_at  :date_time
+#  id          :bigint           not null, primary key
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  skills      :text             default([]), is an Array
+#  country     :string
+#  city        :string
+#  starting_at :time
 #
 class Course < ApplicationRecord
   has_prefix_id :course
--- spec/dummy/app/models/event.rb
+++ spec/dummy/app/models/event.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  event_time :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Event < ApplicationRecord
 end
