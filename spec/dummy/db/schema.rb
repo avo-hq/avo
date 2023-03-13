9306dd80b43119c82b63017a2c114ff3d7c7c432
Paul Lemus <paullemus@protonmail.com>
2023-03-13 12:59:45 -0700

    fix: moved lat/lng migration to dummy directory

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_13_181556) do
+ActiveRecord::Schema.define(version: 2023_03_13_191256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 2023_03_13_181556) do
     t.text "tiny_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
     t.float "longitude"
+    t.float "latitude"
   end
 
   create_table "comments", force: :cascade do |t|
