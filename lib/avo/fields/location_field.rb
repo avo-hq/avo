3b0b029c3ec12d5d4a4b54ff7dc38e33b4041a9b
Paul Lemus <paullemus@protonmail.com>
2023-03-14 16:06:28 -0700

    refactor: removed vars from location field
    I simplified the location field and removed anything not needed to pass
    the specs.

--- lib/avo/fields/location_field.rb
+++ lib/avo/fields/location_field.rb
@@ -2,15 +2,9 @@
 
 module Avo
   module Fields
-    class LocationField < TextField
-      attr_reader :latitude, :longitude, :coordinates
-
+    class LocationField < BaseField
       def initialize(id, **args, &block)
         super(id, **args, &block)
-
-        @coordinates = args[:coordinates].present? ? args[:coordinates].to_f : nil
-        @latitude = args[:latitude].present? ? args[:latitude].to_f : nil
-        @longitude = args[:longitude].present? ? args[:longitude].to_f : nil
       end
     end
   end
