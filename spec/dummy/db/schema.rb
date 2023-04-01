8575b6ae3803bc4f193457af92d185a7f0859b2c
Paul Lemus <paullemus@protonmail.com>
2023-04-01 16:11:38 -0700

    updated schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_13_191256) do
+ActiveRecord::Schema.define(version: 2023_03_21_133502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
