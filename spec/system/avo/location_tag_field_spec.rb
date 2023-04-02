281b79d0bcdb9b42a915e82714f687705bd9f663
Paul Lemus <paullemus@protonmail.com>
2023-04-01 18:32:06 -0700

    refactor: added appropriate whitespace

--- spec/system/avo/location_tag_field_spec.rb
+++ spec/system/avo/location_tag_field_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe 'LocationTagField', type: :system do
   describe 'without value' do
     let(:city) { create :city, coordinates: [] }
+
     context 'show' do
       it 'shows empty location field' do
         visit "/admin/resources/cities/#{city.id}"
@@ -12,6 +13,7 @@ RSpec.describe 'LocationTagField', type: :system do
         expect(find_field_element('coordinates')).to have_text empty_dash
       end
     end
+
     context 'edit' do
       it "has lat/long fields and placeholders" do
         visit "/admin/resources/cities/#{city.id}/edit"
@@ -26,6 +28,7 @@ RSpec.describe 'LocationTagField', type: :system do
         expect(find_by_id("city_coordinates[longitude]", visible: false)).to have_text("")
         expect(find_by_id("city_coordinates[longitude]", visible: false)[:placeholder]).to have_text("Enter longitude")
       end
+
       it "changes the city coordinates" do
         visit "/admin/resources/cities/#{city.id}/edit"
 
@@ -47,9 +50,11 @@ RSpec.describe 'LocationTagField', type: :system do
   describe 'with regular value' do
     # Eiffel Tower coordinates used
     let!(:city) { create :city, latitude: 48.8584, longitude: 2.2945 }
+
     context "show" do
       it "renders a map" do
         visit "/admin/resources/cities/#{city.id}"
+
         expect(page).to have_css("canvas.mapboxgl-canvas")
       end
     end
