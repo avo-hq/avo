b3b90390e4ac9098bc491bf149b818575d85b559
Jim Kane <fastjames@gmail.com>
2023-04-27 09:39:57 -0500

    Move helper methods into component class
    Since these helper methods are only used within the component, they
    belong in the component class.
    
    Update the coordinates array builder method to skip resources that do
    not have valid coordinates.

--- app/components/avo/index/resource_map_component.rb
+++ app/components/avo/index/resource_map_component.rb
@@ -1,13 +1,83 @@
 # frozen_string_literal: true
 
-class Avo::Index::ResourceMapComponent < ViewComponent::Base
-  def initialize(resources: nil, resource: nil, reflection: nil, parent_model: nil, parent_resource: nil, pagy: nil, query: nil)
-    @resources = resources
-    @resource = resource
-    @reflection = reflection
-    @parent_model = parent_model
-    @parent_resource = parent_resource
-    @pagy = pagy
-    @query = query
+module Avo
+  module Index
+    # Render a map view for a list of resources, where each resource is
+    # expected to have an attribute/attribute set representing its location.
+    class ResourceMapComponent < ViewComponent::Base
+      def initialize(resources: nil,
+                     resource: nil,
+                     reflection: nil,
+                     parent_model: nil,
+                     parent_resource: nil,
+                     pagy: nil,
+                     query: nil)
+        super
+        @resources = resources
+        @resource = resource
+        @reflection = reflection
+        @parent_model = parent_model
+        @parent_resource = parent_resource
+        @pagy = pagy
+        @query = query
+      end
+
+      def map_view_table_layout_class
+        case map_options.dig(:table, :layout)
+        when :left
+          'table-left'
+        when :bottom
+          'table-bottom'
+        when :top
+          'table-top'
+        else
+          'table-right'
+        end
+      end
+
+      def resource_location_markers
+        # If we have no proc and no default location method, don't try to create markers
+        return [] unless resource_mappable?
+
+        @resources.map do |marker_resource|
+          coordinates = marker_proc.call(record: marker_resource.record)
+
+          next unless coordinates[:latitude].present? && coordinates[:longitude].present?
+
+          coordinates
+        end.compact
+      end
+
+      def resource_mapkick_options
+        map_options[:mapkick_options] || {}
+      end
+
+      def render_map_view_table?
+        map_options.dig(:table, :visible)
+      end
+
+      private
+
+      def default_record_marker_proc
+        lambda { |record:|
+          {
+            latitude: record.coordinates.first,
+            longitude: record.coordinates.last
+          }
+        }
+      end
+
+      def map_options
+        @resource.map || {}
+      end
+
+      def marker_proc
+        map_options[:record_marker] || default_record_marker_proc
+      end
+
+      def resource_mappable?
+        map_options[:record_marker].present? || @resources.first.record.respond_to?(:coordinates)
+      end
+    end
   end
 end
--- spec/components/avo/index/resource_map_component_spec.rb
+++ spec/components/avo/index/resource_map_component_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Avo::Index::ResourceMapComponent, type: :component do
   #     "Hello, components!"
   #   )
   # end
+
+  describe '#resource_location_markers' do
+    let(:component) { described_class.new}
+    let(:resource1) { double(record: record1) }
+    let(:record1) { double(coordinates: [1, 2]) }
+    let(:resource2) { double(record: record2) }
+    let(:record2) { double(coordinates: [3, 4]) }
+    let(:resource3) { double(record: record3) }
+    let(:record3) { double(coordinates: nil) }
+    let(:resource4) { double(record: record4) }
+    let(:record4) { double(coordinates: []) }
+
+    it 'returns an array of marker hashes'
+
+    it 'skips resources without coordinates'
+  end
 end
