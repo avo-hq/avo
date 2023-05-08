358c0c8e44b4c81bb9fb1543af01a37a2f9def42
Adrian <adrian@adrianthedev.com>
2023-05-08 15:31:44 +0300

    use 6.0 migration

--- spec/dummy/db/migrate/20230419123600_add_city_center_area_to_cities.rb
+++ spec/dummy/db/migrate/20230419123600_add_city_center_area_to_cities.rb
@@ -1,4 +1,4 @@
-class AddCityCenterAreaToCities < ActiveRecord::Migration[6.1]
+class AddCityCenterAreaToCities < ActiveRecord::Migration[6.0]
   def change
     add_column :cities, :city_center_area, :json
   end
