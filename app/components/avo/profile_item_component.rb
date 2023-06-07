b8c48c04dbf48d244199740f536c5316bcad2c31
Adrian <adrian@adrianthedev.com>
2023-06-07 12:41:50 +0300

    feature: add method and params to link_to method in the profile menu

--- app/components/avo/profile_item_component.rb
+++ app/components/avo/profile_item_component.rb
@@ -6,14 +6,20 @@ class Avo::ProfileItemComponent < ViewComponent::Base
   attr_reader :path
   attr_reader :active
   attr_reader :target
+  attr_reader :method
+  attr_reader :params
+  attr_reader :classes
 
-  def initialize(label: nil, icon: nil, path: nil, active: :inclusive, target: nil, title: nil)
+  def initialize(label: nil, icon: nil, path: nil, active: :inclusive, target: nil, title: nil, method: nil, params: {}, classes: "")
     @label = label
     @icon = icon
     @path = path
     @active = active
     @target = target
     @title = title
+    @method = method
+    @params = params
+    @classes = classes
   end
 
   def title
