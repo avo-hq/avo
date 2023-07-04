e9ffc58ae6224856712ef76029376533a7fcb33e
Paul Bob <paul.ionut.bob@gmail.com>
2023-07-04 21:48:38 +0300

    improve readability (try)

--- app/components/avo/tab_switcher_component.rb
+++ app/components/avo/tab_switcher_component.rb
@@ -63,36 +63,31 @@ class Avo::TabSwitcherComponent < Avo::BaseComponent
   # Because the developer hasn't specified that it should be visible on edit views (with the show_on: :edit option),
   # the field should not be visible in the item switcher either.
   def visible_items
-    tabs.select do |item|
-      visible = true
+    tabs.select do |tab|
+      next false if tab.items.blank?
+      next false if tab.is_field? && !tab.authorized?
 
-      if item.items.blank?
-        visible = false
+      if tab.items.count == 1
+        next false if !single_item_visible?(tab.items.first)
       end
 
-      first_item = item.items.first
-      if item.items.count == 1 && first_item.is_field? && first_item.has_own_panel? && !first_item.visible_on?(view)
-        # Return nil if tab contians a has_many type of fields and it's hidden in current view
-        visible = false
-      end
+      next false if !tab.visible? || !tab.visible_on?(view)
 
-      if item.respond_to?(:visible_on?)
-        visible = item.visible_on? view
-      end
-
-      if item.respond_to?(:visible?)
-        visible = item.visible?
-      end
-
-      if item.respond_to?(:authorized?)
-        visible = item.authorized?
-      end
-
-      if item.items.count == 1 && first_item.is_field? && first_item.has_own_panel? && first_item.respond_to?(:authorized?) && first_item.resource.present?
-        visible = first_item.authorized?
-      end
-
-      visible
+      true
     end
+  end
+
+  private
+
+  def single_item_visible?(item)
+    # Item is visible if is not a field or don't have its own panel
+    return true if !item.is_field? || !item.has_own_panel?
+
+    return false unless item.visible_on?(view)
+
+    # If item is hydrated with the correct resource and is not authorized, it's not visible
+    return false if item.resource.present? && !item.authorized?
+
+    true
   end
 end
