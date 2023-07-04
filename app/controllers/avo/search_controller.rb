a076475228dfc010fccd4ae262c0e37df106da8e
Paul Bob <paul.ionut.bob@gmail.com>
2023-07-04 14:04:15 +0300

    improvement: apply policy scope on has many search query

--- app/controllers/avo/search_controller.rb
+++ app/controllers/avo/search_controller.rb
@@ -112,7 +112,7 @@ module Avo
     # This scope is applied if the search is being performed on a has_many association
     def apply_has_many_scope
       association_name = BaseResource.valid_association_name(parent, params[:via_association_id])
-      scope = parent.send(association_name)
+      scope = policy_scope parent.send(association_name)
 
       Avo::Hosts::SearchScopeHost.new(block: @resource.search_query, params: params, scope: scope).handle
     end
