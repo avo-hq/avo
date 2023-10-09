99a8d2658ded708cbab31b062b587e188a118a2d
Brychu <pbrych@gmail.com>
2023-10-09 13:46:06 +0200

    feature: allow params on resource menu item (#1957)
    * feature: allow params on resource menu item
    
    * feature: apply code review suggestions

--- lib/avo/menu/resource.rb
+++ lib/avo/menu/resource.rb
@@ -3,12 +3,20 @@ class Avo::Menu::Resource < Avo::Menu::BaseItem
 
   option :resource
   option :label, optional: true
+  option :params, default: proc { {} }
 
   def parsed_resource
-    Avo::App.guess_resource resource.to_s
+    @parsed_resource ||= Avo::App.guess_resource resource.to_s
   end
 
   def entity_label
     parsed_resource.navigation_label
+  end
+
+  def fetch_params
+    Avo::ExecutionContext.new(
+      target: params,
+      resource: parsed_resource
+    ).handle
   end
 end
