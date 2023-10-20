7457c4822af89c39ce2ba43c65ad91240a7fd0f8
Brychu <pbrych@gmail.com>
2023-10-20 23:34:03 +0200

    feature: allow suggestions_max_items option on tags field

--- app/javascript/js/controllers/fields/tags_field_controller.js
+++ app/javascript/js/controllers/fields/tags_field_controller.js
@@ -1,126 +1,130 @@
-import { first, isObject, merge } from 'lodash'
-import Tagify from '@yaireo/tagify'
-import URI from 'urijs'
+import { first, isObject, merge } from "lodash";
+import Tagify from "@yaireo/tagify";
+import URI from "urijs";
 
-import { suggestionItemTemplate, tagTemplate } from './tags_field_helpers'
-import BaseController from '../base_controller'
-import debouncePromise from '../../helpers/debounce_promise'
+import { suggestionItemTemplate, tagTemplate } from "./tags_field_helpers";
+import BaseController from "../base_controller";
+import debouncePromise from "../../helpers/debounce_promise";
 
 export default class extends BaseController {
-  static targets = ['input', 'fakeInput'];
+  static targets = ["input", "fakeInput"];
 
   static values = {
     whitelistItems: { type: Array, default: [] },
     disallowedItems: { type: Array, default: [] },
     enforceSuggestions: { type: Boolean, default: false },
+    suggestionsMaxItems: { type: Number, default: 20 },
     closeOnSelect: { type: Boolean, default: false },
     delimiters: { type: Array, default: [] },
     mode: String,
     fetchValuesFrom: String,
-  }
+  };
 
   tagify = null;
 
-  searchDebounce = 500
+  searchDebounce = 500;
 
   debouncedFetch = debouncePromise(fetch, this.searchDebounce);
 
   get suggestionsAreObjects() {
-    return isObject(first(this.whitelistItemsValue)) || this.fetchValuesFromValue
+    return (
+      isObject(first(this.whitelistItemsValue)) || this.fetchValuesFromValue
+    );
   }
 
   get tagifyOptions() {
     let options = {
       whitelist: this.whitelistItemsValue,
       blacklist: this.disallowedItemsValue,
-      enforceWhitelist: this.enforceSuggestionsValue || this.fetchValuesFromValue,
-      delimiters: this.delimitersValue.join('|'),
+      enforceWhitelist:
+        this.enforceSuggestionsValue || this.fetchValuesFromValue,
+      delimiters: this.delimitersValue.join("|"),
       dropdown: {
-        maxItems: 20,
+        maxItems: this.suggestionsMaxItemsValue,
         enabled: 0,
         searchKeys: [this.labelAttributeValue],
         closeOnSelect: this.closeOnSelectValue,
       },
-    }
+    };
 
     if (this.modeValue) {
-      options.mode = this.modeValue // null or "select"
+      options.mode = this.modeValue; // null or "select"
     }
 
     if (this.suggestionsAreObjects) {
       options = merge(options, {
-        tagTextProp: 'label',
+        tagTextProp: "label",
         dropdown: {
-          searchKeys: ['label'],
-          mapValueTo: 'value',
+          searchKeys: ["label"],
+          mapValueTo: "value",
         },
         templates: {
           tag: tagTemplate,
           dropdownItem: suggestionItemTemplate,
         },
-        originalInputValueFormat: (valuesArr) => valuesArr.map((item) => item.value),
-      })
+        originalInputValueFormat: (valuesArr) =>
+          valuesArr.map((item) => item.value),
+      });
     } else {
       options = merge(options, {
-        originalInputValueFormat: (valuesArr) => valuesArr.map((item) => item.value).join(','),
-      })
+        originalInputValueFormat: (valuesArr) =>
+          valuesArr.map((item) => item.value).join(","),
+      });
     }
 
-    return options
+    return options;
   }
 
   connect() {
     if (this.hasInputTarget) {
-      this.hideFakeInput()
-      this.showRealInput()
-      this.initTagify()
+      this.hideFakeInput();
+      this.showRealInput();
+      this.initTagify();
     }
   }
 
   initTagify() {
-    this.tagify = new Tagify(this.inputTarget, this.tagifyOptions)
-    const that = this
+    this.tagify = new Tagify(this.inputTarget, this.tagifyOptions);
+    const that = this;
 
     function onInput(e) {
       // Create the URL from which to fetch the values
-      const query = e.detail.value
-      const uri = new URI(that.fetchValuesFromValue)
+      const query = e.detail.value;
+      const uri = new URI(that.fetchValuesFromValue);
       uri.addSearch({
         q: query,
-      })
+      });
 
       // reset current whitelist
-      that.tagify.whitelist = null
+      that.tagify.whitelist = null;
       // show the loader animation
-      that.tagify.loading(true)
+      that.tagify.loading(true);
 
       // get new whitelist from a request
-      that.fetchResults(uri.toString())
+      that
+        .fetchResults(uri.toString())
         .then((result) => {
-          that.tagify.settings.whitelist = result // add already-existing tags to the new whitelist array
+          that.tagify.settings.whitelist = result; // add already-existing tags to the new whitelist array
 
-          that.tagify
-            .loading(false)
-            .dropdown.show(e.detail.value) // render the suggestions dropdown.
+          that.tagify.loading(false).dropdown.show(e.detail.value); // render the suggestions dropdown.
         })
-        .catch(() => that.tagify.dropdown.hide())
+        .catch(() => that.tagify.dropdown.hide());
     }
 
     if (this.fetchValuesFromValue) {
-      this.tagify.on('input', onInput)
+      this.tagify.on("input", onInput);
     }
   }
 
   fetchResults(endpoint) {
-    return this.debouncedFetch(endpoint)
-      .then((response) => response.json())
+    return this.debouncedFetch(endpoint).then((response) => response.json());
   }
 
   hideFakeInput() {
-    this.fakeInputTarget.classList.add('hidden')
+    this.fakeInputTarget.classList.add("hidden");
   }
 
   showRealInput() {
-    this.inputTarget.classList.remove('hidden')
+    this.inputTarget.classList.remove("hidden");
   }
 }
