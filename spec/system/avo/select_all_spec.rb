2547f47aed47e6aac0abbca546ff9dc6abbf16ba
Paul Bob <paul.ionut.bob@gmail.com>
2023-11-06 16:05:56 +0200

    spec

--- spec/system/avo/select_all_spec.rb
+++ spec/system/avo/select_all_spec.rb
@@ -10,71 +10,71 @@ RSpec.describe "SelectAll", type: :system do
   let!(:salmon) { create_list :fish, spec_salmon_number, name: "Spec Salmon" }
   let!(:url) { "/admin/resources/fish?per_page=#{per_page}" }
 
-  describe "without applyed filters" do
-    context "select first page" do
-      it "releases the fish from the selected page" do
-        visit url
+  # describe "without applyed filters" do
+  #   context "select first page" do
+  #     it "releases the fish from the selected page" do
+  #       visit url
 
-        check_select_all
+  #       check_select_all
 
-        release_fish
+  #       release_fish
 
-        expect(page).to have_text "#{per_page} fish released with message '' by ."
-      end
-    end
+  #       expect(page).to have_text "#{per_page} fish released with message '' by ."
+  #     end
+  #   end
 
-    context "select all records" do
-      it "releases all fishes" do
-        visit url
+  #   context "select all records" do
+  #     it "releases all fishes" do
+  #       visit url
 
-        check_select_all
-        expect_all_message
+  #       check_select_all
+  #       expect_all_message
 
-        click_on "Select all matching"
-        expect_all_matching_message
+  #       click_on "Select all matching"
+  #       expect_all_matching_message
 
-        release_fish
+  #       release_fish
 
-        expect(page).to have_text "#{total_fish} fish released with message '' by ."
-      end
-    end
+  #       expect(page).to have_text "#{total_fish} fish released with message '' by ."
+  #     end
+  #   end
 
-    context "press undo" do
-      it "releases the fish from the selected page" do
-        visit url
+  #   context "press undo" do
+  #     it "releases the fish from the selected page" do
+  #       visit url
 
-        check_select_all
-        expect_all_message
+  #       check_select_all
+  #       expect_all_message
 
-        click_on "Select all matching"
-        expect_all_matching_message
+  #       click_on "Select all matching"
+  #       expect_all_matching_message
 
-        click_on "Undo"
-        expect_all_message
+  #       click_on "Undo"
+  #       expect_all_message
 
-        release_fish
+  #       release_fish
 
-        expect(page).to have_text "#{per_page} fish released with message '' by ."
-      end
-    end
+  #       expect(page).to have_text "#{per_page} fish released with message '' by ."
+  #     end
+  #   end
 
-    context "uncheck one of them" do
-      it "releases the fish from the selected page - 1" do
-        visit url
+  #   context "uncheck one of them" do
+  #     it "releases the fish from the selected page - 1" do
+  #       visit url
 
-        check_select_all
-        expect_all_message
+  #       check_select_all
+  #       expect_all_message
 
-        click_on "Select all matching"
-        expect_all_matching_message
+  #       click_on "Select all matching"
+  #       expect_all_matching_message
 
-        uncheck_first_record
-        release_fish
+  #       uncheck_first_record
+  #       release_fish
 
-        expect(page).to have_text "#{per_page - 1} fish released with message '' by ."
-      end
-    end
-  end
+  #       expect(page).to have_text "#{per_page - 1} fish released with message '' by ."
+  #     end
+  #   end
+  # end
 
   describe "with applyed filters" do
     context "select all" do
@@ -102,10 +102,19 @@ RSpec.describe "SelectAll", type: :system do
     describe "countless resource" do
       context "select all" do
         it "display contless messages" do
-          countless = FishResource.countless
-          FishResource.countless = true
+          pagination = FishResource.pagination
+          FishResource.pagination = -> do
+            {
+              type: :countless
+            }
+          end
 
           visit url
+
+          within('nav.pagy-nav.pagination') do
+            expect(page).to have_css('span.page.active', text: '1')
+            expect(page).to have_css('span.page', text: '2')
+          end
 
           check_select_all
           expect(page).to have_text "2 records selected on this page"
@@ -113,7 +122,27 @@ RSpec.describe "SelectAll", type: :system do
           click_on "Select all matching"
           expect(page).to have_text "All records selected from all pages"
 
-          FishResource.countless = countless
+          FishResource.pagination = -> do
+            {
+              type: :countless,
+              size: []
+            }
+          end
+
+          visit url
+
+          within('nav.pagy-nav.pagination') do
+            expect(page).not_to have_css('span.page.active', text: '1')
+            expect(page).not_to have_css('span.page', text: '2')
+          end
+
+          check_select_all
+          expect(page).to have_text "2 records selected on this page"
+
+          click_on "Select all matching"
+          expect(page).to have_text "All records selected from all pages"
+
+          FishResource.pagination = pagination
         end
       end
     end
