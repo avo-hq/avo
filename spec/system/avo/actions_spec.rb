04a28aebb0985c06b2bad2c039b165e8e6d14809
Paul Bob <paul.ionut.bob@gmail.com>
2023-11-24 14:36:34 +0200

    apply 2038 & 2034

--- spec/system/avo/actions_spec.rb
+++ spec/system/avo/actions_spec.rb
@@ -149,6 +149,32 @@ RSpec.describe "Actions", type: :system do
     end
   end
 
+  describe "redirects when no confirmation" do
+    it "redirects to hey page" do
+      original_dummy_action = Avo::Actions::Sub::DummyAction.dup
+
+      Avo::Actions::Sub::DummyAction.class_eval do
+        self.no_confirmation = true
+
+        define_method(:redirect_handle) do |**args|
+          redirect_to main_app.hey_path
+        end
+
+        alias_method :handle, :redirect_handle
+      end
+
+      visit "/admin/resources/users"
+
+      click_on "Actions"
+      click_on "Dummy action"
+
+      expect(page).to have_text "hey en"
+
+      Avo::Actions::Sub.send(:remove_const, "DummyAction")
+      Avo::Actions::Sub.const_set("DummyAction", original_dummy_action)
+    end
+  end
+
   #   let!(:roles) { { admin: false, manager: false, writer: false } }
   #   let!(:user) { create :user, active: true, roles: roles }
 
