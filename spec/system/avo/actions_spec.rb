191b5d158d5f49c07e777bfe624cdd6d0727614a
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2023-11-28 17:05:15 +0200

    chore: fix actions spec (#2068)

--- spec/system/avo/actions_spec.rb
+++ spec/system/avo/actions_spec.rb
@@ -151,27 +151,12 @@ RSpec.describe "Actions", type: :system do
 
   describe "redirects when no confirmation" do
     it "redirects to hey page" do
-      original_dummy_action = Avo::Actions::Sub::DummyAction.dup
-
-      Avo::Actions::Sub::DummyAction.class_eval do
-        self.no_confirmation = true
-
-        define_method(:redirect_handle) do |**args|
-          redirect_to main_app.hey_path
-        end
-
-        alias_method :handle, :redirect_handle
-      end
-
       visit "/admin/resources/users"
 
       click_on "Actions"
-      click_on "Dummy action"
+      click_on "Test No Confirmation Redirect"
 
       expect(page).to have_text "hey en"
-
-      Avo::Actions::Sub.send(:remove_const, "DummyAction")
-      Avo::Actions::Sub.const_set("DummyAction", original_dummy_action)
     end
   end
 
