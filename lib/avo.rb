9e3a96bbbc4474fab0a0e83698103f96062110db
Desmond Naranjo <itsdesmond@hey.com>
2023-12-05 15:55:34 -0600

    Refactor Resource loading to simplify and centralize behavior

--- lib/avo.rb
+++ lib/avo.rb
@@ -18,16 +18,6 @@ module Avo
   IN_DEVELOPMENT = ENV["AVO_IN_DEVELOPMENT"] == "1"
   PACKED = !IN_DEVELOPMENT
   COOKIES_KEY = "avo"
-  ENTITIES = {
-    cards: ["app", "avo", "cards"],
-    scopes: ["app", "avo", "scopes"],
-    fields: ["app", "avo", "fields"],
-    filters: ["app", "avo", "filters"],
-    actions: ["app", "avo", "actions"],
-    resources: ["app", "avo", "resources"],
-    dashboards: ["app", "avo", "dashboards"],
-    resource_tools: ["app", "avo", "resource_tools"]
-  }
 
   class LicenseVerificationTemperedError < StandardError; end
 
