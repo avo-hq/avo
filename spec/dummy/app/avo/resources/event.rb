bab373a1a36cc7f6796c588ebd706554a8e97d08
Brychu <pbrych@gmail.com>
2023-12-06 11:54:53 +0100

    Add comment to Event resource for consistency

--- spec/dummy/app/avo/resources/event.rb
+++ spec/dummy/app/avo/resources/event.rb
@@ -6,6 +6,8 @@ class Avo::Resources::Event < Avo::BaseResource
   def fields
     field :name, as: :text, link_to_record: true, sortable: true, stacked: true
     field :event_time, as: :datetime, sortable: true
+
+    # Example for error message when resource is missing
     field :location, as: :belongs_to
   end
 end
