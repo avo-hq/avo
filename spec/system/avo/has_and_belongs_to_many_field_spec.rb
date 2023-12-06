2bd47f08253cf450ae3a026184c4ece28e894177
Brychu <pbrych@gmail.com>
2023-12-06 11:46:36 +0100

    Move missing resource error specs to one place

--- spec/system/avo/has_and_belongs_to_many_field_spec.rb
+++ spec/system/avo/has_and_belongs_to_many_field_spec.rb
@@ -118,25 +118,4 @@ RSpec.describe "HasAndBelongsToManyField", type: :system do
       end
     end
   end
-
-  context "course -> locations" do
-    let!(:course) { create :course }
-
-    around do |example|
-      old_value = Capybara.raise_server_errors
-      Capybara.raise_server_errors = false
-      example.run
-      Capybara.raise_server_errors = old_value
-    end
-
-    it "shows informative error with suggested solution for missing resource" do
-      visit "/admin/resources/courses/#{course.id}/locations?turbo_frame=has_and_belongs_to_many_field_show_locations"
-
-      expect(page).to have_content("Avo::MissingResourceError")
-      expect(page).to have_content("Missing resource detected while rendering field for locations.")
-      expect(page).to have_content("You can generate that resource running 'rails generate avo:resource location'.")
-      expect(page).to have_content("Alternatively use 'use_resource' option to specify the resource to be used on the field.")
-      expect(page).to have_content("Check more at https://docs.avohq.io/3.0/resources.html.")
-    end
-  end
 end
