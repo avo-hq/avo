2a12594a90c69dfc6e5b5e96fb6d2e91867011cf
Brychu <pbrych@gmail.com>
2023-12-06 12:52:14 +0100

    Rename spec file to model_missing_resource_spec

--- spec/system/avo/model_missing_resource_spec.rb
+++ spec/system/avo/model_missing_resource_spec.rb
@@ -8,6 +8,19 @@ RSpec.feature "MissingResourceError", type: :system do
     Capybara.raise_server_errors = old_value
   end
 
+  context "when belongs_to field" do
+    let!(:event) { create :event }
+
+    it "shows informative error with suggested solution for missing resource" do
+      visit "/admin/resources/events"
+
+      expect(page).to have_content("Missing resource detected while rendering field for location.")
+      expect(page).to have_content("You can generate that resource running 'rails generate avo:resource location'.")
+      expect(page).to have_content("Alternatively use 'use_resource' option to specify the resource to be used on the field.")
+      expect(page).to have_content("Check more at https://docs.avohq.io/3.0/resources.html.")
+    end
+  end
+
   context "when has_many field" do
     let!(:team) { create :team }
 
@@ -28,19 +41,6 @@ RSpec.feature "MissingResourceError", type: :system do
       visit "/admin/resources/courses/#{course.id}/locations?turbo_frame=has_and_belongs_to_many_field_show_locations"
 
       expect(page).to have_content("Missing resource detected while rendering field for locations.")
-      expect(page).to have_content("You can generate that resource running 'rails generate avo:resource location'.")
-      expect(page).to have_content("Alternatively use 'use_resource' option to specify the resource to be used on the field.")
-      expect(page).to have_content("Check more at https://docs.avohq.io/3.0/resources.html.")
-    end
-  end
-
-  context "when belongs_to field" do
-    let!(:event) { create :event }
-
-    it "shows informative error with suggested solution for missing resource" do
-      visit "/admin/resources/events"
-
-      expect(page).to have_content("Missing resource detected while rendering field for location.")
       expect(page).to have_content("You can generate that resource running 'rails generate avo:resource location'.")
       expect(page).to have_content("Alternatively use 'use_resource' option to specify the resource to be used on the field.")
       expect(page).to have_content("Check more at https://docs.avohq.io/3.0/resources.html.")
