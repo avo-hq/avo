507a489f35481d633ac967ce7a68bb59a6ecd420
Desmond Naranjo <itsdesmond@hey.com>
2023-12-08 05:37:51 -0600

    Refactor Resource loading to simplify and centralize behavior (#2084)

--- lib/avo/dynamic_router.rb
+++ lib/avo/dynamic_router.rb
@@ -1,39 +1,15 @@
 module Avo
   class DynamicRouter
-    def self.eager_load(entity)
-      paths = Avo::ENTITIES.fetch entity
-
-      return unless paths.present?
-
-      pathname = Rails.root.join(*paths)
-      if pathname.directory?
-        Rails.autoloaders.main.eager_load_dir(pathname.to_s)
-      end
-    end
-
     def self.routes
       Avo::Engine.routes.draw do
         scope "resources", as: "resources" do
-          # Check if the user chose to manually register the resource files.
-          # If so, eager_load the resources dir.
-          if Avo.configuration.resources.nil?
-            Avo::DynamicRouter.eager_load(:resources) unless Rails.application.config.eager_load
-          end
-
-          Avo::Resources::ResourceManager.fetch_resources
-            .select do |resource|
-              resource != :BaseResource
-            end
-            .select do |resource|
-              resource.is_a? Class
-            end
-            .map do |resource|
-              resources resource.route_key do
-                member do
-                  get :preview
-                end
+          Avo::Resources::ResourceManager.fetch_resources.map do |resource|
+            resources resource.route_key do
+              member do
+                get :preview
               end
             end
+          end
         end
       end
     end
