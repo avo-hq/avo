357a258e43c81cfe9748b6e16ece5a03c19163da
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2023-12-09 19:02:40 +0200

    Update app/javascript/js/controllers/filter_controller.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/javascript/js/controllers/filter_controller.js
+++ app/javascript/js/controllers/filter_controller.js
@@ -43,7 +43,7 @@ export default class extends Controller {
         String.fromCodePoint(
           ...new TextEncoder().encode(
             JSON.stringify(filtered),
-          )
+          ),
         )
       )
     );
