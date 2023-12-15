d40b8e06c6b4b06f3b2b88689c4e9a18fb55a9d2
Djalma Araújo de Andrade <djalma.araujo@gmail.com>
2023-12-15 18:23:12 -0300

    Fix tests for status

--- spec/system/avo/base_field_spec.rb
+++ spec/system/avo/base_field_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Base Fields", type: :system do
-  let(:project) { create :project, :with_files }
+  let!(:project) { create :project }
 
   after :each do
     Avo::Resources::Project.restore_items_from_backup
@@ -10,26 +10,26 @@ RSpec.feature "Base Fields", type: :system do
   describe "base_field_translation_key" do
     it "renders the the default status name" do
       Avo::Resources::Project.with_temporary_items do
-        self.base_field_translation_key = nil
-
         field :status, as: :text
       end
 
       visit "/admin/resources/projects"
 
-      status_wrapper = find('[data-table-header-field-type="status"]')
-      expect(status_wrapper).to have_text("Status")
+      status_text = find('th[data-control="resource-field-th"][data-table-header-field-id="status"]').text
+      expect(status_text).to eq("STATUS")
     end
 
     it "renders the nested translated key" do
       Avo::Resources::Project.with_temporary_items do
+        self.base_field_translation_key = "avo.field_translations.project"
         field :status, as: :text
       end
 
       visit "/admin/resources/projects"
 
-      status_wrapper = find('[data-table-header-field-type="status"]')
-      expect(status_wrapper).to have_text("Status Nested")
+
+      status_text = find('th[data-control="resource-field-th"][data-table-header-field-id="status"]').text
+      expect(status_text).to eq("STATUS NESTED")
     end
   end
 end
