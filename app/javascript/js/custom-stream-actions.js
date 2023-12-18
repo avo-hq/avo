5ca90bdd2ac5ffc7f6737be87dd2c717c154ecd1
Adrian Marin <adrian@adrianthedev.com>
2023-12-18 22:56:38 +0200

    chore: try Turbo 8 (#2196)

--- app/javascript/js/custom-stream-actions.js
+++ app/javascript/js/custom-stream-actions.js
@@ -1,13 +1,18 @@
 /* eslint-disable camelcase */
 import { StreamActions } from '@hotwired/turbo'
 
+StreamActions.close_modal = () => {
+  document.querySelector('turbo_frame#actions_show').innerHTML = ''
+  document.querySelector('turbo_frame#attach_modal').innerHTML = ''
+}
+
 // TODO: move these to the avo_filters gem
 
-StreamActions.close_filters_dropdown = function () {
+StreamActions.close_filters_dropdown = () => {
   document.querySelector('.filters-dropdown-selector').classList.add('hidden')
 }
 
-StreamActions.open_filter = function () {
+StreamActions.open_filter = () => {
   const id = this.getAttribute('unique-id')
   setTimeout(() => {
     document.querySelector(`[data-filter-id="${id}"] .pill`).click()
