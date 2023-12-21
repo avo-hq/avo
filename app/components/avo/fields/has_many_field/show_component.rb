7caceed87f6360b4dba8cbd7f094f386ddf452e9
Paul Bob <paul.ionut.bob@gmail.com>
2023-12-21 17:26:34 +0200

    revert frames helper include

--- app/components/avo/fields/has_many_field/show_component.rb
+++ app/components/avo/fields/has_many_field/show_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Avo::Fields::HasManyField::ShowComponent < Avo::Fields::ShowComponent
+  include Turbo::FramesHelper
   include Avo::Fields::Concerns::FrameLoading
 end
