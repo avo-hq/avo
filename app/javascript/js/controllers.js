3f218bc3142ce3b3da97028987e2af3081113a99
Paul Bob <paul.ionut.bob@gmail.com>
2023-12-27 21:59:42 +0200

    revert controllers

--- app/javascript/js/controllers.js
+++ app/javascript/js/controllers.js
@@ -19,6 +19,7 @@ import ItemSelectorController from './controllers/item_selector_controller'
 import KeyValueController from './controllers/fields/key_value_controller'
 import LoadingButtonController from './controllers/loading_button_controller'
 import MenuController from './controllers/menu_controller'
+import ModalController from './controllers/modal_controller'
 import MultipleSelectFilterController from './controllers/multiple_select_filter_controller'
 import PerPageController from './controllers/per_page_controller'
 import PreviewController from './controllers/preview_controller'
@@ -52,6 +53,7 @@ application.register('item-select-all', ItemSelectAllController)
 application.register('item-selector', ItemSelectorController)
 application.register('loading-button', LoadingButtonController)
 application.register('menu', MenuController)
+application.register('modal', ModalController)
 application.register('multiple-select-filter', MultipleSelectFilterController)
 application.register('per-page', PerPageController)
 application.register('preview', PreviewController)
