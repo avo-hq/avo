fc73cc043b0a73781fa512111a84d36bf278bea9
Paul Bob <paul.ionut.bob@gmail.com>
2023-12-28 18:51:35 +0200

    fix: secret key base warning

--- lib/avo/services/encryption_service.rb
+++ lib/avo/services/encryption_service.rb
@@ -29,14 +29,27 @@ module Avo
 
       def encryption_key
         secret_key_base[0..31]
-      rescue
-        # This will fail the decryption process.
-        # It's here only to keep Avo from crashing
-        SecureRandom.random_bytes(32)
       end
 
       def secret_key_base
-        ENV["SECRET_KEY_BASE"] || Rails.application.credentials.secret_key_base || Rails.application.secrets.secret_key_base
+        # Try to fetch the secret key base from ENV or the credentials file
+        key = ENV["SECRET_KEY_BASE"] || Rails.application.credentials.secret_key_base
+
+        # If key is blank and Rails version is less than 7.2.0
+        # Try to fetch the secret key base from the secrets file
+        # Rails 7.2.0 made secret_key_base from secrets obsolete
+        if key.blank? && (Rails.gem_version < Gem::Version.new('7.2.0'))
+          key = Rails.application.secrets.secret_key_base
+        end
+
+        return key if key.present?
+
+        # Avoid breaking in production
+        # All features relying on encryption will not work properly without a configured secret key base
+        return SecureRandom.random_bytes(32) if Rails.env.production?
+
+        raise "Unable to fetch secret key base. Please set it in your credentials or environment variables\n" \
+          "For more information check https://docs.avohq.io/3.0/encryption-service.html#secret-key-base"
       end
     end
   end
