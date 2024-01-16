d37c9a92d65bc87ecff7f52b372dbd40e3862a2f
Adrian Marin <adrian@adrianthedev.com>
2024-01-16 13:40:51 +0200

    wip

--- app/javascript/js/custom-stream-actions.js
+++ app/javascript/js/custom-stream-actions.js
@@ -1,8 +1,22 @@
 /* eslint-disable camelcase */
+/* eslint-disable func-names */
 import { StreamActions } from '@hotwired/turbo'
 import { saveAs } from 'file-saver'
 
-StreamActions.download = () => {
+// TODO: move these to the avo_filters gem
+StreamActions.close_filters_dropdown = function () {
+  document.querySelector('.filters-dropdown-selector').classList.add('hidden')
+}
+
+StreamActions.open_filter = function () {
+  const id = this.getAttribute('unique-id')
+  setTimeout(() => {
+    document.querySelector(`[data-filter-id="${id}"] .pill`).click()
+  }, 150)
+}
+// END TODO: move these to the avo_filters gem
+
+StreamActions.download = function () {
   saveAs(
     new Blob(
       [this.getAttribute('content')],
