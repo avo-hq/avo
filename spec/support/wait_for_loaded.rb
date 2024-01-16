42074075a3df275e8b05291d5275243b87de3353
Paul Bob <paul.ionut.bob@gmail.com>
2024-01-16 10:32:17 +0200

    fix wait_for_tag_suggestions_to_appear

--- spec/support/wait_for_loaded.rb
+++ spec/support/wait_for_loaded.rb
@@ -73,6 +73,6 @@ end
 def wait_for_tag_suggestions_to_appear(time = Capybara.default_max_wait_time)
   Capybara.using_wait_time(time) do
     last_dropdown_item = page.all('.tagify__dropdown__item').last
-    page.document.synchronize { last_dropdown_item.visible? }
+    page.document.synchronize { last_dropdown_item&.visible? }
   end
 end
