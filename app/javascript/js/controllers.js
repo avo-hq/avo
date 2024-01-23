610734cb1123f88563fae296d18be551f09c8a0b
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-01-23 18:34:02 +0200

    Import has many refresh controller

--- app/javascript/js/controllers.js
+++ app/javascript/js/controllers.js
@@ -12,6 +12,7 @@ import DashboardCardController from './controllers/dashboard_card_controller'
 import DateFieldController from './controllers/fields/date_field_controller'
 import EasyMdeController from './controllers/fields/easy_mde_controller'
 import FilterController from './controllers/filter_controller'
+import HasManyRefreshController from './controllers/has_many_refresh_controller'
 import HiddenInputController from './controllers/hidden_input_controller'
 import InputAutofocusController from './controllers/input_autofocus_controller'
 import ItemSelectAllController from './controllers/item_select_all_controller'
@@ -48,6 +49,7 @@ application.register('boolean-filter', BooleanFilterController)
 application.register('copy-to-clipboard', CopyToClipboardController)
 application.register('dashboard-card', DashboardCardController)
 application.register('filter', FilterController)
+application.register('has-many-refresh', HasManyRefreshController)
 application.register('hidden-input', HiddenInputController)
 application.register('input-autofocus', InputAutofocusController)
 application.register('item-select-all', ItemSelectAllController)
