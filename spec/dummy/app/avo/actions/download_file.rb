bc395540452cc78db2d2ca6eda065ff87a97e244
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2024-01-24 19:41:48 +0200

    fix: actions download (#2398)
    * fix: actions download
    
    * wip

--- spec/dummy/app/avo/actions/download_file.rb
+++ spec/dummy/app/avo/actions/download_file.rb
@@ -5,6 +5,7 @@ class Avo::Actions::DownloadFile < Avo::BaseAction
   # TODO: fix fields for actions
   def fields
     field :read_from_file, as: :boolean, name: "Read from file", default: false
+    field :read_from_pdf_file, as: :boolean, name: "Read from PDF file", default: false
   end
 
   def handle(**args)
@@ -15,6 +16,10 @@ class Avo::Actions::DownloadFile < Avo::BaseAction
       file = File.open(Avo::Engine.root.join("spec", "dummy", "dummy-file.txt"))
 
       download file.read, "dummy-file.txt"
+    elsif fields["read_from_pdf_file"]
+      file = File.open(Avo::Engine.root.join("spec", "dummy", "dummy-file.pdf"))
+
+      download file.read, "dummy-file.pdf"
     else
       download "On the fly dummy content.", "dummy-content.txt"
     end
