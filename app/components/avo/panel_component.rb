25aac67a45038171faff226f41b129e9d83aae1f
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-01-24 18:18:05 +0200

    Get access for turbo_frame inside of panel_component

--- app/components/avo/panel_component.rb
+++ app/components/avo/panel_component.rb
@@ -4,6 +4,7 @@ class Avo::PanelComponent < ViewComponent::Base
   attr_reader :title # deprecating title in favor of name
   attr_reader :name
   attr_reader :classes
+  attr_reader :turbo_frame
 
   delegate :white_panel_classes, to: :helpers
 
@@ -15,7 +16,7 @@ class Avo::PanelComponent < ViewComponent::Base
   renders_one :footer_tools
   renders_one :footer
 
-  def initialize(name: nil, description: nil, body_classes: nil, data: {}, display_breadcrumbs: false, index: nil, classes: nil, **args)
+  def initialize(name: nil, description: nil, body_classes: nil,turbo_frame: nil, data: {}, display_breadcrumbs: false, index: nil, classes: nil, **args)
     # deprecating title in favor of name
     @title = args[:title]
     @name = name || title
@@ -25,6 +26,7 @@ class Avo::PanelComponent < ViewComponent::Base
     @data = data
     @display_breadcrumbs = display_breadcrumbs
     @index = index
+    @turbo_frame = turbo_frame
   end
 
   private
