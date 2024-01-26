78788dae68b3983240514fd73a682f599aff3be5
Paul Bob <paul.ionut.bob@gmail.com>
2024-01-26 18:48:30 +0200

    resource's view instead from params

--- app/components/avo/index/resource_controls_component.rb
+++ app/components/avo/index/resource_controls_component.rb
@@ -182,7 +182,7 @@ class Avo::Index::ResourceControlsComponent < Avo::ResourceComponent
     hidden = {}
 
     hidden[:view_type] = params[:view_type] if params[:view_type]
-    hidden[:view] = params[:view]
+    hidden[:view] = resource.view.to_s
 
     if params[:turbo_frame]
       hidden[:turbo_frame] = params[:turbo_frame]
