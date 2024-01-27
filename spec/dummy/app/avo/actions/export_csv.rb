10e7805459322df5b89a0aac18d062e09a82680a
Adrian Marin <adrian@adrianthedev.com>
2024-01-27 15:14:50 +0200

    refactor: tool-manager-tweaks (#2419)

--- spec/dummy/app/avo/actions/export_csv.rb
+++ spec/dummy/app/avo/actions/export_csv.rb
@@ -1,13 +1,32 @@
 class Avo::Actions::ExportCsv < Avo::BaseAction
-  self.name = "Export csv"
+  self.name = "Export CSV"
   self.no_confirmation = false
+  self.standalone = true
+
+  def fields
+    # Add more fields here for custom user-selected columns
+    field :id, as: :boolean
+    field :created_at, as: :boolean
+  end
 
   def handle(**args)
-    records, resource = args.values_at(:records, :resource)
+    records, resource, fields = args.values_at(:records, :resource, :fields)
+
+    # uncomment if you want to download all the records if none was selected
+    # records = resource.model_class.all if records.blank?
 
     return error "No record selected" if records.blank?
 
-    attributes = get_attributes records.first
+    # uncomment to get all the models' attributes.
+    # attributes = get_attributes_from_record records.first
+
+    # uncomment to get some attributes
+    # attributes = get_some_attributes
+
+    attributes = get_attributes_from_fields fields
+
+    # uncomment to get all the models' attributes if none were selected
+    # attributes = get_attributes_from_record records.first if attributes.blank?
 
     file = CSV.generate(headers: true) do |csv|
       csv << attributes
@@ -22,9 +41,15 @@ class Avo::Actions::ExportCsv < Avo::BaseAction
     download file, "#{resource.plural_name}.csv"
   end
 
-  def get_attributes(record)
-    # return ["id", "created_at"] # uncomment this and fill in for custom model properties
-
+  def get_attributes_from_record(record)
     record.class.columns_hash.keys
+  end
+
+  def get_attributes_from_fields(fields)
+    fields.select { |key, value| value }.keys
+  end
+
+  def get_some_attributes
+    ["id", "created_at"]
   end
 end
