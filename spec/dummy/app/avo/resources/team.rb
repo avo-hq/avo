808a0245094d6ee751c1f42ec0f38b38cd69efa4
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-01-30 16:42:54 +0200

    Add reload_button field

--- spec/dummy/app/avo/resources/team.rb
+++ spec/dummy/app/avo/resources/team.rb
@@ -78,7 +78,7 @@ class Avo::Resources::Team < Avo::BaseResource
 
     field :admin, as: :has_one
     field :team_members, as: :has_many, through: :memberships, translation_key: "avo.resource_translations.team_members"
-    field :reviews, as: :has_many
+    field :reviews, as: :has_many, reload_button: true
 
     if params[:show_location_field] == '1'
       # Example for error message when resource is missing
