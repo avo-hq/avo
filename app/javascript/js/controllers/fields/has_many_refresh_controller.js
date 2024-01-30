bd34160d158d9ba663dbec65b6ec7ae078603ebd
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-01-30 16:56:39 +0200

    Fetch the parent turbo frame

---
+++ app/javascript/js/controllers/fields/has_many_refresh_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  refresh() {
+    var element = this.context.scope.element.closest('turbo-frame');
+    if (element) {
+      element.reload();
+    } else {
+      console.error(
+        "Element with ID '" + this.turboFrameIdValue + "' not found."
+      );
+    }
+  }
+}
