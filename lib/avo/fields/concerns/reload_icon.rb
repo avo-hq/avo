5232a6feff5fcd4a63c457496943e3e2d407794b
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-01-30 16:40:17 +0200

    Add concern with reload_icon

---
+++ lib/avo/fields/concerns/reload_icon.rb
@@ -0,0 +1,19 @@
+module Avo
+  module Fields
+    module Concerns
+      module ReloadIcon
+        extend ActiveSupport::Concern
+
+        included do
+          attr_accessor :reload_button
+        end
+
+        def reload_icon_enabled?
+          # @reload_button = @reload_button.present? ? @reload_button : false
+          # byebug
+          @reload_button = true
+        end
+      end
+    end
+  end
+end
