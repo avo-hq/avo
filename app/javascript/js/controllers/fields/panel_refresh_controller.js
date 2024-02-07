dce0055a6585a6d02e93e4df0acb9ef9961f3135
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-02-07 14:13:49 +0200

    Fix eslint error

--- app/javascript/js/controllers/fields/panel_refresh_controller.js
+++ app/javascript/js/controllers/fields/panel_refresh_controller.js
@@ -1,15 +1,19 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-
   refresh() {
-    var element = this.context.scope.element.closest('turbo-frame');
+    const button = this.element
+    button.classList.add('animate-spin')
+    const element = this.context.scope.element.closest('turbo-frame')
     if (element) {
-      element.reload();
+      element.addEventListener('turbo:before-fetch-response', () => {
+        button.classList.remove('animate-spin')
+      })
+      element.reload()
     } else {
       console.error(
-        "Element with ID '" + this.turboFrameIdValue + "' not found."
-      );
+        `Element with ID '${this.turboFrameIdValue}' not found.`,
+      )
     }
   }
 }
