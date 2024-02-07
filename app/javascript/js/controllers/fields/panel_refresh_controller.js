f26b7163029e4c4a204919598c463dc3fe67da90
Adrian Marin <adrian@adrianthedev.com>
2024-02-07 14:45:47 +0200

    Update app/javascript/js/controllers/fields/panel_refresh_controller.js

--- app/javascript/js/controllers/fields/panel_refresh_controller.js
+++ app/javascript/js/controllers/fields/panel_refresh_controller.js
@@ -2,10 +2,10 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   refresh() {
-    const element = this.context.scope.element.closest('turbo-frame')
+    const frame = this.context.scope.element.closest('turbo-frame')
     this.element.querySelector('svg').classList.add('animate-spin')
-    if (element) {
-      element.reload()
+    if (frame) {
+      frame.reload()
     } else {
       console.error(
         `Element with ID '${this.turboFrameIdValue}' not found.`,
