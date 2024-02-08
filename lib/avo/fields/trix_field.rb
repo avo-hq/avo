ebb471433ec4c780c57326b52d9d6778b90931c2
Olivier Buffon <olivierbuffon@users.noreply.github.com>
2024-02-08 05:47:32 -0500

    fix: Trix field disable_attachments option unexpected behaviour (#2462)

--- lib/avo/fields/trix_field.rb
+++ lib/avo/fields/trix_field.rb
@@ -15,13 +15,20 @@ module Avo
 
         @always_show = args[:always_show] || false
         @attachment_key = args[:attachment_key]
-        # If we don't have an attachment_key, we disable attachments.  There's no point in having
-        # attachments if we can't store them.
-        @attachments_disabled = args[:attachments_disabled] || true
-        @attachments_disabled = false unless @attachment_key.present?
+        @attachments_disabled = disable_attachments?(args)
         @hide_attachment_filename = args[:hide_attachment_filename] || false
         @hide_attachment_filesize = args[:hide_attachment_filesize] || false
         @hide_attachment_url = args[:hide_attachment_url] || false
+      end
+
+      private
+
+      def disable_attachments?(args)
+        # If we don't have an attachment_key, we disable attachments. There's no point in having
+        # attachments if we can't store them.
+        return false if args[:attachment_key].present?
+
+        args[:attachments_disabled] == true
       end
     end
   end
