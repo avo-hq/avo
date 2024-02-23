fa4e10578722780e6149dd928f9e031714a33335
Brychu <pbrych@gmail.com>
2024-02-24 00:30:30 +0100

    Uncomment the spec and change its type to :system

--- spec/features/avo/field_summarizing_spec.rb
+++ spec/features/avo/field_summarizing_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Field Summarizing", type: :feature do
+RSpec.feature "Field Summarizing", type: :system do
   before_all do
     create_list :project, 3, status: :rejected
     create_list :project, 1, status: :closed
@@ -18,21 +18,21 @@ RSpec.feature "Field Summarizing", type: :feature do
       expect(page).to have_css "turbo-frame[id='summary-frame-status']", visible: true
 
       # I can't make the lazy loading work, looks like it's not triggered at all
-      # wait_for_turbo_frame_id("summary-frame-status")
+      wait_for_turbo_frame_id("summary-frame-status")
 
-      # expect(page).to have_css "#status-summary", visible: true
-      # expect(page).to have_css "#chart-1", visible: true
+      expect(page).to have_css "#status-summary", visible: true
+      expect(page).to have_css "#chart-status", visible: true
 
-      # within "#status-summary" do
-      #   expect(page).to have_content "rejected\n3"
-      #   expect(page).to have_content "closed\n1"
-      #   expect(page).to have_content "loading\n4"
-      # end
+      within "#status-summary" do
+        expect(page).to have_content "rejected\n3"
+        expect(page).to have_content "closed\n1"
+        expect(page).to have_content "loading\n4"
+      end
 
-      # find('th[data-table-header-field-id="status"] div svg').click
+      find('th[data-table-header-field-id="status"] div svg').click
 
-      # expect(page).not_to have_css "#status-summary"
-      # expect(page).not_to have_css "#chart-1"
+      expect(page).not_to have_css "#status-summary"
+      expect(page).not_to have_css "#chart-status"
     end
 
     it "doesn't show up for fields without option" do
