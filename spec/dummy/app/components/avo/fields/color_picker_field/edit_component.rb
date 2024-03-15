f6824df74d163bdbef7acd02f550a48a496553af
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-03-15 19:45:21 +0200

    Updated edit_component to support field_input_args

--- spec/dummy/app/components/avo/fields/color_picker_field/edit_component.rb
+++ spec/dummy/app/components/avo/fields/color_picker_field/edit_component.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Avo::Fields::ColorPickerField::EditComponent < Avo::Fields::EditComponent
+  def field_input_args
+    super.merge(
+      {
+        class: helpers.input_classes("w-full py-0", has_error: @field.record_errors.include?(@field.id)).gsub("py-2", "")
+      }
+    )
+  end
 end
