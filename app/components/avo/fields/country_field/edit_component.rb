6d7afd3ebdd0b4284463eb23ca67ac804e526344
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-03-15 19:21:28 +0200

    Updated edit_component to support field_input_args

--- app/components/avo/fields/country_field/edit_component.rb
+++ app/components/avo/fields/country_field/edit_component.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Avo::Fields::CountryField::EditComponent < Avo::Fields::EditComponent
+  def field_input_args
+    super.merge(
+      {
+        aria: {
+          placeholder: @field.placeholder
+        },
+        class: classes("w-full"),
+        placeholder: @field.include_blank.present? ? nil : @field.placeholder
+      }
+    )
+  end
 end
