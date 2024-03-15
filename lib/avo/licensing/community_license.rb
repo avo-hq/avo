f6f3e98da836e3299739ee05884f46f9cc26ffc0
Muhammed Kanyi <kanyi@gcubed.gm>
2024-03-15 23:27:59 +0000

    updated

--- lib/avo/licensing/community_license.rb
+++ lib/avo/licensing/community_license.rb
@@ -1,6 +1,20 @@
 module Avo
   module Licensing
     class CommunityLicense < License
+      def abilities
+        [
+          :authorization,
+          :global_search,
+          :enhanced_search_results,
+          :searchable_associations,
+          :resource_ordering,
+          :dashboards,
+          :menu_editor,
+          :customizable_controls,
+          :resource_sidebar,
+          :advanced_fields
+        ]
+      end
     end
   end
 end
