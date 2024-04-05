a81e4da068700d4229a42f389b8cb5026d88871d
Paul Bob <paul.ionut.bob@gmail.com>
2024-04-05 13:44:33 +0300

    remove --force option

--- lib/generators/avo/concerns/override_controller.rb
+++ lib/generators/avo/concerns/override_controller.rb
@@ -4,20 +4,10 @@ module Generators
       module OverrideController
         extend ActiveSupport::Concern
 
-        included do
-          class_option "--force",
-            desc: "Skip override validation.",
-            type: :string,
-            required: false
-        end
-
         def override_controller?
-          return false if options[:force].present?
           return false unless controller_name.in? controllers_list
 
-          say("Avo uses #{controller_class} internally, overriding it will cause malfunctions.", :red)
-          say("Use --force if you really want to override the \033[1m#{controller_class}.", :yellow)
-
+          say("Avo uses #{controller_class} internally, overriding it would cause malfunctions.", :red)
           true
         end
 
