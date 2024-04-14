05c426613c8565e1788471764c9f5d9aa97f9c63
Adrian Marin <adrian@adrianthedev.com>
2024-04-14 12:41:41 +0300

    wip

--- lib/avo/resources/items/main_panel.rb
+++ lib/avo/resources/items/main_panel.rb
@@ -1,6 +1,6 @@
 class Avo::Resources::Items::MainPanel < Avo::Resources::Items::Panel
   class Builder < Avo::Resources::Items::ItemGroup::Builder
-    def initialize(parent: ,**args)
+    def initialize(parent:, **args)
       @panel = Avo::Resources::Items::MainPanel.new(**args)
       @items_holder = Avo::Resources::Items::Holder.new(from: self.class, parent: parent)
     end
