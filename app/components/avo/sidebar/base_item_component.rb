4a4e6caff6ced2c690cd34916cdb66f7c439953c
Adrian Marin <adrian@adrianthedev.com>
2024-04-16 11:00:25 +0300

    wip

--- app/components/avo/sidebar/base_item_component.rb
+++ app/components/avo/sidebar/base_item_component.rb
@@ -33,9 +33,9 @@ class Avo::Sidebar::BaseItemComponent < ViewComponent::Base
     {
       transition_enter: "transition ease-out duration-100",
       transition_enter_start: "transform opacity-0 -translate-y-4",
-      transition_enter_end: "transform opacity-100 scale-100",
+      transition_enter_end: "transform opacity-100 translate-y-0",
       transition_leave: "transition ease-in duration-75",
-      transition_leave_start: "transform opacity-100 scale-100",
+      transition_leave_start: "transform opacity-100 translate-y-0",
       transition_leave_end: "transform opacity-0 -translate-y-4",
     }
   end
