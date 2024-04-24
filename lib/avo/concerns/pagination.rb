f199fc5cce32184afb55b96d0170064f7c6627e7
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2024-04-24 17:17:26 +0300

    feature: configurable default pagination (#2717)
    * feature: configurable default pagination
    
    * add on template
    
    * revert countless config

--- lib/avo/concerns/pagination.rb
+++ lib/avo/concerns/pagination.rb
@@ -46,7 +46,7 @@ module Avo
       private
 
       def pagination_hash
-        @pagination ||= PAGINATION_DEFAULTS.merge Avo::ExecutionContext.new(
+        @pagination ||= PAGINATION_DEFAULTS.merge(Avo.configuration.pagination).merge Avo::ExecutionContext.new(
           target: pagination,
           resource: self,
           view: @view
