9bdac3977bee2e8fc1c7005f9083344eb5e3c927
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-05-08 20:16:28 +0300

    Add  cards_controller.js

---
+++ app/javascript/js/controllers/cards_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from '@hotwired/stimulus'
+import URI from 'urijs'
+
+export default class extends Controller {
+  static targets = ['cardsElements']
+
+  updateCards(event) {
+    const href = event.currentTarget.getAttribute('href')
+    const dateParam = getDateParam(href)
+    if (this.cardsElementsTarget) {
+      const frames = this.cardsElementsTarget.querySelectorAll('turbo-frame')
+      frames.forEach((frame) => {
+        let src = frame.getAttribute('src')
+        src = updateQueryParam(src, 'date', dateParam)
+        frame.setAttribute('src', src)
+      })
+    }
+  }
+}
+
+function updateQueryParam(url, key, value) {
+  const uri = new URI(url)
+  uri.setQuery(key, value)
+
+  return uri.toString()
+}
+
+function getDateParam(url) {
+  const queryString = url.split('?')[1]
+  const params = new URLSearchParams(queryString)
+
+  return params.get('date')
+}
