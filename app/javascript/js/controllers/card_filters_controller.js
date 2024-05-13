866ad53fc13f924f74c9c06b1d86233708b044dd
Paul Bob <paul.ionut.bob@gmail.com>
2024-05-13 15:30:08 +0300

    tweaks

--- app/javascript/js/controllers/card_filters_controller.js
+++ app/javascript/js/controllers/card_filters_controller.js
@@ -1,21 +1,12 @@
 import { Controller } from '@hotwired/stimulus'
 import URI from 'urijs'
 
-function updateQueryParam(url, key, value) {
-  const uri = new URI(url)
-  uri.setQuery(key, value)
-
-  return uri.toString()
-}
-
 export default class extends Controller {
+  static targets = ['cards']
+
   updateCards(event) {
-    const dateParam = event.target.dataset.days
-    const frames = this.element.querySelectorAll('turbo-frame[data-card-index]')
-    frames.forEach((frame) => {
-      const src = frame.getAttribute('src')
-      const srcUpdated = updateQueryParam(src, 'date', dateParam)
-      frame.setAttribute('src', srcUpdated)
+    this.cardsTargets.forEach((frame) => {
+      frame.src = new URI(frame.src).setQuery('date', event.target.dataset.days).toString()
     })
   }
 }
