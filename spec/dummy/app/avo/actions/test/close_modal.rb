aa96f6f015a758d74f3465438b14251ba8cc34c3
Paul Bob <paul.ionut.bob@gmail.com>
2024-05-22 19:50:10 +0300

    feature: allow actions to render turbo streams

--- spec/dummy/app/avo/actions/test/close_modal.rb
+++ spec/dummy/app/avo/actions/test/close_modal.rb
@@ -9,5 +9,10 @@ class Avo::Actions::Test::CloseModal < Avo::BaseAction
     TestBuddy.hi "Hello from Avo::Actions::Test::CloseModal handle method"
     succeed "Modal closed!!"
     close_modal
+
+    # Test custom added turbo_streams
+    enhance_response turbo_stream: -> {
+      turbo_stream.set_title("Dummy action custom stream ;)")
+    }
   end
 end
