05746d68ef99decf617559b494cbb043848caa0b
Adrian Marin <adrian@adrianthedev.com>
2024-05-22 15:45:18 +0300

    indent

--- app/controllers/concerns/avo/initializes_avo.rb
+++ app/controllers/concerns/avo/initializes_avo.rb
@@ -19,20 +19,20 @@ module Avo
       instance_eval(&Avo.configuration.context)
     end
 
-def default_url_options
-  result = super.dup
-  result[:force_locale] = params[:force_locale]
+    def default_url_options
+      result = super.dup
+      result[:force_locale] = params[:force_locale]
 
-  extra_default_url_options.each do |param_name|
-    result[param_name] = params[param_name]
-  end
+      extra_default_url_options.each do |param_name|
+        result[param_name] = params[param_name]
+      end
 
-  result.compact
-end
+      result.compact
+    end
 
-def extra_default_url_options
-  block_or_array = Avo.configuration.default_url_options
-  block_or_array.respond_to?(:call) ? instance_eval(&block_or_array) : block_or_array
-end
+    def extra_default_url_options
+      block_or_array = Avo.configuration.default_url_options
+      block_or_array.respond_to?(:call) ? instance_eval(&block_or_array) : block_or_array
+    end
   end
 end
