7235449cfc30d51e9b5acddffcf77d2ae3fdb7ce
Adrian Marin <adrian@adrianthedev.com>
2024-05-22 15:44:54 +0300

    Update app/controllers/concerns/avo/initializes_avo.rb
    Co-authored-by: Paul Bob <69730720+Paul-Bob@users.noreply.github.com>

--- app/controllers/concerns/avo/initializes_avo.rb
+++ app/controllers/concerns/avo/initializes_avo.rb
@@ -19,32 +19,20 @@ module Avo
       instance_eval(&Avo.configuration.context)
     end
 
-    def default_url_options
-      result = super.dup
+def default_url_options
+  result = super.dup
+  result[:force_locale] = params[:force_locale]
 
-      if params[:force_locale].present?
-        result[:force_locale] = params[:force_locale]
-      end
+  extra_default_url_options.each do |param_name|
+    result[param_name] = params[param_name]
+  end
 
-      extra_options = get_extra_default_url_options
+  result.compact
+end
 
-      if extra_options.present?
-        extra_options.each do |param_name|
-          result[param_name] = params[param_name]
-        end
-      end
-
-      result
-    end
-
-    def get_extra_default_url_options
-      block_or_array = Avo.configuration.default_url_options
-
-      if block_or_array.respond_to?(:call)
-        instance_eval(&block_or_array)
-      else
-        block_or_array
-      end
-    end
+def extra_default_url_options
+  block_or_array = Avo.configuration.default_url_options
+  block_or_array.respond_to?(:call) ? instance_eval(&block_or_array) : block_or_array
+end
   end
 end
