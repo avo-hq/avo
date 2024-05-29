9b0901a1fc867529e1854d581dfa27b8b556a2ba
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-05-29 15:18:18 +0300

    Add textarea autogrow

--- app/javascript/js/application.js
+++ app/javascript/js/application.js
@@ -1,10 +1,12 @@
 import { Alert, Popover } from 'tailwindcss-stimulus-components'
 import { Application } from '@hotwired/stimulus'
+import TextareaAutogrow from 'stimulus-textarea-autogrow'
 import TurboPower from 'turbo_power'
 
 TurboPower.initialize(window.Turbo.StreamActions)
 
 const application = Application.start()
+application.register('textarea-autogrow', TextareaAutogrow)
 
 // Configure Stimulus development experience
 application.debug = window?.localStorage.getItem('avo.debug')
