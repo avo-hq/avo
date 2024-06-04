9b0b241a21933dde1f487ca626dc9c347f5830a5
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-06-04 13:32:41 +0300

    Update test

--- spec/features/avo/divider_spec.rb
+++ spec/features/avo/divider_spec.rb
@@ -6,19 +6,18 @@ RSpec.feature "Divider", type: :feature do
   before do
     visit "/admin/resources/users"
     click_on "Actions"
-    @parsed_body = Nokogiri::HTML(page.body)
-    @dividers = @parsed_body.css(".relative.col-span-full.border-t")
   end
 
   describe "Divider in actions" do
     it "renders divider without label" do
-      unlabeled_dividers = @dividers.select { |divider| divider.at_css(".absolute").text.strip.empty? }
+      expect(page.all(".relative.col-span-full.border-t").count).to be >= 1
+      unlabeled_dividers = page.all(".relative.col-span-full.border-t").select { |divider| divider.find(".absolute").text.strip.empty? }
       expect(unlabeled_dividers.count).to be > 0
     end
 
     it "renders divider with label" do
       expect(page).to have_css ".relative.col-span-full.border-t"
-      expect(@dividers.text.strip).to eq("Other actions")
+      expect(page).to have_content "Other actions"
     end
   end
 end
