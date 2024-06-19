784d5e19ae3c432e1900138a2dcbe9f582563f77
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-06-19 11:20:22 +0300

    Test if the svg has a link or button

--- app/javascript/js/controllers/table_row_controller.js
+++ app/javascript/js/controllers/table_row_controller.js
@@ -3,10 +3,12 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   visitRecord(event) {
     if (event.type === 'click') {
-      const isLinkOrButton = event.target.closest('a, button, svg')
+      const isLinkOrButton = event.target.closest('a, button')
       const isCheckbox = event.target.closest('input[type="checkbox"]')
+      const svg = event.target.closest('svg')
+      const isSvgLink = svg.closest('button, a')
 
-      if (isLinkOrButton || isCheckbox) {
+      if (isLinkOrButton || isCheckbox || isSvgLink) {
         return // Don't navigate if a link or button is clicked
       }
 
