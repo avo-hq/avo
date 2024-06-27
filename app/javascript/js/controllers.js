b1c4ee51955dc9e6d054d8be57218b0ffde9ece5
Paul Bob <paul.ionut.bob@gmail.com>
2024-06-27 18:09:55 +0300

    Revert "fix: go back button"
    This reverts commit 408266abdb1bc57abe1c647364c2d436e7fd58e9.

--- app/javascript/js/controllers.js
+++ app/javascript/js/controllers.js
@@ -5,7 +5,6 @@ import ActionsOverflowController from './controllers/actions_overflow_controller
 import ActionsPickerController from './controllers/actions_picker_controller'
 import AttachmentsController from './controllers/attachments_controller'
 import BelongsToFieldController from './controllers/fields/belongs_to_field_controller'
-import BodyController from './controllers/body_controller'
 import BooleanFilterController from './controllers/boolean_filter_controller'
 import CardFiltersController from './controllers/card_filters_controller'
 import CodeFieldController from './controllers/fields/code_field_controller'
@@ -49,7 +48,6 @@ application.register('action', ActionController)
 application.register('actions-overflow', ActionsOverflowController)
 application.register('actions-picker', ActionsPickerController)
 application.register('attachments', AttachmentsController)
-application.register('body', BodyController)
 application.register('boolean-filter', BooleanFilterController)
 application.register('card-filters', CardFiltersController)
 application.register('copy-to-clipboard', CopyToClipboardController)
--- app/javascript/js/controllers/body_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus'
-
-export default class extends Controller {
-  connect() {
-    if (window.history.state && window.history.state.turbo) {
-      window.addEventListener('popstate', () => { location.reload(true) })
-    }
-  }
-}
