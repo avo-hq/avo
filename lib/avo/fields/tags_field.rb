3cbaae83e33de6b4c452965f8ab2da9134c5d184
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2024-07-03 15:45:46 +0300

    fix: tags delimiters (#2932)
    * fix: tags delimiters
    
    * performance

--- lib/avo/fields/tags_field.rb
+++ lib/avo/fields/tags_field.rb
@@ -54,7 +54,7 @@ module Avo
         return fill_acts_as_taggable(record, key, value, params) if acts_as_taggable_on.present?
 
         value = if value.is_a?(String)
-          value.split(",")
+          value.split(delimiters[0])
         else
           value
         end
