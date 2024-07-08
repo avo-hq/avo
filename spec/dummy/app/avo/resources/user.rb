e10adfb430726d9a5a7dd159dbd6f1c3e007ba15
Gerard Simpson <gardesimpson@gmail.com>
2024-07-08 23:35:56 +1200

    Feature: alias close_modal action response to do_nothing (#2952)
    Co-authored-by: Adrian Marin <adrian@adrianthedev.com>

--- spec/dummy/app/avo/resources/user.rb
+++ spec/dummy/app/avo/resources/user.rb
@@ -73,6 +73,7 @@ class Avo::Resources::User < Avo::BaseResource
     divider
     action Avo::Actions::Test::NoConfirmationRedirect
     action Avo::Actions::Test::CloseModal
+    action Avo::Actions::Test::DoNothing
     action Avo::Actions::DetachUser
   end
 
