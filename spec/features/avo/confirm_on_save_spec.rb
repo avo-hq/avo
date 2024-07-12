b0a1824424b2f6b947f1f0af2ff3631efcd60dc2
Gerard <gardesimpson@gmail.com>
2024-07-12 23:43:55 +1200

    prefer double quotes

--- spec/features/avo/confirm_on_save_spec.rb
+++ spec/features/avo/confirm_on_save_spec.rb
@@ -1,54 +1,54 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'ConfirmOnSave', type: :feature do
-  context 'edit' do
+RSpec.feature "ConfirmOnSave", type: :feature do
+  context "edit" do
     let!(:store) { create :store, name: store_name }
-    let(:store_name) { 'original name' }
-    let(:name_field) { 'store[name]' }
-    let(:changed_name) { 'new name' }
+    let(:store_name) { "original name" }
+    let(:name_field) { "store[name]" }
+    let(:changed_name) { "new name" }
 
-    describe 'when saving the record' do
-      it 'confirms the operation' do
+    describe "when saving the record" do
+      it "confirms the operation" do
         visit "/admin/resources/stores/#{store.id}/edit"
 
         save
 
-        expect(page).to have_selector('#turbo-confirm button[value="confirm"]')
+        expect(page).to have_selector("#turbo-confirm button[value="confirm"]")
       end
 
-      it 'completes the operation on confirmation' do
+      it "completes the operation on confirmation" do
         visit "/admin/resources/stores/#{store.id}/edit"
 
         expect(page).to have_field name_field
 
-        fill_in 'store[name]', with: changed_name
+        fill_in "store[name]", with: changed_name
 
         save
 
         accept_custom_alert do
-          click_on "Yes, I'm sure"
+          click_on "Yes, I"m sure"
         end
 
         expect(store.reload.name).to eq(changed_name)
       end
 
-      it 'does not complete the operation on denial' do
+      it "does not complete the operation on denial" do
         visit "/admin/resources/stores/#{store.id}/edit"
 
         expect(page).to have_field name_field
 
-        fill_in 'store[name]', with: changed_name
+        fill_in "store[name]", with: changed_name
 
         save
 
         accept_custom_alert do
-          click_on 'No, cancel'
+          click_on "No, cancel"
         end
 
-        expect(page).not_to have_selector('.button-spinner')
-        expect(find_field_value_element('name')).to have_text changed_name
+        expect(page).not_to have_selector(".button-spinner")
+        expect(find_field_value_element("name")).to have_text changed_name
         expect(store.name).to eq(store_name)
       end
     end
