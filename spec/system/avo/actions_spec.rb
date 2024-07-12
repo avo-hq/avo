5b269f40a07f14a59ddce0ca9e4616e1405ed22f
Paul Bob <paul.ionut.bob@gmail.com>
2024-07-12 16:13:04 +0300

    lint

--- spec/system/avo/actions_spec.rb
+++ spec/system/avo/actions_spec.rb
@@ -198,7 +198,7 @@ RSpec.describe "Actions", type: :system do
       expect(page).not_to have_selector(modal)
       expect(page).to have_text "Modal closed!!"
       expect(page).to have_title("Cool title")
-      expect(page).to have_field('user_first_name', with: 'First name should persist after action.')
+      expect(page).to have_field("user_first_name", with: "First name should persist after action.")
     end
   end
 
