ed1a5a06d4b1ff567505b49451ed23df3d365651
binarygit <inscription4@outlook.com>
2024-07-25 16:11:21 +0545

    Refactor: mutate value in file_field instead of base_field

--- lib/avo/fields/file_field.rb
+++ lib/avo/fields/file_field.rb
@@ -24,6 +24,17 @@ module Avo
       def path
         rails_blob_url value
       end
+
+      def value
+        final_value = super
+        # On edit view always show the persisted image. Related: issue#3008
+        if final_value.instance_of?(ActiveStorage::Attached::One) && final_value.image? && view == "edit"
+          persisted_record = record.class.find_by(id: record.id)
+          property = @for_attribute || id
+          final_value = persisted_record.send(property)
+        end
+        final_value
+      end
     end
   end
 end
