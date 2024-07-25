83d3008db6f4f4a0eac27d34f6956fc3d44a2c9b
binarygit <inscription4@outlook.com>
2024-07-25 16:50:53 +0545

    Always display persisted attachment in edit view

--- lib/avo/fields/file_field.rb
+++ lib/avo/fields/file_field.rb
@@ -28,10 +28,9 @@ module Avo
       def value
         final_value = super
         # On edit view always show the persisted image. Related: issue#3008
-        if final_value.instance_of?(ActiveStorage::Attached::One) && final_value.image? && view == "edit"
+        if final_value.instance_of?(ActiveStorage::Attached::One) && view == "edit"
           persisted_record = record.class.find_by(id: record.id)
-          property = @for_attribute || id
-          final_value = persisted_record.send(property)
+          final_value = persisted_record.send(@for_attribute || id)
         end
         final_value
       end
--- spec/system/avo/display_persisted_image_spec.rb
+++ spec/system/avo/display_persisted_image_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "DisplayPersistedImage", type: :system do
+RSpec.describe "DisplayPersistedAttachment", type: :system do
   describe "display persisted image on edit view" do
     let(:post) do
       create(:post) do |p|
@@ -21,6 +21,29 @@ RSpec.describe "DisplayPersistedImage", type: :system do
       wait_for_loaded
 
       expect(page).to have_selector 'div[data-field-id="cover_photo"] img'
+    end
+  end
+
+  describe "display persisted audio on edit view" do
+    let(:post) do
+      create(:post) do |p|
+        p.audio.attach(
+          io: File.open(Rails.root.join("db", "seed_files", "dummy-music.mp3")),
+          filename: "dummy-music.mp3",
+          content_type: "audio/mp3"
+        )
+      end
+    end
+
+    it "displays persisted audio on edit view" do
+      visit "/admin/resources/posts/#{post.to_param}/edit"
+      fill_in "Name", with: ""
+      attach_file Rails.root.join("db", "seed_files", "dummy-music-two.mp3"), id: "post_audio"
+
+      click_button "Save"
+      wait_for_loaded
+
+      expect(page).to have_selector 'audio'
     end
   end
 end
