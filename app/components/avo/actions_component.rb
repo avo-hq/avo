be2a68741d0caf1a9827cee5da8aa8acd1f453fc
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2024-08-02 11:23:16 +0300

    Update app/components/avo/actions_component.rb

--- app/components/avo/actions_component.rb
+++ app/components/avo/actions_component.rb
@@ -2,7 +2,6 @@
 
 class Avo::ActionsComponent < Avo::BaseComponent
   include Avo::ApplicationHelper
-  
   ACTION_FILTER = _Set(_Class(Avo::BaseAction))
 
   prop :as_row_control, _Boolean, default: false
