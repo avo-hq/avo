d76033f4714277f34a9858a9482e8c55dc2ab807
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2024-08-05 18:17:48 +0300

    fix: `set_record` when fetching `fields` on actions' `handle` (#3093)
    * feature: hydrate `record` when fetching `fields` on actions' `handle`
    
    * action link improvements

--- app/controllers/avo/actions_controller.rb
+++ app/controllers/avo/actions_controller.rb
@@ -4,7 +4,7 @@ module Avo
   class ActionsController < ApplicationController
     before_action :set_resource_name
     before_action :set_resource
-    before_action :set_record, only: :show, if: ->(request) do
+    before_action :set_record, only: [:show, :handle], if: ->(request) do
       # Try to se the record only if the user is on the record page.
       # set_record will fail if it's tried to be used from the Index page.
       request.params[:id].present?
