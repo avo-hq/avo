b33e5c37424a9d55e184bb635144aaa99d6e5a84
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-05 12:04:58 +0300

    belongs to field tweaks

--- lib/avo/fields/belongs_to_field.rb
+++ lib/avo/fields/belongs_to_field.rb
@@ -204,12 +204,18 @@ module Avo
           record.send(:"#{polymorphic_as}_type=", valid_model_class)
 
           # If the type is blank, reset the id too.
-          if valid_model_class.blank?
+          id_from_param = params["#{polymorphic_as}_id"]
+
+          if valid_model_class.blank? || id_from_param.blank?
             record.send(:"#{polymorphic_as}_id=", nil)
           else
-            record.send(:"#{polymorphic_as}_id=", params["#{polymorphic_as}_id"])
+            record_id = target_resource(record:, polymorphic_model_class: value.safe_constantize).find_record(id_from_param).id
+
+            record.send(:"#{polymorphic_as}_id=", record_id)
           end
         else
+          record_id = value.blank? ? value : target_resource(record:).find_record(value).id
+
           record.send(:"#{key}=", value)
         end
 
@@ -231,7 +237,7 @@ module Avo
         id
       end
 
-      def target_resource
+      def target_resource(record: @record, polymorphic_model_class: value&.class)
         @target_resource ||= if use_resource.present?
           use_resource
         elsif is_polymorphic?
