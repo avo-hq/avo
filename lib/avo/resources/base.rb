40e3798039ade2582ab852195967af5494423e74
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-05 12:05:49 +0300

    lint

--- lib/avo/resources/base.rb
+++ lib/avo/resources/base.rb
@@ -395,7 +395,7 @@ module Avo
         return name if @record.nil?
 
         # Get the title from the record if title is not set, try to get the name, title or label, or fallback to the to_param
-        return @record.try(:name) || @record.try(:title) || @record.try(:label) || @record.to_param  if title.nil?
+        return @record.try(:name) || @record.try(:title) || @record.try(:label) || @record.to_param if title.nil?
 
         # If the title is a symbol, get the value from the record else execute the block/string
         case title
