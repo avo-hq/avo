bb6c07cb9b8995de73575309b8121063a7d9cf6e
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-05 13:05:12 +0300

    wip

--- spec/dummy/app/models/city.rb
+++ spec/dummy/app/models/city.rb
@@ -18,7 +18,7 @@
 #  city_center_area :json
 #
 class City < ApplicationRecord
-  include Hashid::Rails
+  include ::Hashid::Rails
   if Gem::Version.new(Rails.version) >= Gem::Version.new("7.3.0")
     enum :status, {Open: "open", Closed: "closed", Quarantine: "On Quarantine"}
   else
