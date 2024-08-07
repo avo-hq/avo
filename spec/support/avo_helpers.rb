a9697dc8b238174a3453208bae98da137f5d5cef
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-07 16:59:42 +0300

    lint

--- spec/support/avo_helpers.rb
+++ spec/support/avo_helpers.rb
@@ -9,13 +9,13 @@ module TestHelpers
       expect(page).not_to have_css "[data-component-name=\"#{name.to_s.underscore}\"]"
     end
 
-    def with_temporary_class_option(_class, option_name, option_value, &block)
-      previous_value =  _class.send(option_name)
-      _class.send(:"#{option_name}=", option_value)
+    def with_temporary_class_option(klass, option_name, option_value, &block)
+      previous_value =  klass.send(option_name)
+      klass.send(:"#{option_name}=", option_value)
 
       block.call
 
-      _class.send(:"#{option_name}=", previous_value)
+      klass.send(:"#{option_name}=", previous_value)
     end
   end
 end
