d2ee937d5e9a2281cba9a9bc2cd0bd1297c6e301
binarygit <inscription4@outlook.com>
2024-08-08 16:47:25 +0545

    Apply literal to row_component

--- app/components/avo/row_component.rb
+++ app/components/avo/row_component.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class Avo::RowComponent < Avo::BaseComponent
-  attr_reader :classes
-
   renders_one :body
 
-  def initialize(classes: nil, data: {})
-    @classes = classes
-    @data = data
-  end
+  prop :classes, _Nilable(String), reader: :public
+  prop :data, Hash, default: {}.freeze
 end
