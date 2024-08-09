d0cf60fdb93905663f8af440de6391636d209253
binarygit <inscription4@outlook.com>
2024-08-09 16:43:26 +0545

    Apply literal to app/components/avo/fields/common/boolean_check_component.rb

--- app/components/avo/fields/common/boolean_check_component.rb
+++ app/components/avo/fields/common/boolean_check_component.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Avo::Fields::Common::BooleanCheckComponent < Avo::BaseComponent
-  def initialize(checked: false)
-    @icon = checked ? "heroicons/outline/check-circle" : "heroicons/outline/x-circle"
-    @classes = "h-6 #{checked ? "text-green-600" : "text-red-500"}"
+  prop :checked, _Boolean, default: false
+
+  def after_initialize
+    @icon = @checked ? "heroicons/outline/check-circle" : "heroicons/outline/x-circle"
+    @classes = "h-6 #{@checked ? "text-green-600" : "text-red-500"}"
   end
 end
