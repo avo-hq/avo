65b1303e18937e8cc896fdc990a0de9ffa5812f9
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-09 23:07:04 +0300

    fix: pagy 9 `data-turbo-frame`

--- app/components/avo/paginator_component.rb
+++ app/components/avo/paginator_component.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class Avo::PaginatorComponent < Avo::BaseComponent
-  attr_reader :pagy
-  attr_reader :turbo_frame
-  attr_reader :index_params
-  attr_reader :resource
-  attr_reader :parent_record
-  attr_reader :discreet_pagination
-
   def initialize(resource: nil, parent_record: nil, pagy: nil, turbo_frame: nil, index_params: nil, discreet_pagination: nil)
     @pagy = pagy
-    @turbo_frame = turbo_frame
+    @turbo_frame = CGI.escapeHTML(turbo_frame) if turbo_frame
     @index_params = index_params
     @resource = resource
     @parent_record = parent_record
@@ -18,15 +11,15 @@ class Avo::PaginatorComponent < Avo::BaseComponent
   end
 
   def change_items_per_page_url(option)
-    if parent_record.present?
-      helpers.related_resources_path(parent_record, parent_record, per_page: option, keep_query_params: true, page: 1)
+    if @parent_record.present?
+      helpers.related_resources_path(@parent_record, @parent_record, per_page: option, keep_query_params: true, page: 1)
     else
-      helpers.resources_path(resource: resource, per_page: option, keep_query_params: true, page: 1)
+      helpers.resources_path(resource: @resource, per_page: option, keep_query_params: true, page: 1)
     end
   end
 
   def render?
-    return false if discreet_pagination && pagy.pages <= 1
+    return false if @discreet_pagination && @pagy.pages <= 1
 
     if ::Pagy::VERSION >= ::Gem::Version.new("9.0")
       @pagy.limit > 0
@@ -37,9 +30,9 @@ class Avo::PaginatorComponent < Avo::BaseComponent
 
   def per_page_options
     @per_page_options ||= begin
-      options = [*Avo.configuration.per_page_steps, Avo.configuration.per_page.to_i, index_params[:per_page].to_i]
+      options = [*Avo.configuration.per_page_steps, Avo.configuration.per_page.to_i, @index_params[:per_page].to_i]
 
-      if parent_record.present?
+      if @parent_record.present?
         options.prepend Avo.configuration.via_per_page
       end
 
