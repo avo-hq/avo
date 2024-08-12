fd33e40f45bcb63c03825b7401741832e6341c03
binarygit <inscription4@outlook.com>
2024-08-12 20:11:23 +0545

    Iterate over only groups to check if there are any visible items

--- app/components/avo/sidebar/section_component.rb
+++ app/components/avo/sidebar/section_component.rb
@@ -2,12 +2,24 @@
 
 class Avo::Sidebar::SectionComponent < Avo::Sidebar::BaseItemComponent
   def render?
-    super && items.any? { |group| group.items.find(&:visible?) }
+    items.any? do |item| 
+      if item.is_a? Avo::Menu::Group
+        item.items.find(&:visible?)
+      else
+        true
+      end
+    end
   end
 
   def icon
     return nil if item.icon.nil?
 
     item.icon
+  end
+
+  private
+
+  def groups
+    items.select {|item| item.is_a? Avo::Menu::Group}
   end
 end
