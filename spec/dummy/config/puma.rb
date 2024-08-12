68ed1eaed813e14584d50fb96cc4db4c78c3063e
Adrian Marin <adrian@adrianthedev.com>
2024-08-12 13:27:38 +0300

    chore: update CI files and a few tweaks

--- spec/dummy/config/puma.rb
+++ spec/dummy/config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
