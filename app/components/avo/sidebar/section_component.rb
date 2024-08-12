6343b51074e401d2263add86ae4e8ac2c6ee706e
binarygit <inscription4@outlook.com>
2024-08-12 18:37:16 +0545

    Hide section if group has no visible elements

--- app/components/avo/sidebar/section_component.rb
+++ app/components/avo/sidebar/section_component.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Avo::Sidebar::SectionComponent < Avo::Sidebar::BaseItemComponent
+  def render?
+    super && items.any? { |group| group.items.find(&:visible?) }
+  end
+
   def icon
     return nil if item.icon.nil?
 
