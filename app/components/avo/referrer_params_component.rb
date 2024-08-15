7078b17c99cad343725de68a4f06a46bcdc8ad6b
binarygit <inscription4@outlook.com>
2024-08-15 15:05:51 +0545

    Remove reader from app/components/avo/referrer_params_component.rb

--- app/components/avo/referrer_params_component.rb
+++ app/components/avo/referrer_params_component.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class Avo::ReferrerParamsComponent < Avo::BaseComponent
-
-  prop :back_path, _Nilable(String), reader: :public
+  prop :back_path, _Nilable(String)
 end
