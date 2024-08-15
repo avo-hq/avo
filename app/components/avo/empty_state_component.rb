5678836ccfc4a6370e3ece64f16391d9348b2b45
binarygit <inscription4@outlook.com>
2024-08-15 13:23:04 +0545

    Remove reader from app/components/avo/empty_state_component.rb

--- app/components/avo/empty_state_component.rb
+++ app/components/avo/empty_state_component.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Avo::EmptyStateComponent < Avo::BaseComponent
-  attr_reader :message, :view_type, :add_background, :by_association
-
   prop :message, _Nilable(String)
   prop :view_type, Symbol, default: :table do |value|
     value&.to_sym
@@ -11,16 +9,16 @@ class Avo::EmptyStateComponent < Avo::BaseComponent
   prop :by_association, _Boolean, default: false
 
   def text
-    message || locale_message
+    @message || locale_message
   end
 
   def view_type_svg
-    "avo/#{view_type}-empty-state"
+    "avo/#{@view_type}-empty-state"
   end
 
   private
 
   def locale_message
-    helpers.t by_association ? "avo.no_related_item_found" : "avo.no_item_found"
+    helpers.t @by_association ? "avo.no_related_item_found" : "avo.no_item_found"
   end
 end
