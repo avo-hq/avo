da7088faa8c358c1eba973c105e8b2d3df55abd7
binarygit <inscription4@outlook.com>
2024-08-15 13:27:48 +0545

    Remove reader from app/components/avo/fields/tags_field/tag_component.rb

--- app/components/avo/fields/tags_field/tag_component.rb
+++ app/components/avo/fields/tags_field/tag_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Avo::Fields::TagsField::TagComponent < Avo::BaseComponent
-  prop :label, _Nilable(String), reader: :public
-  prop :title, _Nilable(String), reader: :public
+  prop :label, _Nilable(String)
+  prop :title, _Nilable(String)
 end
