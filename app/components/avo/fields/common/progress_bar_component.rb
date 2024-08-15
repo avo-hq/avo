fd6064aed01f31adba27709d585ed7c7ba3f0042
binarygit <inscription4@outlook.com>
2024-08-15 13:37:20 +0545

    Remove reader from app/components/avo/fields/common/progress_bar_component.rb

--- app/components/avo/fields/common/progress_bar_component.rb
+++ app/components/avo/fields/common/progress_bar_component.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Avo::Fields::Common::ProgressBarComponent < Avo::BaseComponent
-  prop :value, Integer, reader: :public
-  prop :display_value, _Boolean, default: false, reader: :public
-  prop :value_suffix, _Nilable(String), reader: :public
-  prop :max, Integer, default: 100, reader: :public
+  prop :value, Integer
+  prop :display_value, _Boolean, default: false
+  prop :value_suffix, _Nilable(String)
+  prop :max, Integer, default: 100
   prop :view, Avo::ViewInquirer, reader: :public do |value|
     value = :index if value.nil?
     Avo::ViewInquirer.new(value.to_sym)
