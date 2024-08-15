f38b9e4abcebb84388a72fdfab0983545f8cb35a
binarygit <inscription4@outlook.com>
2024-08-15 17:27:53 +0545

    Remove after_initialize for app/components/avo/fields/common/boolean_check_component.rb

--- app/components/avo/fields/common/boolean_check_component.rb
+++ app/components/avo/fields/common/boolean_check_component.rb
@@ -2,9 +2,10 @@
 
 class Avo::Fields::Common::BooleanCheckComponent < Avo::BaseComponent
   prop :checked, _Boolean, default: false
-
-  def after_initialize
-    @icon = @checked ? "heroicons/outline/check-circle" : "heroicons/outline/x-circle"
-    @classes = "h-6 #{@checked ? "text-green-600" : "text-red-500"}"
+  prop :icon, _Nilable(String) do |value|
+    @checked ? "heroicons/outline/check-circle" : "heroicons/outline/x-circle"
+  end
+  prop :classes, _Nilable(String) do |value|
+    "h-6 #{@checked ? "text-green-600" : "text-red-500"}"
   end
 end
