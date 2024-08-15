b76368866ac87e97b01cb1113ba77f0f06949f60
binarygit <inscription4@outlook.com>
2024-08-15 16:09:06 +0545

    Delegate id to @field

--- app/components/avo/fields/common/files/controls_component.rb
+++ app/components/avo/fields/common/files/controls_component.rb
@@ -4,13 +4,13 @@ class Avo::Fields::Common::Files::ControlsComponent < Avo::BaseComponent
   include Avo::ApplicationHelper
   include Avo::Fields::Concerns::FileAuthorization
 
-  delegate :id, to: :field
+  delegate :id, to: :@field
 
   prop :field, Avo::Fields::BaseField
   prop :file, ActiveStorage::Attachment
   prop :resource, Avo::BaseResource
 
   def destroy_path
-    Avo::Services::URIService.parse(@resource.record_path).append_paths("active_storage_attachments", id, file.id).to_s
+    Avo::Services::URIService.parse(@resource.record_path).append_paths("active_storage_attachments", id, @file.id).to_s
   end
 end
