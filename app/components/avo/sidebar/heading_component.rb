0d461fb9014c370e5405f59c2c765b9241eb3e84
binarygit <inscription4@outlook.com>
2024-08-15 15:05:25 +0545

    Remove reader from app/components/avo/turbo_frame_wrapper_component.rb

--- app/components/avo/sidebar/heading_component.rb
+++ app/components/avo/sidebar/heading_component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Avo::Sidebar::HeadingComponent < Avo::BaseComponent
-  prop :label, _Nilable(String), reader: :public
-  prop :icon, _Nilable(String), reader: :public
-  prop :collapsable, _Boolean, default: false, reader: :public
-  prop :collapsed, _Boolean, default: false, reader: :public
-  prop :key, _Nilable(_Boolean), default: false, reader: :public
+  prop :label, _Nilable(String)
+  prop :icon, _Nilable(String)
+  prop :collapsable, _Boolean, default: false
+  prop :collapsed, _Boolean, default: false
+  prop :key, _Nilable(_Boolean), default: false
 end
--- app/components/avo/turbo_frame_wrapper_component.rb
+++ app/components/avo/turbo_frame_wrapper_component.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Avo::TurboFrameWrapperComponent < Avo::BaseComponent
-  prop :name, _Nilable(String), :positional, reader: :public
+  prop :name, _Nilable(String), :positional
 end
