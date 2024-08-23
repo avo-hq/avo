32807e290a3bee274d99c43d21f8b49ec27499d5
Paul Bob <paul.ionut.bob@gmail.com>
2024-08-23 16:46:06 +0300

    :_top symbol instead "_top" string

--- app/components/avo/paginator_component.rb
+++ app/components/avo/paginator_component.rb
@@ -4,8 +4,8 @@ class Avo::PaginatorComponent < Avo::BaseComponent
   prop :resource, _Nilable(Avo::BaseResource)
   prop :parent_record, _Nilable(ActiveRecord::Base)
   prop :pagy, _Nilable(Pagy)
-  prop :turbo_frame, _Nilable(String) do |frame|
-    frame.present? ? CGI.escapeHTML(frame) : "_top"
+  prop :turbo_frame, _Nilable(_Union(String, Symbol)) do |frame|
+    frame.present? ? CGI.escapeHTML(frame) : :_top
   end
   prop :index_params, _Nilable(Hash)
   prop :discreet_pagination, _Nilable(_Boolean)
