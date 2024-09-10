cb37696149b1f2c3c421ba3474a15b2b3d1ae007
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-10 10:08:25 +0300

    lint

--- lib/avo/concerns/find_association_field.rb
+++ lib/avo/concerns/find_association_field.rb
@@ -2,7 +2,7 @@ module Avo
   module Concerns
     module FindAssociationField
       def find_association_field(resource:, association:)
-        if params[:turbo_frame] && params[:turbo_frame].starts_with?("has_")
+        if params[:turbo_frame]&.starts_with?("has_")
           type = params[:turbo_frame][/.*(?=_field)/]
 
           resource.get_field_definitions.find do |field|
