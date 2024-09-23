4dca5857f247d39d15cb1bd64d76013b5028930c
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-23 11:51:28 +0300

    remove direct audit call

--- app/controllers/avo/actions_controller.rb
+++ app/controllers/avo/actions_controller.rb
@@ -49,23 +49,16 @@ module Avo
     def handle
       resource_ids = action_params[:fields][:avo_resource_ids].split(",")
 
-      query = decrypted_query || (resource_ids.any? ? @resource.find_record(resource_ids, params: params) : [])
-      fields = action_params[:fields].except(:avo_resource_ids, :avo_selected_query)
+      @query = decrypted_query || (resource_ids.any? ? @resource.find_record(resource_ids, params: params) : [])
+      @fields = action_params[:fields].except(:avo_resource_ids, :avo_selected_query)
 
-      safe_call :audit,
-        activity_class: @action.class,
-        payload: {
-          fields: fields,
-          resource: resource,
-        },
-        action: __method__,
-        records: query
+      instance_exec(__method__, &audit)
 
       performed_action = @action.handle_action(
-        fields: fields,
+        fields: @fields,
         current_user: _current_user,
         resource: @resource,
-        query: query
+        query: @query
       )
 
       @response = performed_action.response
