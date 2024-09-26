e1faed7c03170b96032446b651303d24c1bbc282
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-26 19:37:50 +0300

    lint

--- spec/system/avo/tabs_spec.rb
+++ spec/system/avo/tabs_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Tabs", type: :system do
   let!(:user) { create :user, birthday: "10.02.1988" }
-  let!(:projects) {create_list :project, 9, users: [user]}
+  let!(:projects) { create_list :project, 9, users: [user] }
 
   describe "doesn't display tabs content" do
     context "on index" do
@@ -178,11 +178,11 @@ RSpec.describe "Tabs", type: :system do
     visit avo.resources_user_path user
 
     find('a[data-selected="false"][data-tabs-tab-name-param="Projects"]').click
-    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: '1')
+    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: "1")
     expect(page).to have_text "Displaying items 1-8 of 9 in total"
 
-    find('a[data-turbo-frame="has_and_belongs_to_many_field_show_projects"]', text: '2').click
-    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: '2')
+    find('a[data-turbo-frame="has_and_belongs_to_many_field_show_projects"]', text: "2").click
+    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: "2")
     expect(page).to have_text "Displaying items 9-9 of 9 in total"
 
     find('a[aria-label="View project"]').click
@@ -191,7 +191,7 @@ RSpec.describe "Tabs", type: :system do
     page.go_back
     wait_for_loaded
 
-    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: '2')
+    expect(page).to have_css('a.current[role="link"][aria-disabled="true"][aria-current="page"]', text: "2")
     expect(page).to have_text "Displaying items 9-9 of 9 in total"
   end
 end
