e15823b3410a7c01c1c331ff9c23cbac533bf70a
Adrian Marin <adrian@adrianthedev.com>
2024-09-29 12:03:23 +0100

    refactor: boolean group and checkbox improvements

--- app/components/avo/fields/common/boolean_check_component.rb
+++ app/components/avo/fields/common/boolean_check_component.rb
@@ -2,10 +2,18 @@
 
 class Avo::Fields::Common::BooleanCheckComponent < Avo::BaseComponent
   prop :checked, _Boolean, default: false
+  prop :size, Symbol, default: :md
   prop :icon, _Nilable(String) do |value|
     @checked ? "heroicons/outline/check-circle" : "heroicons/outline/x-circle"
   end
-  prop :classes, _Nilable(String) do |value|
-    "h-6 #{@checked ? "text-green-600" : "text-red-500"}"
+  prop :classes, _Nilable(String)
+
+  def classes
+    helpers.class_names({
+      "h-5": @size == :sm,
+      "h-6": @size == :md,
+      "text-green-600": @checked,
+      "text-red-600": !@checked,
+    })
   end
 end
