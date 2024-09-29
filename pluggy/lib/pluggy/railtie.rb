c51008c9dcf8cc5c88ca797735c2d5b413cb4ee8
Adrian Marin <adrian@adrianthedev.com>
2024-09-29 10:00:08 +0300

    refactor: plugin rework (#3259)
    * refactor: plugin rework
    
    * wip
    
    ---------
    
    Co-authored-by: Paul Bob <69730720+Paul-Bob@users.noreply.github.com>

--- pluggy/lib/pluggy/railtie.rb
+++ pluggy/lib/pluggy/railtie.rb
@@ -1,7 +1,11 @@
 module Pluggy
   class Railtie < Rails::Railtie
     initializer "pluggy.init" do
-      Avo.plugin_manager.register Pluggy::Plugin
+      ActiveSupport.on_load(:avo_boot) do
+        Avo.plugin_manager.register :pluggy
+
+        Avo.plugin_manager.register_field :radio, Pluggy::Fields::RadioField
+      end
     end
   end
 end
