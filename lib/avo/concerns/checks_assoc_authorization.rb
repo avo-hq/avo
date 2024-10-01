27d82119f70c97bf87129ab9731ea844bdf16d75
Paul Bob <paul.ionut.bob@gmail.com>
2024-10-01 20:24:30 +0300

    improve authorize_association_for

--- lib/avo/concerns/checks_assoc_authorization.rb
+++ lib/avo/concerns/checks_assoc_authorization.rb
@@ -7,6 +7,11 @@ module Avo
       def authorize_association_for(policy_method)
         return true if @reflection.blank?
 
+        # Use the related_name as the base of the association
+        association_name = @reflection.name
+
+        return true if association_name.blank?
+
         # Fetch the appropriate resource
         reflection_resource = field.resource
 
@@ -14,21 +19,12 @@ module Avo
         # Hydrate the resource with the record if we have one
         reflection_resource.hydrate(record: @parent_record) if @parent_record.present?
 
-        # Use the related_name as the base of the association
-        association_name = @reflection.name
-
-        return true if association_name.blank?
-
-        method_name = :"#{policy_method}_#{association_name}?".to_sym
-
         # Some policy methods should get the parent record in order to have the necessary information to do the authorization
         # Example: Post->has_many->Comments
         #
         # When you want to authorize the creation/attaching of a Comment, you don't have the Comment instance.
         # But you do have the Post instance and you can get that in your policy to authorize against.
-        parent_policy_methods = [:view, :create, :attach, :act_on]
-
-        record = if parent_policy_methods.include?(policy_method)
+        record = if policy_method.in? [:view, :create, :attach, :act_on]
           # Use the parent record (Post)
           reflection_resource.record
         else
@@ -37,7 +33,11 @@ module Avo
         end
 
         # Use the policy methods from the parent (Post)
-        reflection_resource.authorization.authorize_action(method_name, record: record, raise_exception: false)
+        reflection_resource.authorization.authorize_action(
+          :"#{policy_method}_#{association_name}?".to_sym,
+          record:,
+          raise_exception: false
+        )
       end
     end
   end
