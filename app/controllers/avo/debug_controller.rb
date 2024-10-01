134de10e45b9f356bdab05b6079a1447782d89e7
Adrian Marin <adrian@adrianthedev.com>
2024-10-01 10:20:51 +0300

    refactor: plugins reporting and versions hiding (#3287)

--- app/controllers/avo/debug_controller.rb
+++ app/controllers/avo/debug_controller.rb
@@ -2,7 +2,13 @@ require_dependency "avo/application_controller"
 
 module Avo
   class DebugController < ApplicationController
+    before_action :authenticate_developer_or_admin!
+
     def status
+      respond_to do |format|
+        format.html { render :status }
+        format.text { render :status }
+      end
     end
 
     def send_to_hq
