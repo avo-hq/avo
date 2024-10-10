14740aa1265512ed2e0299f63a1709340c90a43d
Yash Singh Pathania <newtoneinstienfarady@gmail.com>
2024-10-10 14:31:01 +0100

    Linting Fix

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
