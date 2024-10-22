57084142620ed05e5eed3b1b60b3a72158a7372d
Paul Bob <paul.ionut.bob@gmail.com>
2024-10-22 18:37:09 +0300

    comment

--- spec/dummy/config/initializers/avo.rb
+++ spec/dummy/config/initializers/avo.rb
@@ -32,6 +32,7 @@ Avo.configure do |config|
   config.locale = :en
   # config.raise_error_on_missing_policy = true
   # config.authorization_client = "Avo::Services::AuthorizationClients::ExtraPunditClient"
+  # Shouldn't impact on community only if custom authorization service was configured.
   config.implicit_authorization = true
 
   ## == Customization ==
