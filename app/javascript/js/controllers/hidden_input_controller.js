5dc169c6c454895174ba00086fa329106b4278e4
zhephyn <zhephynwinter@gmail.com>
2024-10-27 02:45:56 +0300

    Refactor for hide/show content feature for Trix fields

--- app/javascript/js/controllers/hidden_input_controller.js
+++ app/javascript/js/controllers/hidden_input_controller.js
@@ -4,14 +4,14 @@ export default class extends Controller {
   static targets = ['content', 'hideContentLink', 'showContentLink']
 
   showContent(event) {
-    this.contentTarget.classList.toggle('hidden')
-    event.target.classList.add('hidden')
-    this.hideContentLinkTarget.classList.remove('hidden')
+    this.contentTarget.classList.remove('hidden') // Make content visible
+    this.showContentLinkTarget.classList.add('hidden') // Hide "Show content" link
+    this.hideContentLinkTarget.classList.remove('hidden') // Show "Hide content" link
   }
 
   hideContent(event) {
-    this.contentTarget.classList.add('hidden')
-    event.target.classList.add('hidden')
-    this.showContentLink.classList.remove('hidden')
+    this.contentTarget.classList.add('hidden') // Hide content
+    this.hideContentLinkTarget.classList.add('hidden') // Hide "Hide content" link
+    this.showContentLinkTarget.classList.remove('hidden') // Show "Show content" link
   }
 }
