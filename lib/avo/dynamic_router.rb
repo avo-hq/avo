29a21d44f64a10233b84d09b2baccbfc076acc36
David Lormor <davidlormor@gmail.com>
2024-10-29 15:13:45 -0500

    Fix issue with loading dynamic routes
    Per the Rails guides, you should not call `Avo::Engine.routes.draw` again when loading routes from another file.
    
    See https://guides.rubyonrails.org/routing.html#breaking-up-very-large-route-file-into-multiple-small-ones for details.

--- lib/avo/dynamic_router.rb
+++ lib/avo/dynamic_router.rb
@@ -1,14 +1,10 @@
 module Avo
   class DynamicRouter
     def self.routes
-      Avo::Engine.routes.draw do
-        scope "resources", as: "resources" do
-          Avo::Resources::ResourceManager.fetch_resources.map do |resource|
-            resources resource.route_key do
-              member do
-                get :preview
-              end
-            end
+      Avo::Resources::ResourceManager.fetch_resources.map do |resource|
+        resources resource.route_key do
+          member do
+            get :preview
           end
         end
       end
