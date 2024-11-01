dda9c25f79dfd274d9cdebd22cf532ba71dc277e
Paul Bob <paul.ionut.bob@gmail.com>
2024-11-01 11:15:37 +0200

    reorder_text & DRY getters

--- lib/avo/fields/key_value_field.rb
+++ lib/avo/fields/key_value_field.rb
@@ -5,16 +5,18 @@ module Avo
     class KeyValueField < BaseField
       attr_reader :disable_editing_keys
       attr_reader :disable_adding_rows
+      attr_reader :key_label, :value_label, :action_text
 
       def initialize(id, **args, &block)
         super(id, **args, &block)
 
         hide_on :index
 
-        @key_label = args[:key_label] if args[:key_label].present?
-        @value_label = args[:value_label] if args[:value_label].present?
-        @action_text = args[:action_text] if args[:action_text].present? # This should be add_row_label
-        @delete_text = args[:delete_text] if args[:delete_text].present?
+        @key_label = args[:key_label] || I18n.translate("avo.key_value_field.key")
+        @value_label = args[:value_label] || I18n.translate("avo.key_value_field.value")
+        @action_text = args[:action_text] || I18n.translate("avo.key_value_field.add_row")
+        @delete_text = args[:delete_text] || I18n.translate("avo.key_value_field.delete_row")
+        @reorder_text = args[:reorder_text] || I18n.translate("avo.key_value_field.reorder_row")
 
         if args[:disabled] == true
           @disable_editing_keys = true
@@ -36,30 +38,6 @@ module Avo
         end
       end
 
-      def key_label
-        return @key_label if @key_label.present?
-
-        I18n.translate("avo.key_value_field.key")
-      end
-
-      def value_label
-        return @value_label if @value_label.present?
-
-        I18n.translate("avo.key_value_field.value")
-      end
-
-      def action_text
-        return @action_text if @action_text.present?
-
-        I18n.translate("avo.key_value_field.add_row")
-      end
-
-      def delete_text
-        return @delete_text if @delete_text.present?
-
-        I18n.translate("avo.key_value_field.delete_row")
-      end
-
       def to_permitted_param
         [:"#{id}", "#{id}": {}]
       end
@@ -72,10 +50,11 @@ module Avo
 
       def options
         {
-          key_label: key_label,
-          value_label: value_label,
-          action_text: action_text,
-          delete_text: delete_text,
+          key_label: @key_label,
+          value_label: @value_label,
+          action_text: @action_text,
+          delete_text: @delete_text,
+          reorder_text: @reorder_text,
           disable_editing_keys: @disable_editing_keys,
           disable_editing_values: @disable_editing_values,
           disable_adding_rows: @disable_adding_rows,
