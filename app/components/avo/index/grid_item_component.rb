4489117b26dbbb00d239615ebb576b004d5579b3
Adrian Marin <adrian@adrianthedev.com>
2024-12-02 14:53:28 +0200

    feature: grid item badge

--- app/components/avo/index/grid_item_component.rb
+++ app/components/avo/index/grid_item_component.rb
@@ -65,4 +65,16 @@ class Avo::Index::GridItemComponent < Avo::BaseComponent
       @card[:body]
     end
   end
+
+  def render_badge
+    return if @card[:badge_label].blank?
+
+    content_tag :div, class: "absolute block inset-auto top-0 right-0 mt-2 mr-2 text-sm font-semibold bg-#{badge_color}-50 border border-#{badge_color}-300 text-#{badge_color}-700 rounded shadow-lg px-2 py-px z-10 #{html(:badge, :classes)}", style: html(:badge, :style) do
+      @card[:badge_label]
+    end
+  end
+
+  def badge_color
+    @card[:badge_color] || "gray"
+  end
 end
