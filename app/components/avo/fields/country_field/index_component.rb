c964d47b3b67697a62cbfe95048f9c7e043b5404
Nevelito <szymonnewel@gmail.com>
2024-12-10 16:20:25 +0100

    Self code review

--- app/components/avo/fields/country_field/index_component.rb
+++ app/components/avo/fields/country_field/index_component.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class Avo::Fields::CountryField::IndexComponent < Avo::Fields::IndexComponent
-  private
-
-  def field_value
-    @field.display_code ? @field.value : @field.countries[@field.value]
-  end
 end
--- app/components/avo/fields/country_field/show_component.rb
+++ app/components/avo/fields/country_field/show_component.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class Avo::Fields::CountryField::ShowComponent < Avo::Fields::ShowComponent
-  private
-
-  def field_value
-    @field.display_code ? @field.value : @field.countries[@field.value]
-  end
 end
