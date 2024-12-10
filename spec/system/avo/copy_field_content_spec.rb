013c52600ee0fb4357fd195d8ff0485e323335ca
Nevelito <szymonnewel@gmail.com>
2024-12-10 16:52:27 +0100

    Fix copy firld content spec

--- spec/system/avo/copy_field_content_spec.rb
+++ spec/system/avo/copy_field_content_spec.rb
@@ -5,26 +5,26 @@ RSpec.describe "CopyFieldContent", type: :system do
 
   def test_copy_to_clipboard(path)
     visit path
-    element = find('div[data-controller="clipboard"]')
-    element.hover
-    find('button[data-action="clipboard#copy"]').click
+    container = find('div[data-controller="clipboard"]', visible: :all)
+    expect(container).to be_present
+    container.hover
+    container.find('button[data-action="clipboard#copy"]', visible: :all).click
 
-    expect(page).to have_css('svg[class*="clipboard-document-check"]')
+    expect(container).to have_css('svg[class*="clipboard-document-check"]', visible: :all, wait: 2)
+  end
 
-    sleep 2
-    expect(page).to have_no_css('svg[class*="clipboard-document-check"]')
+  def test_button_visability(path)
+    visit path
+
+    element = find('div[data-controller="clipboard"]', visible: :all)
+    expect(element).to be_present
   end
 
   describe "index view" do
     let(:path) { "/admin/resources/projects" }
 
     it "shows copy to clipboard icon" do
-      visit path
-
-      element = find('div[data-controller="clipboard"]')
-      element.hover
-
-      expect(page).to have_css('svg[class*="clipboard"]')
+      test_button_visability(path)
     end
 
     it "copies to clipboard after clicking button" do
@@ -36,12 +36,7 @@ RSpec.describe "CopyFieldContent", type: :system do
     let(:path) { "/admin/resources/projects/#{project.id}" }
 
     it "shows copy to clipboard icon" do
-      visit path
-
-      element = find('div[data-controller="clipboard"]')
-      element.hover
-
-      expect(page).to have_css('svg[class*="clipboard"]')
+      test_button_visability(path)
     end
 
     it "copies to clipboard after clicking button" do
