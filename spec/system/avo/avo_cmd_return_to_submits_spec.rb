f289f899d49aaa3369ef002e09d596c46736a00c
Paul Bob <paul.ionut.bob@gmail.com>
2024-12-16 03:51:38 +0200

    fix test

--- spec/system/avo/avo_cmd_return_to_submits_spec.rb
+++ spec/system/avo/avo_cmd_return_to_submits_spec.rb
@@ -1,16 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "CmdReturnToSubmits", type: :system do
-  let(:admin) { create :user, roles: {admin: true} }
   let(:project) { create :project }
-  before do
-    login_as admin
-  end  # include TestHelpers::DisableAuthentication
 
   it "submits form when cmd+enter is pressed" do
     visit "/admin/resources/projects/#{project.id}/edit"
-    element = find("input[name='project[name]']")
-    element.send_keys [:control, :enter]
-    expect(page).to have_text "Project updated"
+    find("input[name='project[name]']").send_keys [:control, :enter]
+    expect(page).to have_text "Project was successfully updated"
   end
 end
