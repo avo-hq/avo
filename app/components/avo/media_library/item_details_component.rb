013feea3b89ea70afcfe9a339537484c48c262f3
Adrian Marin <adrian@adrianthedev.com>
2024-12-29 16:15:07 +0200

    wip

--- app/components/avo/media_library/item_details_component.rb
+++ app/components/avo/media_library/item_details_component.rb
@@ -8,15 +8,15 @@ module Avo
 
       prop :attachment
 
-      def parent_title(parent)
-        # TODO: find the resource and get the title attribute from there
-        parent.to_param
-      end
+      # def parent_title(parent)
+      #   # TODO: find the resource and get the title attribute from there
+      #   parent.to_param
+      # end
 
-      def parent_path(parent)
-        # TODO: find the resource and get the path from there
-        parent.to_param
-      end
+      # def parent_path(parent)
+      #   # TODO: find the resource and get the path from there
+      #   parent.to_param
+      # end
     end
   end
 end
