d7fd7b6c3c5c5feefb1facc0c50444d35eb19134
Edmund Tay <edmundtay96@gmail.com>
2025-01-03 11:29:58 +0800

    fix: reject generator failing due to undefined source_location

--- lib/generators/avo/eject_generator.rb
+++ lib/generators/avo/eject_generator.rb
@@ -120,7 +120,7 @@ module Generators
           # Get the component path for both, the rb and erb files
           rb, erb = if (component_constant = component_to_eject.safe_constantize)
             # If component is a constant, find the source location
-            source_location = component_constant.source_location
+            source_location = component_constant.new.method(component_constant.instance_methods.first).source_location.first
 
             [source_location, source_location.gsub(".rb", ".html.erb")]
           else
