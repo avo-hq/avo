41558328e3327969376b5c9f500cf13ebaa17d73
Adrian Marin <adrian@adrianthedev.com>
2025-01-08 17:15:08 +0200

    wip

--- spec/system/avo/avo_record_selectors_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "RecordSelectors", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
-end
