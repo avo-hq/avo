16dd5f9ae40c62589dcc3876b754900853562f6b
Paul Bob <paul.ionut.bob@gmail.com>
2025-01-10 13:06:26 +0200

    make it work with array of active records

--- lib/avo/resources/array_resource.rb
+++ lib/avo/resources/array_resource.rb
@@ -11,20 +11,25 @@ module Avo
 
       class << self
         def model_class
-          ActiveSupport::OrderedOptions.new.tap do |obj|
+          @@model_class ||= ActiveSupport::OrderedOptions.new.tap do |obj|
             obj.model_name = ActiveSupport::OrderedOptions.new.tap do |thing|
-              thing.plural = "Array"
+              thing.plural = route_key
             end
           end
         end
       end
 
       def find_record(id, query: nil, params: nil)
+        return super if array_of_active_records?
+
         fetch_records.find { |i| i.id.to_s == id.to_s }
       end
 
       def fetch_records
-        @fetched_records ||= begin
+        @fetched_records ||= if array_of_active_records?
+          @@model_class = records.model
+          records
+        else
           # Dynamically create a class with accessors for all unique keys from the records
           keys = records.flat_map(&:keys).uniq
 
@@ -44,6 +49,10 @@ module Avo
             custom_class.new(item)
           end
         end
+      end
+
+      def array_of_active_records?
+        @array_of_active_records ||= records.is_a?(ActiveRecord::Relation)
       end
     end
   end
