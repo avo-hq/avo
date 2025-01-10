ac6625833ef084dbbe5ac7ccdffdedd6c197e2e0
ObiWanKeoni <keoni.garner@fabrichealth.com>
2025-01-09 17:16:05 -0800

    Rubocop / Refactor for readability + solving problems with select inputs

--- lib/avo/concerns/has_field_discovery.rb
+++ lib/avo/concerns/has_field_discovery.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
+# TODO: Refactor this concern to be more readable and maintainable
+# rubocop:disable Metrics/ModuleLength
 module Avo
   module Concerns
-    # This concern facilitates field discovery for models in Avo, mapping database columns and associations to Avo fields.
+    # This concern facilitates field discovery for models in Avo,
+    # mapping database columns and associations to Avo fields.
     # It supports:
     #  - Automatic detection of fields based on column names, types, and associations.
     #  - Customization via `only`, `except`, and global configuration overrides.
@@ -9,35 +14,35 @@ module Avo
       extend ActiveSupport::Concern
 
       DEFAULT_COLUMN_NAMES_MAPPING = {
-        id: { field: "id" },
-        description: { field: "textarea" },
-        gravatar: { field: "gravatar" },
-        email: { field: "text" },
-        password: { field: "password" },
-        password_confirmation: { field: "password" },
-        created_at: { field: "date_time" },
-        updated_at: { field: "date_time" },
-        stage: { field: "select" },
-        budget: { field: "currency" },
-        money: { field: "currency" },
-        country: { field: "country" },
+        id: { field: 'id' },
+        description: { field: 'textarea' },
+        gravatar: { field: 'gravatar' },
+        email: { field: 'text' },
+        password: { field: 'password' },
+        password_confirmation: { field: 'password' },
+        created_at: { field: 'date_time' },
+        updated_at: { field: 'date_time' },
+        stage: { field: 'select' },
+        budget: { field: 'currency' },
+        money: { field: 'currency' },
+        country: { field: 'country' }
       }.freeze
 
       DEFAULT_COLUMN_TYPES_MAPPING = {
-        primary_key: { field: "id" },
-        string: { field: "text" },
-        text: { field: "textarea" },
-        integer: { field: "number" },
-        float: { field: "number" },
-        decimal: { field: "number" },
-        datetime: { field: "date_time" },
-        timestamp: { field: "date_time" },
-        time: { field: "date_time" },
-        date: { field: "date" },
-        binary: { field: "number" },
-        boolean: { field: "boolean" },
-        references: { field: "belongs_to" },
-        json: { field: "code" },
+        primary_key: { field: 'id' },
+        string: { field: 'text' },
+        text: { field: 'textarea' },
+        integer: { field: 'number' },
+        float: { field: 'number' },
+        decimal: { field: 'number' },
+        datetime: { field: 'date_time' },
+        timestamp: { field: 'date_time' },
+        time: { field: 'date_time' },
+        date: { field: 'date' },
+        binary: { field: 'number' },
+        boolean: { field: 'boolean' },
+        references: { field: 'belongs_to' },
+        json: { field: 'code' }
       }.freeze
 
       COLUMN_NAMES_TO_IGNORE = %i[
@@ -47,12 +52,12 @@ module Avo
       class_methods do
         def column_names_mapping
           @column_names_mapping ||= DEFAULT_COLUMN_NAMES_MAPPING.dup
-                                    .merge(Avo.configuration.column_names_mapping || {})
+                                                                .merge(Avo.configuration.column_names_mapping || {})
         end
 
         def column_types_mapping
           @column_types_mapping ||= DEFAULT_COLUMN_TYPES_MAPPING.dup
-                                    .merge(Avo.configuration.column_types_mapping || {})
+                                                                .merge(Avo.configuration.column_types_mapping || {})
         end
       end
 
@@ -63,35 +68,88 @@ module Avo
 
       # Discovers and configures database columns as fields
       def discover_columns(only: nil, except: nil, **field_options)
-        @only, @except, @field_options = only, except, field_options
+        setup_discovery_options(only, except, field_options)
         return unless safe_model_class.respond_to?(:columns_hash)
 
-        model_db_columns.each do |column_name, column|
-          next unless column_in_scope?(column_name)
-          next if reflections.key?(column_name) || rich_texts.key?("rich_text_#{column_name}")
-
-          field_config = determine_field_config(column_name, column)
-          next unless field_config
-
-          field_options = build_field_options(field_config, column)
-          field column_name, **field_options, **@field_options
+        discoverable_columns.each do |column_name, column|
+          process_column(column_name, column)
         end
+
+        discover_by_type(tags, :tags) { |name| name.split('_').pop.join('_').pluralize }
+        discover_by_type(rich_texts, :trix) { |name| name.to_s.delete_prefix('rich_text_') }
       end
 
       # Discovers and configures associations as fields
       def discover_associations(only: nil, except: nil, **field_options)
-        @only, @except, @field_options = only, except, field_options
+        setup_discovery_options(only, except, field_options)
         return unless safe_model_class.respond_to?(:reflections)
 
-        discover_by_type(tags, :tags) { |name| name.split("_").pop.join("_").pluralize }
-        discover_by_type(rich_texts, :trix) { |name| name.delete_prefix("rich_text_") }
         discover_attachments
         discover_basic_associations
       end
 
       private
 
-      # Fetches the model class, falling back to the items_holder parent record in certain instances (e.g. in the context of the sidebar)
+      def setup_discovery_options(only, except, field_options)
+        @only = only
+        @except = except
+        @field_options = field_options
+      end
+
+      def discoverable_columns
+        model_db_columns.reject do |column_name, _|
+          skip_column?(column_name)
+        end
+      end
+
+      def skip_column?(column_name)
+        !column_in_scope?(column_name) ||
+          reflections.key?(column_name) ||
+          rich_text_column?(column_name)
+      end
+
+      def rich_text_column?(column_name)
+        rich_texts.key?("rich_text_#{column_name}")
+      end
+
+      def process_column(column_name, column)
+        field_config = determine_field_config(column_name, column)
+        return unless field_config
+
+        create_field(column_name, field_config)
+      end
+
+      def create_field(column_name, field_config)
+        field_options = { as: field_config.dup.delete(:field).to_sym }.merge(field_config)
+        field(column_name, **field_options.symbolize_keys, **@field_options.symbolize_keys)
+      end
+
+      def create_attachment_field(association_name, reflection)
+        field_type = determine_attachment_field_type(reflection)
+        field(association_name, as: field_type, **@field_options)
+      end
+
+      def determine_attachment_field_type(reflection)
+        reflection.options[:as] == :has_one_attached ? :file : :files
+      end
+
+      def create_association_field(association_name, reflection)
+        options = base_association_options(reflection)
+        options.merge!(polymorphic_options(reflection)) if reflection.options[:polymorphic]
+
+        field(association_name, **options, **@field_options)
+      end
+
+      def base_association_options(reflection)
+        {
+          as: reflection.macro,
+          searchable: true,
+          sortable: true
+        }
+      end
+
+      # Fetches the model class, falling back to the items_holder parent record in certain instances
+      # (e.g. in the context of the sidebar)
       def safe_model_class
         respond_to?(:model_class) ? model_class : @items_holder.parent.model_class
       rescue ActiveRecord::NoDatabaseError, ActiveRecord::ConnectionNotEstablished
@@ -100,12 +158,15 @@ module Avo
 
       def model_enums
         @model_enums ||= if safe_model_class.respond_to?(:defined_enums)
-          safe_model_class.defined_enums.transform_values do |enum_values|
-            { field: "select", enum: "::#{safe_model_class.name}.#{enum_values.keys.first.pluralize}" }
-          end
-        else
-          {}
-        end.with_indifferent_access
+                           safe_model_class.defined_enums.transform_values do |options|
+                             {
+                               field: :select,
+                               options:
+                             }
+                           end
+                         else
+                           {}
+                         end.with_indifferent_access
       end
 
       # Determines if a column is included in the discovery scope.
@@ -120,15 +181,11 @@ module Avo
           self.class.column_types_mapping[column.type]
       end
 
-      def build_field_options(field_config, column)
-        { as: field_config.delete(:field).to_sym }.merge(field_config)
-      end
-
       def discover_by_type(associations, as_type)
         associations.each_key do |association_name|
           next unless column_in_scope?(association_name)
 
-          field association_name, as: as_type, **@field_options.merge(yield(association_name))
+          field association_name, as: as_type, **@field_options.merge(name: yield(association_name))
         end
       end
 
@@ -136,8 +193,7 @@ module Avo
         attachment_associations.each do |association_name, reflection|
           next unless column_in_scope?(association_name)
 
-          field_type = reflection.options[:as] == :has_one_attached ? :file : :files
-          field association_name, as: field_type, **@field_options
+          create_attachment_field(association_name, reflection)
         end
       end
 
@@ -145,10 +201,7 @@ module Avo
         associations.each do |association_name, reflection|
           next unless column_in_scope?(association_name)
 
-          options = { as: reflection.macro, searchable: true, sortable: true }
-          options.merge!(polymorphic_options(reflection)) if reflection.options[:polymorphic]
-
-          field association_name, **options, **@field_options
+          create_association_field(association_name, reflection)
         end
       end
 
@@ -167,11 +220,11 @@ module Avo
       end
 
       def attachment_associations
-        @attachment_associations ||= reflections.select { |_, r| r.options[:class_name] == "ActiveStorage::Attachment" }
+        @attachment_associations ||= reflections.select { |_, r| r.options[:class_name] == 'ActiveStorage::Attachment' }
       end
 
       def rich_texts
-        @rich_texts ||= reflections.select { |_, r| r.options[:class_name] == "ActionText::RichText" }
+        @rich_texts ||= reflections.select { |_, r| r.options[:class_name] == 'ActionText::RichText' }
       end
 
       def tags
@@ -179,12 +232,15 @@ module Avo
       end
 
       def associations
-        @associations ||= reflections.reject { |key| attachment_associations.key?(key) || tags.key?(key) || rich_texts.key?(key) }
+        @associations ||= reflections.reject do |key|
+          attachment_associations.key?(key) || tags.key?(key) || rich_texts.key?(key)
+        end
       end
 
       def ignore_reflection?(name)
-        %w[blob blobs tags].include?(name.split("_").pop) || name.to_sym == :taggings
+        %w[blob blobs tags].include?(name.split('_').pop) || name.to_sym == :taggings
       end
     end
   end
 end
+# rubocop:enable Metrics/ModuleLength
