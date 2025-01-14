a58ac736eeeca2cd863d98e36ce8411e2da97f5b
Adrian Marin <adrian@adrianthedev.com>
2025-01-14 16:33:55 +0200

    wip

--- lib/avo/fields/markdown_field.rb
+++ lib/avo/fields/markdown_field.rb
@@ -11,8 +11,19 @@ module Avo
       end
 
       def self.parser
-        renderer = ::Redcarpet::Render::HTML.new(hard_wrap: true)
-        ::Redcarpet::Markdown.new(renderer, lax_spacing: true, fenced_code_blocks: true, hard_wrap: true)
+        ::Redcarpet::Markdown.new(::Redcarpet::Render::HTML,
+          tables: true,
+          lax_spacing: true,
+          fenced_code_blocks: true,
+          space_after_headers: true,
+          hard_wrap: true,
+          autolink: true,
+          strikethrough: true,
+          underline: true,
+          highlight: true,
+          quote: true,
+          with_toc_data: true
+        )
       end
     end
   end
