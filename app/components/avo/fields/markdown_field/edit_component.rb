928623ea7dd0e82ee9ba04cfbd591e1b0dd425b9
Adrian Marin <adrian@adrianthedev.com>
2025-01-14 15:25:38 +0200

    color zinc

--- app/components/avo/fields/markdown_field/edit_component.rb
+++ app/components/avo/fields/markdown_field/edit_component.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Avo::Fields::MarkdownField::EditComponent < Avo::Fields::EditComponent
-  BUTTON_CLASSES = "cursor-pointer py-1 px-1.5 hover:bg-slate-200 rounded"
+  BUTTON_CLASSES = "cursor-pointer py-1 px-1.5 hover:bg-zinc-200 rounded"
 end
