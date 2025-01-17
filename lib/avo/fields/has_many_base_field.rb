8d7f82d05dbf26a7f72886a9bd887d36b80de47a
Paul Bob <paul.ionut.bob@gmail.com>
2025-01-17 16:08:54 +0200

    fix habtm

--- lib/avo/fields/has_many_base_field.rb
+++ lib/avo/fields/has_many_base_field.rb
@@ -1,12 +1,17 @@
 module Avo
   module Fields
     class HasManyBaseField < ManyFrameBaseField
-      attr_accessor :attach_scope
+      attr_reader :attach_scope,
+        :link_to_child_resource,
+        :attach_fields
 
       def initialize(id, **args, &block)
         super(id, **args, &block)
 
         @attach_scope = args[:attach_scope]
+        # Defaults to nil so that if not set falls back to `link_to_child_resource` defined in the resource
+        @link_to_child_resource = args[:link_to_child_resource]
+        @attach_fields = args[:attach_fields]
       end
     end
   end
