b099483e7cdf022e8ed97108df003f726d738c81
Paul Bob <paul.ionut.bob@gmail.com>
2025-01-20 16:07:35 +0200

    lint

--- lib/avo/mappings.rb
+++ lib/avo/mappings.rb
@@ -1,105 +1,113 @@
 module Avo
   module Mappings
-    ASSOCIATIONS_MAPPING ||= {
-      ActiveRecord::Reflection::BelongsToReflection => {
-        field: "belongs_to"
-      },
-      ActiveRecord::Reflection::HasOneReflection => {
-        field: "has_one"
-      },
-      ActiveRecord::Reflection::HasManyReflection => {
-        field: "has_many"
-      },
-      ActiveRecord::Reflection::HasAndBelongsToManyReflection => {
-        field: "has_and_belongs_to_many"
-      }
-    }.freeze
+    unless defined?(ASSOCIATIONS_MAPPING)
+      ASSOCIATIONS_MAPPING = {
+        ActiveRecord::Reflection::BelongsToReflection => {
+          field: "belongs_to"
+        },
+        ActiveRecord::Reflection::HasOneReflection => {
+          field: "has_one"
+        },
+        ActiveRecord::Reflection::HasManyReflection => {
+          field: "has_many"
+        },
+        ActiveRecord::Reflection::HasAndBelongsToManyReflection => {
+          field: "has_and_belongs_to_many"
+        }
+      }.freeze
+    end
 
-    ATTACHMENTS_MAPPING ||= {
-      ActiveRecord::Reflection::HasOneReflection => {
-        field: "file"
-      },
-      ActiveRecord::Reflection::HasManyReflection => {
-        field: "files"
-      }
-    }.freeze
+    unless defined?(ATTACHMENTS_MAPPING)
+      ATTACHMENTS_MAPPING = {
+        ActiveRecord::Reflection::HasOneReflection => {
+          field: "file"
+        },
+        ActiveRecord::Reflection::HasManyReflection => {
+          field: "files"
+        }
+      }.freeze
+    end
 
-    FIELDS_MAPPING ||= {
-      primary_key: {
-        field: "id"
-      },
-      string: {
-        field: "text"
-      },
-      text: {
-        field: "textarea"
-      },
-      integer: {
-        field: "number"
-      },
-      float: {
-        field: "number"
-      },
-      decimal: {
-        field: "number"
-      },
-      datetime: {
-        field: "date_time"
-      },
-      timestamp: {
-        field: "date_time"
-      },
-      time: {
-        field: "date_time"
-      },
-      date: {
-        field: "date"
-      },
-      binary: {
-        field: "number"
-      },
-      boolean: {
-        field: "boolean"
-      },
-      references: {
-        field: "belongs_to"
-      },
-      json: {
-        field: "code"
-      }
-    }.freeze
+    unless defined?(FIELDS_MAPPING)
+      FIELDS_MAPPING = {
+        primary_key: {
+          field: "id"
+        },
+        string: {
+          field: "text"
+        },
+        text: {
+          field: "textarea"
+        },
+        integer: {
+          field: "number"
+        },
+        float: {
+          field: "number"
+        },
+        decimal: {
+          field: "number"
+        },
+        datetime: {
+          field: "date_time"
+        },
+        timestamp: {
+          field: "date_time"
+        },
+        time: {
+          field: "date_time"
+        },
+        date: {
+          field: "date"
+        },
+        binary: {
+          field: "number"
+        },
+        boolean: {
+          field: "boolean"
+        },
+        references: {
+          field: "belongs_to"
+        },
+        json: {
+          field: "code"
+        }
+      }.freeze
+    end
 
-    NAMES_MAPPING ||= {
-      id: {
-        field: "id"
-      },
-      description: {
-        field: "textarea"
-      },
-      gravatar: {
-        field: "gravatar"
-      },
-      email: {
-        field: "text"
-      },
-      password: {
-        field: "password"
-      },
-      password_confirmation: {
-        field: "password"
-      },
-      stage: {
-        field: "select"
-      },
-      budget: {
-        field: "currency"
-      },
-      money: {
-        field: "currency"
-      },
-      country: {
-        field: "country"
-      }
-    }.freeze
+    unless defined?(NAMES_MAPPING)
+      NAMES_MAPPING = {
+        id: {
+          field: "id"
+        },
+        description: {
+          field: "textarea"
+        },
+        gravatar: {
+          field: "gravatar"
+        },
+        email: {
+          field: "text"
+        },
+        password: {
+          field: "password"
+        },
+        password_confirmation: {
+          field: "password"
+        },
+        stage: {
+          field: "select"
+        },
+        budget: {
+          field: "currency"
+        },
+        money: {
+          field: "currency"
+        },
+        country: {
+          field: "country"
+        }
+      }.freeze
+    end
   end
 end
