67a12b035441dbbe904ef5c3f4fb30efd4b9773c
zhephyn <zhephynwinter@gmail.com>
2025-01-21 02:42:38 +0300

    Fix migration version error thrown with rails 8 plus lint issues

--- db/migrate/20250103192051_add_sizes_to_products.rb
+++ db/migrate/20250103192051_add_sizes_to_products.rb
@@ -1,4 +1,4 @@
-class AddSizesToProducts < ActiveRecord::Migration[8.0]
+class AddSizesToProducts < ActiveRecord::Migration[7.1]
   def change
     add_column :products, :sizes, :string, array: true, default: []
   end
