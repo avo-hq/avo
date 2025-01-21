f688e5976138cf8d563160c847c8840a6e86bdcb
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2025-01-21 09:37:59 +0200

    fix migration version

--- db/migrate/20250103192051_add_sizes_to_products.rb
+++ db/migrate/20250103192051_add_sizes_to_products.rb
@@ -1,4 +1,4 @@
-class AddSizesToProducts < ActiveRecord::Migration[7.1]
+class AddSizesToProducts < ActiveRecord::Migration[6.1]
   def change
     add_column :products, :sizes, :string, array: true, default: []
   end
