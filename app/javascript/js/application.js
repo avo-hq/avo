fc85b7d9c77fc13c6120b6356454ce13af03db32
Adrian Marin <adrian@adrianthedev.com>
2025-01-25 22:24:45 +0200

    chore: register stimulus controllers from plugins

--- app/javascript/js/application.js
+++ app/javascript/js/application.js
@@ -1,5 +1,5 @@
 import { Alert, Popover } from 'tailwindcss-stimulus-components'
-import { Application } from '@hotwired/stimulus'
+import { Application, Controller } from '@hotwired/stimulus'
 import Clipboard from '@stimulus-components/clipboard'
 import PasswordVisibility from '@stimulus-components/password-visibility'
 import TextareaAutogrow from 'stimulus-textarea-autogrow'
@@ -12,6 +12,19 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = window?.localStorage.getItem('avo.debug')
 window.Stimulus = application
+
+/* Using this patter of providing a fake Stimulus object so the plugins do not have to bundle the Stimulus controller too.
+   Their rollup config is isntructed to use `fakeStimulus` instead of `Stimulus`.
+   output: {
+   ...
+    globals: {
+      "@hotwired/stimulus": "FakeStimulus"
+    }
+  }
+*/
+window.FakeStimulus = {
+  Controller,
+}
 
 // Register stimulus-components controller
 application.register('alert', Alert)
