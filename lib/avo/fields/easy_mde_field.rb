2120913329d4ad3a92a103c47a3e16d2d133d5d6
Exequiel Rozas <exequiel98@gmail.com>
2025-01-27 03:21:05 -0400

    Add image uploads to the EasyMDE editor

--- lib/avo/fields/easy_mde_field.rb
+++ lib/avo/fields/easy_mde_field.rb
@@ -10,12 +10,22 @@ module Avo
 
         @always_show = args[:always_show].present? ? args[:always_show] : false
         @height = args[:height].present? ? args[:height].to_s : "auto"
+        @upload_url = args[:upload_url].present? ? args[:upload_url] : direct_uploads_url
+        @image_upload = args[:image_upload].present? ? args[:image_upload] : false
         @spell_checker = args[:spell_checker].present? ? args[:spell_checker] : false
         @options = {
           spell_checker: @spell_checker,
           always_show: @always_show,
-          height: @height
+          height: @height,
+          image_upload: @image_upload,
+          upload_url: direct_uploads_url
         }
+      end
+
+      private
+
+      def direct_uploads_url
+        "/rails/active_storage/direct_uploads"
       end
     end
   end
