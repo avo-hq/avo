85fa3083cf9a80d67d2f0682332200b3d7418de9
Adrian Marin <adrian@adrianthedev.com>
2025-01-30 13:48:06 +0200

    wip

--- app/javascript/js/controllers/media_library_attach_controller.js
+++ app/javascript/js/controllers/media_library_attach_controller.js
@@ -1,10 +1,63 @@
 /* eslint-disable no-console */
+// eslint-disable-next-line max-classes-per-file
 import { Controller } from '@hotwired/stimulus'
 import { DirectUpload } from '@rails/activestorage'
 
+class UploadObject extends EventTarget {
+  constructor(file, controller) {
+    super()
+
+    this.file = file
+    this.checksum = file.checksum
+    this.controller = controller
+    this.finished = false
+  }
+
+  hasFinished() {
+    return this.finished
+  }
+
+  handle() {
+    const upload = new DirectUpload(this.file, this.controller.directUploadsUrlValue, this)
+
+    this.#addUploadingItem(this.file)
+
+    upload.create((error, blob) => {
+      if (error) {
+        console.log('Error', error)
+      }
+      this.listItem.classList.add('text-gray-400')
+
+      this.finished = true
+      this.dispatchEvent(new Event('done'))
+    })
+  }
+
+  #addUploadingItem(file) {
+    const div = document.createElement('div')
+    div.classList.add('flex', 'justify-between', 'gap-2', 'text-sm')
+    div.dataset.checksum = file.checksum
+    div.innerHTML = `<div>${file.name}</div><div class="progress">0%</div>`
+    this.listItem = this.controller.uploadingContainerTarget.appendChild(div)
+  }
+
+  directUploadWillStoreFileWithXHR(request) {
+    request.upload.addEventListener(
+      'progress',
+      (event) => this.#directUploadDidProgress(event),
+    )
+  }
+
+  #directUploadDidProgress(event) {
+    const progress = (event.loaded / event.total) * 100
+    const element = this.listItem.querySelector('.progress')
+    element.textContent = `${progress.toFixed(2)}%`
+  }
+}
+
 // Connects to data-controller="media-library-new"
 export default class extends Controller {
-  static targets = ['dropzone']
+  static targets = ['dropzone', 'uploadingContainer']
 
   static values = {
     directUploadsUrl: String,
@@ -36,7 +89,7 @@ export default class extends Controller {
     // Handle file selection
     this.fileInput.addEventListener('change', (e) => {
       const files = Array.from(e.target.files)
-      files.forEach((file) => this.submitAction(file))
+      this.#submitFiles(files)
       this.fileInput.value = '' // Reset the input for future selections
     })
   }
@@ -66,39 +119,43 @@ export default class extends Controller {
     this.dropzoneTarget.addEventListener('drop', (e) => {
       vm.dropAction()
       const { files } = e.dataTransfer
-      Array.from(files).forEach((file) => {
-        vm.submitAction(file)
-      })
+      this.#submitFiles(files)
     }, false)
   }
 
-  // Check out this guide when adding progress tracking
-  // https://guides.rubyonrails.org/active_storage_overview.html#track-the-progress-of-the-file-upload
-  // uploadRequestDidProgress(event) {
-  //   console.log('uploadRequestDidProgress', event)
-  // }
-
-  submitAction(file) {
-    const upload = new DirectUpload(file, this.directUploadsUrlValue)
-    // console.log('upload', upload, this.dropzoneTarget, this)
-    // this.dropzoneTarget.addEventListener('direct-upload:progress', (id, file, progress) => {
-    //   console.log('this.dropzoneTarget.progress', id, file, progress)
-    // })
-
-    upload.create((error, blob) => {
-      if (error) {
-        console.log('Error', error)
-      }
+  #submitFiles(files) {
+    this.uploadObjects = []
+    Array.from(files).forEach((file) => {
+      const uploadObject = new UploadObject(file, this)
+      this.uploadObjects.push(uploadObject)
+      uploadObject.addEventListener('done', () => {
+        this.#checkIfAllFinished()
+      })
+      uploadObject.handle()
     })
   }
 
+  #checkIfAllFinished() {
+    if (this.uploadObjects.every((uploadObject) => uploadObject.hasFinished())) {
+      this.#refreshPage()
+    }
+  }
+
+  #refreshPage() {
+    const streamElement = document.createElement('turbo-stream')
+    streamElement.setAttribute('action', 'refresh')
+    streamElement.setAttribute('method', 'morph')
+    streamElement.setAttribute('render', 'morph')
+    this.element.appendChild(streamElement)
+  }
+
   dragAction() {
-    this.element.classList.add(...this.draggingClasses)
-    this.element.innerHTML = this.dropCopy
+    this.dropzoneTarget.classList.add(...this.draggingClasses)
+    this.dropzoneTarget.innerHTML = this.dropCopy
   }
 
   dropAction() {
-    this.element.classList.remove(...this.draggingClasses)
-    this.element.innerHTML = this.droppedCopy
+    this.dropzoneTarget.classList.remove(...this.draggingClasses)
+    this.dropzoneTarget.innerHTML = this.droppedCopy
   }
 }
