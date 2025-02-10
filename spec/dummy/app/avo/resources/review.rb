a3880d154b02913444e30cf77a81620addd0c326
Nevelito <szymonnewel@gmail.com>
2025-02-10 09:36:28 +0100

    fix

--- spec/dummy/app/avo/resources/review.rb
+++ spec/dummy/app/avo/resources/review.rb
@@ -16,41 +16,41 @@ class Avo::Resources::Review < Avo::BaseResource
     end
 
     field :user,
-          as: :belongs_to,
-          searchable: true,
-          allow_via_detaching: true,
-          help: "For the review with the ID of 1 only admin users will be displayed.",
-          attach_scope: -> do
-            # For the parent record with ID 1 we'll apply this rule.
-            # This is for testing purposes only. Just to show that it's possible.
-            if parent.present? && parent.id == 1
-              query.admins
-            else
-              query
-            end
-          end
+      as: :belongs_to,
+      searchable: true,
+      allow_via_detaching: true,
+      help: "For the review with the ID of 1 only admin users will be displayed.",
+      attach_scope: -> do
+        # For the parent record with ID 1 we'll apply this rule.
+        # This is for testing purposes only. Just to show that it's possible.
+        if parent.present? && parent.id == 1
+          query.admins
+        else
+          query
+        end
+      end
     field :reviewable,
-          as: :belongs_to,
-          polymorphic_as: :reviewable,
-          types: [::Fish, ::Post, ::Project, ::Team],
-          searchable: true,
-          allow_via_detaching: true,
-          html: {
-            data: {
-              "resource-edit-target": "emailField",
-              action: "input->resource-edit#emailUpdate"
-            }
-          },
-          attach_scope: -> do
-            # For the parent record with ID 1 we'll apply this rule.
-            # This is for testing purposes only. Just to show that it's possible.
-            if parent.present? && parent.id == 1
-              query.where("lower(name) like ?", "%#{parent.body[0].downcase}%")
-            else
-              query
-            end
-          end,
-          polymorphic_help: "Select the polymorphic type",
-          help: "For the review with the ID of 1 the scope is modified. Please check the code under <code>review_resource.rb</code>"
+      as: :belongs_to,
+      polymorphic_as: :reviewable,
+      types: [::Fish, ::Post, ::Project, ::Team],
+      searchable: true,
+      allow_via_detaching: true,
+      html: {
+        data: {
+          "resource-edit-target": "emailField",
+          action: "input->resource-edit#emailUpdate"
+        }
+      },
+      attach_scope: -> do
+        # For the parent record with ID 1 we'll apply this rule.
+        # This is for testing purposes only. Just to show that it's possible.
+        if parent.present? && parent.id == 1
+          query.where("lower(name) like ?", "%#{parent.body[0].downcase}%")
+        else
+          query
+        end
+      end,
+      polymorphic_help: "Select the polymorphic type",
+      help: "For the review with the ID of 1 the scope is modified. Please check the code under <code>review_resource.rb</code>"
   end
 end
