055578be51118330568a9b1bbe22bf834d91d4c9
Nevelito <szymonnewel@gmail.com>
2025-02-11 14:05:14 +0100

    fix bug with types

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  class << self
+    alias_method :original_inspect, :inspect
+
+    def inspect
+      name
+    end
+  end
 end
