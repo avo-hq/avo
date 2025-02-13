1c4b138437202b0253aa640cb291102ec06ad3b9
Paul Bob <paul.ionut.bob@gmail.com>
2025-02-13 13:28:59 +0200

    lint

--- app/components/avo/fields/preview_field/index_component.rb
+++ app/components/avo/fields/preview_field/index_component.rb
@@ -8,7 +8,7 @@ class Avo::Fields::PreviewField::IndexComponent < Avo::Fields::IndexComponent
     # as the link exposes the result of `record.to_param`.
     return preview_icon if !can_view?
 
-    link_to resource_view_path, title: t('avo.view_item', item: @resource.name).humanize do
+    link_to resource_view_path, title: t("avo.view_item", item: @resource.name).humanize do
       preview_icon
     end
   end
