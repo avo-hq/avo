ef16e942a1dbb7bcbe5bc5eb57eabc90ebe78192
Paul Bob <paul.ionut.bob@gmail.com>
2025-02-13 13:28:39 +0200

    lint

--- app/components/avo/fields/preview_field/index_component.rb
+++ app/components/avo/fields/preview_field/index_component.rb
@@ -13,14 +13,13 @@ class Avo::Fields::PreviewField::IndexComponent < Avo::Fields::IndexComponent
     end
   end
 
-
   def preview_icon
     helpers.svg(
       "heroicons/outline/magnifying-glass-circle",
       class: "block h-6 text-gray-600",
       data: {
         controller: "preview",
-        preview_url_value: helpers.preview_resource_path(resource: @resource,  turbo_frame: :preview_modal),
+        preview_url_value: helpers.preview_resource_path(resource: @resource, turbo_frame: :preview_modal),
       }
     )
   end
