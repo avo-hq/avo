77227666a77ef38706589fc272bddd7bb60278fc
Adrian Marin <adrian@adrianthedev.com>
2025-02-14 21:35:40 +0200

    wip

--- spec/dummy/app/avo/resources/product.rb
+++ spec/dummy/app/avo/resources/product.rb
@@ -31,7 +31,7 @@ class Avo::Resources::Product < Avo::BaseResource
   self.discreet_information = [
     {
       tooltip: -> { sanitize("Product is <strong>#{record.status}</strong>", tags: %w[strong]) },
-      icon: -> { "heroicons/outline/#{record.status == :new ? "arrow-trending-up" : "arrow-trending-down"}" }
+      icon: -> { "heroicons/outline/#{(record.status == :new) ? "arrow-trending-up" : "arrow-trending-down"}" }
     },
     :timestamps
   ]
