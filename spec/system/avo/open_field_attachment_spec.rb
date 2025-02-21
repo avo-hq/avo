04add95e2de10f03d08ed0a3e75773c44120d9ad
Nevelito <szymonnewel@gmail.com>
2025-02-21 15:35:28 +0100

    fix specs

--- spec/system/avo/open_field_attachment_spec.rb
+++ spec/system/avo/open_field_attachment_spec.rb
@@ -2,14 +2,19 @@ require "rails_helper"
 
 RSpec.describe "OpenFieldAttachment", type: :system do
   let!(:user) { User.first }
+  let!(:cv_file) { Rails.root.join('app', 'assets', 'pdfs', 'cv_sample.pdf') }
   let(:path) { "/admin/resources/field_discovery_users/#{user.slug}" }
+
+  before do
+    user.cv.attach(io: File.open(cv_file), filename: 'cv_sample.pdf', content_type: 'application/pdf')
+  end
 
   def test_open_field_attachment(path)
     visit path
 
-    element = find('svg[data-slot="icon"]')
-    expect(element).to be_present
-    element.click
+    link = find('a[rel="noopener noreferrer"][target="_blank"]', visible: :all)
+    expect(link).to be_present
+    link.click
 
     expect(page.driver.browser.current_url).not_to include("download")
     expect(page.driver.browser.window_handles.length).to eq 2
