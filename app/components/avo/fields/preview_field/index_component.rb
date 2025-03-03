3b8c88dcf129f994b99c02e790a6206814bba693
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2025-03-03 09:34:21 +0200

    🚨 enhancement: preview policy (#3660)
    * enhancement: log errors rescued `perform_action_and_record_errors`
    
    * enhancement: preview policy
    
    * fix preview exposing record.to_param when `show` is not authorized
    
    * lint
    
    * lint
    
    * fix resource reader
    
    * revert show policy check

--- app/components/avo/fields/preview_field/index_component.rb
+++ app/components/avo/fields/preview_field/index_component.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Avo::Fields::PreviewField::IndexComponent < Avo::Fields::IndexComponent
+  def render_preview
+    link_to resource_view_path, title: t("avo.view_item", item: @resource.name).humanize do
+      helpers.svg(
+        "heroicons/outline/magnifying-glass-circle",
+        class: "block h-6 text-gray-600",
+        data: {
+          controller: "preview",
+          preview_url_value: helpers.preview_resource_path(resource: @resource, turbo_frame: :preview_modal),
+        }
+      )
+    end
+  end
 end
