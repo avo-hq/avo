74370e7eb2922aca5e4e84ca26570612afe80a76
Nevelito <szymonnewel@gmail.com>
2025-03-10 18:16:37 +0100

    fix bugs

--- spec/dummy/app/models/volunteer.rb
+++ spec/dummy/app/models/volunteer.rb
@@ -1,6 +1,3 @@
 class Volunteer < ApplicationRecord
   belongs_to :event, foreign_key: "event_uuid", primary_key: "uuid"
-
-  validates :name, presence: true
-  validates :role, presence: true
 end
--- spec/dummy/db/migrate/20250310162810_add_uuid_to_events.rb
+++ spec/dummy/db/migrate/20250310162810_add_uuid_to_events.rb
@@ -1,6 +1,6 @@
 class AddUuidToEvents < ActiveRecord::Migration[8.0]
   def change
-    add_column :events, :uuid, :uuid, default: "gen_random_uuid()", null: false
+    add_column :events, :uuid, :uuid
     add_index :events, :uuid, unique: true
   end
 end
