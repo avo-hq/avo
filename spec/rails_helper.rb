6ed062007d3e3c22fec2f489c9fc78700a015db2
Paul Bob <paul.ionut.bob@gmail.com>
2025-03-11 18:06:55 +0200

    .

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -113,7 +113,7 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
   config.before(:each) do
-    Rails.application.reload_routes_unless_loaded
+    Rails.application.try(:reload_routes_unless_loaded)
   end
 
   config.before(:each, type: :system) {
