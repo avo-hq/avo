be8cc4cc047ef217964cbab59a0310f0849ce658
Paul Bob <paul.ionut.bob@gmail.com>
2025-03-13 11:26:01 +0200

    rubocop:disable Style/ArgumentsForwarding

--- lib/avo/concerns/safe_call.rb
+++ lib/avo/concerns/safe_call.rb
@@ -1,9 +1,11 @@
 module Avo
   module Concerns
     module SafeCall
-      def safe_call(method, **)
-        send(method, **) if respond_to?(method, true)
+      # rubocop:disable Style/ArgumentsForwarding
+      def safe_call(method, **kwargs)
+        send(method, **kwargs) if respond_to?(method, true)
       end
+      # rubocop:enable Style/ArgumentsForwarding
     end
   end
 end
