e0591f4f9f4e9db1fdede352865b4e2a69b275b9
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2025-04-04 22:29:53 +0200

    remove logger from update product action

--- spec/dummy/app/avo/actions/update_product.rb
+++ spec/dummy/app/avo/actions/update_product.rb
@@ -2,8 +2,6 @@ class Avo::Actions::UpdateProduct < Avo::BaseAction
   self.name = "Update Product"
 
   def handle(**args)
-    Rails.logger.debug "view_type_from_update_product: #{arguments[:view_type]}"
-
     records, resource = args.values_at(:records, :resource)
     view_type = arguments[:view_type]
 
