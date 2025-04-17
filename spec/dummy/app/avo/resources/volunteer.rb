60c550082801e38059a6466abf9c4e701d1d2286
Paul Bob <paul.ionut.bob@gmail.com>
2025-04-17 19:57:40 +0300

    wip

--- spec/dummy/app/avo/resources/volunteer.rb
+++ spec/dummy/app/avo/resources/volunteer.rb
@@ -3,6 +3,6 @@ class Avo::Resources::Volunteer < Avo::BaseResource
     field :id, as: :id
     field :name, as: :text
     field :role, as: :text
-    field :event, as: :belongs_to, foreign_key: :event_uuid, primary_key: :uuid, placeholder: "—"
+    field :event, as: :belongs_to, placeholder: "—"
   end
 end
