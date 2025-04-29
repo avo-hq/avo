e851739ebaef63fef97db95bb403dc8334aa54d5
Paul Bob <paul.ionut.bob@gmail.com>
2025-04-29 13:01:32 +0300

    different test approach

--- spec/system/avo/open_field_attachment_spec.rb
+++ spec/system/avo/open_field_attachment_spec.rb
@@ -1,56 +1,55 @@
 require "rails_helper"
+include ActionView::RecordIdentifier
 
 RSpec.describe "OpenFieldAttachment", type: :system do
-  let!(:user) { User.first }
-  let!(:cv_file) { Rails.root.join("app", "assets", "pdfs", "cv_sample.pdf") }
-  let!(:csv_file) { Rails.root.join("app", "assets", "csvs", "sample.csv") }
-  let(:path) { "/admin/resources/field_discovery_users/#{user.slug}" }
+  let!(:user) { create :user }
 
   context "with PDF attachment" do
-    before do
-      user.cv.attach(io: File.open(cv_file), filename: "cv_sample.pdf", content_type: "application/pdf")
-    end
-
     it "opens attachment in new window without download" do
-      test_open_PDF_attachment(path)
+      user.cv.attach(
+        io: File.open(Rails.root.join("db", "seed_files", "dummy-file.pdf")),
+        filename: "dummy-file.pdf",
+        content_type: "application/pdf"
+      )
+
+      visit avo.resources_field_discovery_user_path(user)
+
+      file_path = Rails.application.routes.url_helpers.rails_blob_path(user.cv, only_path: true)
+
+      within("##{dom_id(user.cv)}") do
+        link = find(:css, "a[href*='#{file_path}']:not([download])")
+        expect(link[:target]).to eq("_blank")
+        expect(link[:rel]).to eq("noopener noreferrer")
+
+        new_window = window_opened_by { link.click }
+        expect(new_window).not_to be_nil
+
+        within_window new_window do
+          # Check that final URL ends with filename, since path changes after redirect
+          expect(page.current_url).to match(/dummy-file\.pdf\z/)
+        end
+      end
     end
   end
 
   context "with CSV attachment" do
-    before do
-      user.cv.attach(io: File.open(csv_file), filename: "sample.csv", content_type: "application/csv")
-    end
-
     it "can not open or download attachment in new window" do
-      test_open_CSV_attachment(path)
+      csv_file_path = Rails.root.join("db", "seed_files", "sample.csv")
+
+      user.cv.attach(
+        io: File.open(csv_file_path),
+        filename: "sample.csv",
+        content_type: "application/csv"
+      )
+
+      visit avo.resources_field_discovery_user_path(user)
+
+      file_path = Rails.application.routes.url_helpers.rails_blob_path(user.cv, only_path: true)
+
+      within("##{dom_id(user.cv)}") do
+        expect(page).not_to have_selector(:css, "a[href*='#{file_path}']:not([download])")
+        expect(page).to have_selector(:css, "a[href*='#{file_path}'][download]")
+      end
     end
-  end
-
-  def test_open_PDF_attachment(path)
-    visit path
-
-    link = find('a[rel="noopener noreferrer"][target="_blank"]', visible: :all, wait: 5)
-
-    expect(link).to be_present
-    expect(link[:target]).to eq("_blank")
-    expect(link[:rel]).to eq("noopener noreferrer")
-    link.click
-
-    expect(page.driver.browser.current_url).not_to include("download")
-    expect(page.driver.browser.window_handles.length).to eq 2
-  end
-
-  def test_open_CSV_attachment(path)
-    visit path
-
-    div = find('span[title="' + csv_file.basename.to_s + '"]').find(:xpath, './ancestor::div[@class="flex flex-col h-full"]')
-
-    expect(div).not_to have_selector("a")
-
-    svg = div.find('svg[data-slot="icon"]')
-    svg.click
-
-    expect(page).to have_current_path(path, ignore_query: true)
-    expect(page.driver.browser.window_handles.length).to eq 1
   end
 end
