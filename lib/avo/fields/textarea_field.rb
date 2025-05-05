2784a124823ea637298e78bdbbd9ffa2315ec8c7
Paul Bob <paul.ionut.bob@gmail.com>
2025-05-05 10:42:22 +0300

    wip

--- lib/avo/fields/textarea_field.rb
+++ lib/avo/fields/textarea_field.rb
@@ -4,6 +4,8 @@ module Avo
       attr_reader :rows
 
       def initialize(id, **args, &block)
+        hide_on :index
+
         super(id, **args, &block)
 
         @rows = args[:rows].present? ? args[:rows].to_i : 5
