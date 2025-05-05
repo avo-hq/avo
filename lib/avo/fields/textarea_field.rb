331ee5ff1b8f5f482371d76e752631badf5addd7
Paul Bob <paul.ionut.bob@gmail.com>
2025-05-05 10:32:28 +0300

    fix: allow textarea index field

--- lib/avo/fields/textarea_field.rb
+++ lib/avo/fields/textarea_field.rb
@@ -6,8 +6,6 @@ module Avo
       def initialize(id, **args, &block)
         super(id, **args, &block)
 
-        hide_on :index
-
         @rows = args[:rows].present? ? args[:rows].to_i : 5
       end
     end
