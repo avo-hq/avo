5f2a1b0e13b281c776dd5bc0311ba3782210b278
Paul Bob <paul.ionut.bob@gmail.com>
2025-05-20 13:15:18 +0300

    reproduction for belongs to polymorphic cluster

--- spec/dummy/app/avo/resources/comment.rb
+++ spec/dummy/app/avo/resources/comment.rb
@@ -11,15 +11,20 @@ class Avo::Resources::Comment < Avo::BaseResource
   self.after_update_path = :index
 
   def fields
-    field :id, as: :id
-    field :body, as: :textarea, copyable: true
-    field :tiny_name, as: :text, only_on: :index
-    field :posted_at,
+    main_panel do
+      cluster do
+        field :user, as: :belongs_to, use_resource: Avo::Resources::CompactUser, link_to_record: true
+        field :commentable, as: :belongs_to, polymorphic_as: :commentable, types: [::Post, ::Project]
+      end
+
+      field :id, as: :id
+      field :body, as: :textarea, copyable: true
+      field :tiny_name, as: :text, only_on: :index
+      field :posted_at,
       as: :date_time,
       picker_format: "Y-m-d H:i:S",
       format: "cccc, d LLLL yyyy, HH:mm ZZZZ" # Wednesday, 10 February 1988, 16:00 GMT
-    field :user, as: :belongs_to, use_resource: Avo::Resources::CompactUser, link_to_record: true
-    field :commentable, as: :belongs_to, polymorphic_as: :commentable, types: [::Post, ::Project]
+    end
   end
 
   def filters
