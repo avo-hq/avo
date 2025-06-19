99c14a330229be2f5b9284b83bf1bf7a55115757
Paul Bob <paul.ionut.bob@gmail.com>
2025-06-19 17:54:10 +0300

    migration

--- spec/dummy/db/migrate/20250619140631_add_grouped_select_fields_to_volunteers.rb
+++ spec/dummy/db/migrate/20250619140631_add_grouped_select_fields_to_volunteers.rb
@@ -1,4 +1,4 @@
-class AddGroupedSelectFieldsToVolunteers < ActiveRecord::Migration[8.0]
+class AddGroupedSelectFieldsToVolunteers < ActiveRecord::Migration[6.1]
   def change
     add_column :volunteers, :department, :string
     add_column :volunteers, :skills, :string, array: true, default: []
