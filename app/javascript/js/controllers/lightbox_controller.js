8fdb0847a0947d89528bf5524d8b3ad3a4fc8bb8
zhephyn <zhephynwinter@gmail.com>
2025-07-13 11:07:28 +0300

    image is rendered perfectly inside the modal

--- app/javascript/js/controllers/lightbox_controller.js
+++ app/javascript/js/controllers/lightbox_controller.js
@@ -9,13 +9,20 @@ export default class extends Controller {
 
   open(event) {
     const imageUrl = event.target.getAttribute("src")
+    const imageAlt = event.target.getAttribute("alt") || "Expanded view of the image"
+
+    this.imageTarget.setAttribute("alt", imageAlt)
     this.imageTarget.setAttribute("src", imageUrl)
     this.modalTarget.classList.remove("hidden")
+    
+    document.body.style.overflow = "hidden"
   }
 
   close(event) {
-    if (event.target === this.modalTarget) {
+    if (event.target === this.modalTarget || event.target.closest('[data-action*="lightbox#close"]')) {
       this.modalTarget.classList.add("hidden")
     }
+
+    document.body.style.overflow = "auto"
   }
 }
