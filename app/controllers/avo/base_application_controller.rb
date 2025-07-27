135f359bea85b784a0aa689946c95b5b20296c90
Adrian Marin <adrian@adrianthedev.com>
2025-07-27 15:49:48 +0300

    feature: light/dark mode switcher

--- app/controllers/avo/base_application_controller.rb
+++ app/controllers/avo/base_application_controller.rb
@@ -26,6 +26,7 @@ module Avo
     before_action :set_view
     before_action :set_sidebar_open
     before_action :set_stylesheet_assets_path
+    before_action :set_color_scheme
 
     rescue_from Avo::NotAuthorizedError, with: :render_unauthorized
     rescue_from ActiveRecord::RecordInvalid, with: :exception_logger
@@ -329,6 +330,10 @@ module Avo
         params[:raw_return_to] = params[:return_to]
         params[:return_to] = d(params[:return_to])
       end
+    end
+
+    def set_color_scheme
+      @color_scheme = cookies[:color_scheme] || "light"
     end
   end
 end
