2065f3bdddd4c11a09cc6930fa547b501b819239
Paul Bob <paul.ionut.bob@gmail.com>
2025-08-01 12:25:04 +0300

    dry

--- app/components/avo/resource_bare_content_component.rb
+++ app/components/avo/resource_bare_content_component.rb
@@ -11,4 +11,16 @@ class Avo::ResourceBareContentComponent < Avo::BaseComponent
   prop :actions
   prop :turbo_frame
   prop :index_params
+
+  def paginator_component
+    Avo::PaginatorComponent.new(
+      pagy: @pagy,
+      turbo_frame: @turbo_frame,
+      index_params: @index_params,
+      resource: @resource,
+      parent_record: @parent_record,
+      parent_resource: @parent_resource,
+      discreet_pagination: field&.discreet_pagination
+    )
+  end
 end
