ef4ce595da9c1bfd560612409e6f70b6bbc94ed5
zhephyn <zhephynwinter@gmail.com>
2025-08-03 23:04:53 +0300

    Initial Approach

---
+++ app/components/avo/fields/common/stars_component.rb
@@ -0,0 +1,4 @@
+class Avo::Fields::Common::StarsComponent < Avo::BaseComponent
+    prop :value
+    prop :max, default: 5
+end
---
+++ lib/avo/fields/stars_field.rb
@@ -0,0 +1,13 @@
+module Avo 
+  module Fields
+    class StarsField < BaseField
+      attr_reader :max
+
+      def initialize(name, **args, &block)
+        super(name, **args, &block)
+
+        @max = args[:max] || 5
+      end
+    end
+  end
+end
