4a156f3e6c4be980777bb1c4cfb3e63fdbdf3b4f
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2025-08-13 13:08:10 +0300

    fix: attach scope (#4029)
    * fix: attach scope
    
    * safe check
    
    * wip
    
    * public_send
    
    * restructuration

--- app/controllers/avo/search_controller.rb
+++ app/controllers/avo/search_controller.rb
@@ -112,9 +112,22 @@ module Avo
         reflection_class = BaseResource.get_model_by_name params[:via_reflection_class]
 
         grandparent = parent_resource_class.find params[:via_parent_resource_id]
-        parent = reflection_class.new(
-          params[:via_relation] => grandparent
-        )
+        parent = reflection_class.new
+
+        via_relation = params[:via_relation].to_s
+        # Whitelist allowed relations to prevent code injection
+        if reflection_class.reflections.keys.map(&:to_s).include?(via_relation)
+          # Verify if the relation is a collection proxy
+          # If it is, add the grandparent to the collection
+          # If it is not, set the grandparent as the parent of the relation
+          if parent.public_send(via_relation).is_a?(ActiveRecord::Associations::CollectionProxy)
+            parent.public_send(via_relation) << grandparent
+          else
+            parent.public_send(:"#{via_relation}=", grandparent)
+          end
+        else
+          raise ArgumentError, "Invalid relation name: #{via_relation}"
+        end
       end
 
       Avo::ExecutionContext.new(target: attach_scope, query: query, parent: parent).handle
@@ -276,6 +289,8 @@ module Avo
 
     def render_error?
       Rails.env.development?
+
+      false
     end
   end
 end
