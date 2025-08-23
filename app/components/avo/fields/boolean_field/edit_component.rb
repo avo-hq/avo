f937b23a4d9e2e1fc6fb42110e9107c4fb3c5304
Shahzaib Iqbal <98122801+Shahzaib-Iqbal-1057@users.noreply.github.com>
2025-08-23 22:20:30 +0500

    implemented reviewed changes

--- app/components/avo/fields/boolean_field/edit_component.rb
+++ app/components/avo/fields/boolean_field/edit_component.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
 class Avo::Fields::BooleanField::EditComponent < Avo::Fields::EditComponent
+  def as_toggle?
+    @field.instance_variable_get(:@args)&.dig(:as_toggle)
+  end
+
+  def checkbox_classes
+    if as_toggle?
+      "sr-only"
+    else
+      "text-lg h-4 w-4 checked:bg-primary-400 focus:checked:!bg-primary-400 rounded #{@field.get_html(:classes, view: view, element: :input)}"
+    end
+  end
+
+  def checkbox_data_attributes
+    base_data = @field.get_html(:data, view: view, element: :input) || {}
+    
+    if as_toggle?
+      base_data.merge(avo_boolean_toggle_target: "checkbox")
+    else
+      base_data
+    end
+  end
+
+  def checkbox_style
+    @field.get_html(:style, view: view, element: :input)
+  end
+
+  private
+
+  def common_checkbox_attributes
+    {
+      value: @field.value,
+      checked: @field.value,
+      disabled: disabled?,
+      autofocus: @autofocus,
+      id: "#{@field.id}_checkbox"
+    }
+  end
 end
