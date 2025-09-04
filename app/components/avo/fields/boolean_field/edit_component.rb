e1cfbd363aed502c62f67ef58034dd69bc945405
Paul Bob <paul.ionut.bob@gmail.com>
2025-09-04 11:54:30 +0300

    rm _checkbox prefix from input

--- app/components/avo/fields/boolean_field/edit_component.rb
+++ app/components/avo/fields/boolean_field/edit_component.rb
@@ -2,8 +2,4 @@
 
 class Avo::Fields::BooleanField::EditComponent < Avo::Fields::EditComponent
   delegate :as_toggle?, to: :@field
-
-  def after_initialize
-    @checkbox_id = "#{@field.id}_checkbox"
-  end
 end
