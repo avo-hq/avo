4101cbb68f088379123b5c1bdb3e9e61341d8460
Paul Bob <paul.ionut.bob@gmail.com>
2025-09-04 10:37:44 +0300

    use `class_names` for `checkbox_classes`

--- app/components/avo/fields/boolean_field/edit_component.rb
+++ app/components/avo/fields/boolean_field/edit_component.rb
@@ -4,11 +4,11 @@ class Avo::Fields::BooleanField::EditComponent < Avo::Fields::EditComponent
   delegate :as_toggle?, to: :@field
 
   def checkbox_classes
-    if as_toggle?
-      "sr-only"
-    else
-      "text-lg h-4 w-4 checked:bg-primary-400 focus:checked:!bg-primary-400 rounded #{@field.get_html(:classes, view: view, element: :input)}"
-    end
+    class_names(
+      "text-lg h-4 w-4 checked:bg-primary-400 focus:checked:!bg-primary-400 rounded",
+      @field.get_html(:classes, view: view, element: :input),
+      "sr-only": as_toggle?
+    )
   end
 
   def checkbox_data_attributes
