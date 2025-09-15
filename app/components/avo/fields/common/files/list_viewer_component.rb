5bac1a8a852a015a57ded976e628d41b3b03cbee
Paul Bob <paul.ionut.bob@gmail.com>
2025-09-15 14:57:26 +0300

    refactor

--- app/components/avo/fields/common/files/list_viewer_component.rb
+++ app/components/avo/fields/common/files/list_viewer_component.rb
@@ -16,26 +16,11 @@ class Avo::Fields::Common::Files::ListViewerComponent < Avo::BaseComponent
     else
       @field.view_type
     end
-  end
 
-  def classes
-    base_classes = "py-4 rounded-2xl max-w-full"
-
-    view_type_classes = if @view_type == :list
-      "flex flex-col space-y-2"
-    else
-      "relative grid xs:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6"
-    end
-
-    "#{base_classes} #{view_type_classes}"
-  end
-
-  def wrapper_classes
-    (@field.stacked && !@field.hide_view_type_switcher) ? "-mt-9" : ""
+    @component_class = "Avo::Fields::Common::Files::ViewType::#{@view_type.to_s.capitalize}ItemComponent".constantize
   end
 
   def view_type_component(file)
-    component = "Avo::Fields::Common::Files::ViewType::#{@view_type.to_s.capitalize}ItemComponent".constantize
-    component.new(field: @field, resource: @resource, file: file, extra_classes: "aspect-video")
+    @component_class.new(field: @field, resource: @resource, file: file, extra_classes: "aspect-video")
   end
 end
