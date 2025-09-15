fe61618e5f28bc6c0cd8e24c3b1473651d8d5e10
Paul Bob <paul.ionut.bob@gmail.com>
2025-09-15 14:50:25 +0300

    fix: files field view_type

--- app/components/avo/fields/common/files/list_viewer_component.rb
+++ app/components/avo/fields/common/files/list_viewer_component.rb
@@ -6,10 +6,22 @@ class Avo::Fields::Common::Files::ListViewerComponent < Avo::BaseComponent
   prop :field
   prop :resource
 
+  def after_initialize
+    @available_view_types = [:list, :grid]
+
+    params_view_type = @resource.params.dig(:view_type)&.to_sym
+
+    @view_type = if params_view_type.in?(@available_view_types)
+      params_view_type
+    else
+      @field.view_type
+    end
+  end
+
   def classes
     base_classes = "py-4 rounded-2xl max-w-full"
 
-    view_type_classes = if view_type == :list
+    view_type_classes = if @view_type == :list
       "flex flex-col space-y-2"
     else
       "relative grid xs:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6"
@@ -22,16 +34,8 @@ class Avo::Fields::Common::Files::ListViewerComponent < Avo::BaseComponent
     (@field.stacked && !@field.hide_view_type_switcher) ? "-mt-9" : ""
   end
 
-  def available_view_types
-    [:list, :grid]
-  end
-
   def view_type_component(file)
-    component = "Avo::Fields::Common::Files::ViewType::#{view_type.to_s.capitalize}ItemComponent".constantize
+    component = "Avo::Fields::Common::Files::ViewType::#{@view_type.to_s.capitalize}ItemComponent".constantize
     component.new(field: @field, resource: @resource, file: file, extra_classes: "aspect-video")
-  end
-
-  def view_type
-    @view_type ||= (@resource.params.dig(:view_type) || @field.view_type).to_sym
   end
 end
