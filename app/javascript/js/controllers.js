686d8d8e9d5df957095bf555f1c07d5fdd3e4a1d
Adrian Marin <adrian@adrianthedev.com>
2025-12-11 14:44:09 +0200

    revert controllers js change

--- app/javascript/js/controllers.js
+++ app/javascript/js/controllers.js
@@ -38,7 +38,6 @@ import ResourceEditController from './controllers/resource_edit_controller'
 import ResourceIndexController from './controllers/resource_index_controller'
 import ResourceSearchController from './controllers/resource_search_controller'
 import ResourceShowController from './controllers/resource_show_controller'
-import SearchController from './controllers/search_controller'
 import SelectController from './controllers/select_controller'
 import SelectFilterController from './controllers/select_filter_controller'
 import SelfDestroyController from './controllers/self_destroy_controller'
@@ -53,6 +52,7 @@ import TiptapFieldController from './controllers/fields/tiptap_field_controller'
 import ToggleController from './controllers/toggle_controller'
 import TrixBodyController from './controllers/trix_body_controller'
 import TrixFieldController from './controllers/fields/trix_field_controller'
+import SearchController from './controllers/search_controller'
 
 application.register('action', ActionController)
 application.register('actions-overflow', ActionsOverflowController)
