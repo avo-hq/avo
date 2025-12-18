bde4d15dbaafd0d42bf1aaca166455931d6d00b2
Paul Bob <paul.ionut.bob@gmail.com>
2025-12-18 12:15:19 +0200

    refactor

--- app/components/avo/u_i/badge_component.rb
+++ app/components/avo/u_i/badge_component.rb
@@ -1,58 +1,46 @@
 # frozen_string_literal: true
 
 class Avo::UI::BadgeComponent < Avo::BaseComponent
-  STYLES = %w[solid subtle].freeze
+  unless defined?(VALID_STYLES)
+    VALID_STYLES = %w[solid subtle].freeze
+  end
+
+  unless defined?(VALID_COLORS)
+    VALID_COLORS = %w[
+      secondary
+      success
+      informative
+      warning
+      error
+      orange
+      yellow
+      green
+      teal
+      blue
+      purple
+    ].freeze
+  end
+
+  prop :color do |value|
+    value = value.to_s
+
+    VALID_COLORS.include?(value) ? value : "secondary"
+  end
+
+  prop :style do |value|
+    value = value.to_s
+
+    VALID_STYLES.include?(value) ? value : "subtle"
+  end
 
   prop :label, default: ""
-  prop :color, default: "secondary"
-  prop :style, default: "subtle"
   prop :icon
   prop :icon_only, default: false
   prop :classes, default: ""
 
   private
 
-  def normalize_color(value)
-    return "secondary" if value.blank?
-
-    normalized = Avo::UI::Colors.normalize(value)
-    Avo::UI::Colors.valid(normalized) ? normalized : "secondary"
-  end
-
-  def badge_classes
-    classes = ["badge"]
-
-    # Add custom classes if provided
-    classes << @classes if @classes.present?
-
-    style_value = normalize_style(@style).to_s
-    classes << "badge--#{style_value}" if style_value.present?
-
-    color_value = normalize_color(@color).to_s
-    classes << "badge--#{color_value}" if color_value.present?
-
-    # Icon-only modifier
-    classes << "badge--icon-only" if icon_only?
-
-    classes.compact.join(" ")
-  end
-
-  def normalize_style(value)
-    style_str = value.to_s
-    STYLES.include?(style_str) ? style_str : "subtle"
-  end
-
   def icon_only?
     @icon_only || (@icon.present? && @label.blank?)
-  end
-
-  def icon_classes
-    "badge__icon"
-  end
-
-  def render_icon
-    return unless @icon.present?
-
-    helpers.svg(@icon, class: icon_classes)
   end
 end
