1c81e74ce1ea4a61b1644f02748713f8c34c7f5d
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2025-12-22 16:16:37 +0200

    Revert "ui4: button component" (#4176)
    This reverts commit 1d795b87492124a17857a85d164deec82a49ea08.

--- spec/dummy/test/components/previews/button_component_preview.rb
+++ spec/dummy/test/components/previews/button_component_preview.rb
@@ -7,16 +7,9 @@ class ButtonComponentPreview < Lookbook::Preview
   # @param style select { choices: [primary, outline, text, icon] }
   # @param color select { choices: [gray, red, green, blue] }
   # @param icon text "Icon to display in the button"
-  def default(label: "Click me", size: :md, style: :primary, color: :primary, icon: "heroicons/outline/paper-clip")
-    render_with_template(
-      template: "button_component_preview/default",
-      locals: {
-        label: label,
-        size: size,
-        style: style,
-        color: color,
-        icon: icon
-      }
-    )
+  def standard(label: "Click me", size: :md, style: :primary, color: :primary, icon: "heroicons/outline/paper-clip")
+    a_button(size:, style:, color:, icon:) do
+      label
+    end
   end
 end
