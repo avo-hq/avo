934bef780a702f309ca0c390318a80875e0ca2e9
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2025-12-23 12:25:48 +0200

    fix to suport the new tabs component and tabswitcher

--- lib/avo/test_helpers.rb
+++ lib/avo/test_helpers.rb
@@ -143,14 +143,17 @@ module Avo
     end
 
     def click_tab(tab_name = "", within_target: nil, **args)
+      # Support both old TabSwitcherComponent (data-tabs-target) and new ScopeTabsComponent (role=tablist)
+      tab_container_selector = '[data-controller="tabs"] [data-tabs-target="tabSwitcher"], [data-controller="tabs"] [role="tablist"]'
+
       if within_target.present?
         within within_target do
-          within find('[data-controller="tabs"] [data-tabs-target="tabSwitcher"]') do
+          within find(tab_container_selector) do
             find_link(tab_name).trigger("click")
           end
         end
       else
-        within find('[data-controller="tabs"] [data-tabs-target="tabSwitcher"]') do
+        within find(tab_container_selector) do
           find_link(tab_name).trigger("click")
         end
       end
