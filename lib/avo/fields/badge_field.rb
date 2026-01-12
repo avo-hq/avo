513654aeaec2b8b24e23856af12e0b997c8eb8aa
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-01-12 23:12:38 +0200

    refactor the color method from badge_field

--- lib/avo/fields/badge_field.rb
+++ lib/avo/fields/badge_field.rb
@@ -17,9 +17,15 @@ module Avo
         @options.values.flatten.uniq
       end
 
+      # Maps field value to a color based on @options configuration
+      # Example: "Done" -> "success" if options = { success: [:done, :complete] }
       def color
-        # Maps field value to a color based on @options configuration
-        badge_color_for_value
+        return "neutral" if value.blank?
+
+        values = @options.find do |_, configured_values|
+          Array.wrap(configured_values).map { |v| v.to_s }.include?(value.to_s)
+        end
+        values&.first&.to_s || "neutral"
       end
 
       def style
@@ -28,17 +34,6 @@ module Avo
 
       def icon
         execute_context(@icon)
-      end
-
-      # Maps field value to a color based on @options configuration
-      # Example: "Done" -> "success" if options = { success: [:done, :complete] }
-      def badge_color_for_value
-        return "neutral" if value.blank?
-
-        values = @options.find do |_, configured_values|
-          Array.wrap(configured_values).map { |v| v.to_s }.include?(value.to_s)
-        end
-        values&.first&.to_s || "neutral"
       end
     end
   end
