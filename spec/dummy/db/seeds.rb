bf2023bac3453542fc32e7bf3b083b5fec00baa2
Adrian Marin <adrian@adrianthedev.com>
2026-01-22 14:07:58 +0200

    map view tweaks

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -284,6 +284,144 @@ end
     latitude: "22.3193",
     longitude: "114.1694",
     population: "7_500_000"
+  },
+  {
+    name: "Tokyo",
+    latitude: "35.6762",
+    longitude: "139.6503",
+    population: "13_900_000"
+  },
+  {
+    name: "London",
+    latitude: "51.5074",
+    longitude: "-0.1278",
+    population: "9_000_000"
+  },
+  {
+    name: "Paris",
+    latitude: "48.8566",
+    longitude: "2.3522",
+    population: "2_100_000"
+  },
+  {
+    name: "Berlin",
+    latitude: "52.5200",
+    longitude: "13.4050",
+    population: "3_700_000"
+  },
+  {
+    name: "Rome",
+    latitude: "41.9028",
+    longitude: "12.4964",
+    population: "2_800_000"
+  },
+  {
+    name: "Madrid",
+    latitude: "40.4168",
+    longitude: "-3.7038",
+    population: "3_200_000"
+  },
+  {
+    name: "Amsterdam",
+    latitude: "52.3676",
+    longitude: "4.9041",
+    population: "900_000"
+  },
+  {
+    name: "Sydney",
+    latitude: "-33.8688",
+    longitude: "151.2093",
+    population: "5_300_000"
+  },
+  {
+    name: "Melbourne",
+    latitude: "-37.8136",
+    longitude: "144.9631",
+    population: "5_000_000"
+  },
+  {
+    name: "Singapore",
+    latitude: "1.3521",
+    longitude: "103.8198",
+    population: "5_700_000"
+  },
+  {
+    name: "Dubai",
+    latitude: "25.2048",
+    longitude: "55.2708",
+    population: "3_400_000"
+  },
+  {
+    name: "Mumbai",
+    latitude: "19.0760",
+    longitude: "72.8777",
+    population: "20_400_000"
+  },
+  {
+    name: "São Paulo",
+    latitude: "-23.5505",
+    longitude: "-46.6333",
+    population: "12_300_000"
+  },
+  {
+    name: "Mexico City",
+    latitude: "19.4326",
+    longitude: "-99.1332",
+    population: "9_200_000"
+  },
+  {
+    name: "Los Angeles",
+    latitude: "34.0522",
+    longitude: "-118.2437",
+    population: "4_000_000"
+  },
+  {
+    name: "Chicago",
+    latitude: "41.8781",
+    longitude: "-87.6298",
+    population: "2_700_000"
+  },
+  {
+    name: "Toronto",
+    latitude: "43.6532",
+    longitude: "-79.3832",
+    population: "2_900_000"
+  },
+  {
+    name: "Vancouver",
+    latitude: "49.2827",
+    longitude: "-123.1207",
+    population: "675_000"
+  },
+  {
+    name: "Stockholm",
+    latitude: "59.3293",
+    longitude: "18.0686",
+    population: "975_000"
+  },
+  {
+    name: "Copenhagen",
+    latitude: "55.6761",
+    longitude: "12.5683",
+    population: "800_000"
+  },
+  {
+    name: "Vienna",
+    latitude: "48.2082",
+    longitude: "16.3738",
+    population: "1_900_000"
+  },
+  {
+    name: "Zurich",
+    latitude: "47.3769",
+    longitude: "8.5417",
+    population: "420_000"
+  },
+  {
+    name: "Seoul",
+    latitude: "37.5665",
+    longitude: "126.9780",
+    population: "9_700_000"
   }
 ].each do |city|
   City.create(**city)
