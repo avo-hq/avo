9256467f0304be9781d1caefeee292eb42a19ef1
Adrian Marin <adrian@adrianthedev.com>
2026-01-26 14:33:04 +0200

    ui4: button component

--- agents.md
+++ agents.md
@@ -23,5 +23,22 @@ Don't implement a component if the Figma MCP is not available and fails and aler
 
 - check the components and see if they use other components. If so, use the existing components.
 
-#### Files
 
+#### SVGs
+- use `svg` helper to render the SVGs.
+- use the `helpers.svg` helper to render the SVGs only in view components, never in lookbook previews.
+- use a string that is composed of `tabler/outline/{NAME_OF_ICON}` or `tabler/filled/{NAME_OF_ICON}` and the name of the icon and choose an existing icon from the library. if you can't think of one use `paperclip`, `info-circle`, or `external-link` for outgoing links.
+
+#### Tailwind CSS
+- when writing css try to use the `@apply` directive whenever you can and preserve the regular Tailwind syntax.
+- when writing media queries, try to land in Tailwind's regular breakpoints and write the media query as a Tailwind prefix `sm:text-sm` instead of `@media (min-width: 640px) { .text-sm { font-size: 1rem; } }`.
+- we work only with Tailwind CSS v4 and above. don't try to support anything below v4.
+
+#### Naming conventions
+- use header, sidebar, and body for the component areas.
+- use `title` instead of `name` when you need to reference the title of a component.
+
+#### Node.js
+- use `yarn`, not npm
+
+#### Files
--- app/components/avo/button_component.rb
+++ app/components/avo/button_component.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 # A button/link can have the following settings:
-# style: primary/outline/text/icon
-# size: :xs :sm, :md, :lg, :xl
-# color: :gray, :red, :green, :blue, or any other tailwind color
+# style: primary/outline/text
+# size: :sm, :md, :lg
+# color: :gray, :red, :green, :blue, or any other tailwind color (legacy support)
 # icon: "heroicons/outline/paperclip" as specified in the docs (https://docs.avohq.io/3.0/icons.html)
 class Avo::ButtonComponent < Avo::BaseComponent
-  # include Avo::ApplicationHelper
-
   prop :path, kind: :positional
   prop :size, default: :md
   prop :style, default: :outline
@@ -36,18 +34,15 @@ class Avo::ButtonComponent < Avo::BaseComponent
   end
 
   def button_classes
-    classes = "button-component inline-flex grow-0 items-center font-semibold leading-6 fill-current whitespace-nowrap transition duration-100 transform transition duration-100 cursor-pointer disabled:cursor-not-allowed disabled:opacity-60 data-[disabled='true']:opacity-60 justify-center #{@class}"
+    base_classes = [
+      "button-component",
+      "button-component--size-#{@size}",
+      "button-component--style-#{@style}",
+      @class
+    ]
+    base_classes << "button-component--loading" if @args[:loading]
 
-    # For non-icon-styled buttons we should not add borders.
-    classes += " border active:outline active:outline-1" if is_not_icon_style?
-
-    classes += " rounded-sm" if @rounded.present?
-    classes += style_classes
-    classes += horizontal_padding_classes
-    classes += vertical_padding_classes
-    classes += text_size_classes
-
-    classes
+    class_names(*base_classes.compact)
   end
 
   def is_link?
@@ -62,22 +57,6 @@ class Avo::ButtonComponent < Avo::BaseComponent
     !is_icon_style?
   end
 
-  def full_content
-    result = ""
-    # space out the icon from the text if text is present
-    # add the icon height
-    icon_classes = class_names(@icon_class, "pointer-events-none", icon_size_classes, "me-1": content.present? && is_not_icon_style?)
-
-    # Add the icon
-    result += helpers.svg(@icon, class: icon_classes) if @icon.present?
-
-    if is_not_icon_style? && content.present?
-      result += content
-    end
-
-    result.html_safe
-  end
-
   def call
     if is_link?
       output_link
@@ -88,104 +67,32 @@ class Avo::ButtonComponent < Avo::BaseComponent
 
   def output_link
     link_to @path, **args do
-      full_content
+      render_content
     end
   end
 
   def output_button
     if args.dig(:method).present? || args.dig(:data, :turbo_method).present?
       button_to args[:url], **args do
-        full_content
+        render_content
       end
     else
       button_tag(**args) do
-        full_content
+        render_content
       end
     end
   end
 
   private
 
-  def vertical_padding_classes
-    return " py-0" if is_icon_style?
-
-    case @size.to_sym
-    when :xs
-      " py-0"
-    when :sm
-      " py-1"
-    when :md
-      " py-1.5"
-    when :lg
-      " py-2"
-    when :xl
-      " py-3"
-    else
-      ""
-    end
-  end
-
-  def horizontal_padding_classes
-    return " px-0" if is_icon_style?
-    return " px-1" if @compact
-
-    case @size.to_sym
-    when :xs
-      " px-2"
-    when :sm
-      " px-3"
-    when :md
-      " px-3"
-    when :lg
-      " px-5"
-    when :xl
-      " px-6"
-    else
-      "px-4"
-    end
-  end
-
-  def text_size_classes
-    case @size.to_sym
-    when :xs
-      " text-xs"
-    else
-      " text-sm"
-    end
-  end
-
-  def style_classes
-    case @style
-    when :primary
-      " bg-#{@color}-500 text-white border-#{@color}-500 hover:bg-#{@color}-600 hover:border-#{@color}-600 active:border-#{@color}-600 active:outline-#{@color}-600 active:bg-#{@color}-600"
-    when :outline
-      " bg-white text-#{@color}-500 border-#{@color}-500 hover:bg-#{@color}-100 active:bg-#{@color}-100 active:border-#{@color}-500 active:outline-#{@color}-500"
-    when :text
-      " text-#{@color}-500 active:outline-#{@color}-500 hover:bg-gray-100 border-transparent"
-    when :icon
-      " text-#{@color}-600"
-    else
-      ""
-    end
-  end
-
-  def icon_size_classes
-    icon_classes = ""
-    return icon_classes if is_icon_style?
-
-    case @size
-    when :xs
-      icon_classes += " h-4 my-1"
-    when :sm
-      icon_classes += " h-4 my-1"
-    when :md
-      icon_classes += " h-4 my-1"
-    when :lg
-      icon_classes += " h-5 my-0.5"
-    when :xl
-      icon_classes += " h-6"
-    end
-
-    icon_classes
+  def render_content
+    # if is_icon_style?
+    #   helpers.svg(@icon, class: class_names("button-component__icon", @icon_class)) if @icon.present?
+    # else
+    # content_tag :span, class: "button-component__content" do
+    concat helpers.svg(@icon, class: class_names("button-component__icon", @icon_class)) if @icon.present?
+    concat content if content.present?
+    # end
+    # end
   end
 end
