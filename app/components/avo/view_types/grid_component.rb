673fd34ba7e008d72f79e6ace294e36c7a85ac48
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-02-02 18:51:00 +0200

    fix the item selector attributes

--- app/components/avo/view_types/grid_component.rb
+++ app/components/avo/view_types/grid_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Avo::ViewTypes::GridComponent < Avo::ViewTypes::BaseViewTypeComponent
+  include Avo::ResourcesHelper
+
   def grid_card_for(resource)
     Avo::ExecutionContext.new(target: resource.grid_view[:card], resource: resource, record: resource.record).handle
   end
