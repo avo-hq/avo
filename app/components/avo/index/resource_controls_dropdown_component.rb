58c44b11db251218146ee95a7d59558191237ad0
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-02-10 13:35:39 +0200

    fixes

--- app/components/avo/index/resource_controls_dropdown_component.rb
+++ app/components/avo/index/resource_controls_dropdown_component.rb
@@ -14,18 +14,8 @@ class Avo::Index::ResourceControlsDropdownComponent < Avo::Index::ResourceContro
   def render_edit_button(control)
     return unless can_edit?
 
-    link_options = {
-      class: "flex items-center gap-2",
-      aria: {label: control.title},
-      data: {
-        target: "control:edit",
-        control: :edit,
-        "resource-id": @resource.record_param,
-      }
-    }
-
-    link_to edit_path, **link_options do
-      concat svg("tabler/outline/edit", class: svg_classes)
+    link_to edit_path, **control_link_options(control, target: "edit", control_key: :edit) do
+      concat svg("tabler/outline/edit")
       concat control_title_without_resource(control.title)
     end
   end
@@ -33,44 +23,20 @@ class Avo::Index::ResourceControlsDropdownComponent < Avo::Index::ResourceContro
   def render_show_button(control)
     return unless can_view?
 
-    link_options = {
-      class: "flex items-center gap-2",
-      aria: {label: control.title},
-      data: {
-        target: "control:view",
-        control: :show,
-      }
-    }
-
-    link_to show_path, **link_options do
-      concat svg("tabler/outline/eye", class: svg_classes)
+    link_to show_path, **control_link_options(control, target: "view", control_key: :show, include_resource_id: false) do
+      concat svg("tabler/outline/eye")
       concat control_title_without_resource(control.title)
     end
   end
 
   def render_delete_button(control)
-    # If the resource is a related resource, we use the can_delete? policy method because it uses
-    # authorize_association_for(:destroy).
-    # Otherwise we use the can_see_the_destroy_button? policy method because it do no check for association
-    # only for authorize_action .
-    policy_method = is_a_related_resource? ? :can_delete? : :can_see_the_destroy_button?
-    return unless send policy_method
+    return unless destroy_permitted?
 
-    link_options = {
-      form_class: "flex flex-col sm:flex-row sm:inline-flex",
-      aria: {label: control.title},
-      data: {
-        turbo_frame: params[:turbo_frame],
-        turbo_confirm: control.confirmation_message,
-        turbo_method: :delete,
-        target: "control:destroy",
-        control: :destroy,
-        "resource-id": @resource.record_param,
-      }
-    }
-
-    link_to delete_path, **link_options do
-      concat svg("tabler/outline/trash", class: svg_classes)
+    link_to delete_path, **control_link_options(control, target: "destroy", control_key: :destroy,
+      turbo_frame: params[:turbo_frame],
+      turbo_confirm: control.confirmation_message,
+      turbo_method: :delete) do
+      concat svg("tabler/outline/trash")
       concat control_title_without_resource(control.title)
     end
   end
@@ -78,28 +44,37 @@ class Avo::Index::ResourceControlsDropdownComponent < Avo::Index::ResourceContro
   def render_detach_button(control)
     return unless can_detach?
 
-    link_options = {
-      title: control.title,
-      aria: {label: control.title},
-      data: {
-        turbo_method: :delete,
-        turbo_frame: params[:turbo_frame],
-        turbo_confirm: control.confirmation_message,
-        target: "control:detach",
-        control: :detach,
-        "resource-id": @resource.record_param,
-      }
-    }
-
-    svg = svg("tabler/outline/unlink", class: svg_classes)
-    link_to detach_path, **link_options do
-      concat svg
-      concat control.title
+    link_to detach_path, **control_link_options(control, target: "detach", control_key: :detach,
+      turbo_frame: params[:turbo_frame],
+      turbo_confirm: control.confirmation_message,
+      turbo_method: :delete) do
+      concat svg("tabler/outline/unlink")
+      concat control_title_without_resource(control.title)
     end
   end
 
+  def control_link_options(control, target:, control_key:, include_resource_id: true, **extra_data)
+    data = {
+      target: "control:#{target}",
+      control: control_key,
+    }
+    data["resource-id"] = @resource.record_param if include_resource_id
+    data.merge!(extra_data)
+
+    {
+      aria: {label: control.title},
+      title: control.title,
+      data: data,
+    }
+  end
+
+  def destroy_permitted?
+    # Related resources use can_delete? (authorize_association_for :destroy); index uses can_see_the_destroy_button?
+    policy_method = is_a_related_resource? ? :can_delete? : :can_see_the_destroy_button?
+    send(policy_method)
+  end
+
   def control_title_without_resource(title)
-    # Remove resource name in any case (Product, product, PRODUCT)
     resource_name = singular_resource_name.humanize
     title.gsub(/\b#{Regexp.escape(resource_name)}\b/i, "").strip
   end
