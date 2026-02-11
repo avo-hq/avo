08f856c555d9b02115d0596df7eb614e8ab39e02
Paul Bob <paul.ionut.bob@gmail.com>
2026-02-11 10:54:04 +0200

    refactor: enhance toggle controller to support multiple exemption containers
    Updated the filters_component to include a new data attribute for exemption containers. Modified the toggle_controller to utilize flatMap for improved selection of exemption container elements, allowing for better handling of multiple selectors.

--- app/javascript/js/controllers/toggle_controller.js
+++ app/javascript/js/controllers/toggle_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
   }
 
   get exemptionContainerTargets() {
-    return this.exemptionContainersValue.map((selector) => document.querySelector(selector)).filter(Boolean)
+    return this.exemptionContainersValue.flatMap((selector) => [...document.querySelectorAll(selector)])
   }
 
   connect() {
