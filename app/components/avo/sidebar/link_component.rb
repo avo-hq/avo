76d46229deb7ff9488ba911b4dd94e6c91ec39ef
Adrian Marin <adrian@adrianthedev.com>
2026-03-02 13:09:30 +0200

    active submenu indicator

--- app/components/avo/sidebar/link_component.rb
+++ app/components/avo/sidebar/link_component.rb
@@ -43,6 +43,33 @@ class Avo::Sidebar::LinkComponent < Avo::BaseComponent
   end
 
   def link_icon
-    @icon.presence || "tabler/outline/arrow-right"
+    if @label == "Users"
+      "tabler/outline/users"
+    elsif @label == "Projects"
+      "tabler/outline/users"
+    else
+      @icon.presence || ""
+    end
+  end
+
+  def active_item_index
+    return @active_item_index if defined?(@active_item_index)
+
+    @active_item_index = @items&.index do |item|
+      item.path.present? && helpers.is_active_link?(item.path, @active)
+    end
+  end
+
+  def subitem_bar_class(index)
+    active_idx = active_item_index
+    return "" if active_idx.nil?
+
+    if index == active_idx
+      "sidebar-subitem--bar-active"
+    elsif index < active_idx
+      "sidebar-subitem--bar-pass"
+    else
+      ""
+    end
   end
 end
