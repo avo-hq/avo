b2e09a31ccf158d02868bd8f99357e9a401d467d
Paul Bob <69730720+Paul-Bob@users.noreply.github.com>
2026-03-05 16:51:19 +0200

    add dropdown_menu_classes prop to DropdownComponent and DropdownMenuComponent (#4298)
    Introduced a new prop 'dropdown_menu_classes' to both DropdownComponent and DropdownMenuComponent for enhanced styling flexibility. Updated the corresponding HTML templates to utilize this new prop, allowing for dynamic class assignment in the dropdown menu.

--- app/components/avo/u_i/dropdown_menu_component.rb
+++ app/components/avo/u_i/dropdown_menu_component.rb
@@ -4,4 +4,5 @@ class Avo::UI::DropdownMenuComponent < Avo::BaseComponent
   prop :data, default: {}.freeze
   prop :classes
   prop :open, default: false
+  prop :dropdown_menu_classes, default: ""
 end
