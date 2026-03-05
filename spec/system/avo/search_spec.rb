09302000e992fe8cbefd5397dc3eccf321027a35
Paul Bob <paul.ionut.bob@gmail.com>
2026-03-05 14:56:23 +0200

    fix search

--- spec/system/avo/search_spec.rb
+++ spec/system/avo/search_spec.rb
@@ -2,26 +2,27 @@ require "rails_helper"
 
 RSpec.describe "Search", type: :system do
   let!(:course) { create :course, name: "Ruby on Rails" }
+  let!(:other_course) { create :course, name: "Advanced Elixir" }
 
   describe "resource search with multi-word query" do
     it "finds the record when searching with multiple words" do
       visit "admin/resources/courses"
 
-      click_resource_search_input
       write_in_search("Ruby on Rails")
-      select_first_result_in_search
 
-      expect(page).to have_current_path("/admin/resources/courses/#{course.to_param}")
+      expect(page).to have_text("Displaying 1 item")
+      expect(page).to have_selector("[data-component-name='avo/index/table_row_component'][data-resource-id='#{course.to_param}']")
+      expect(page).not_to have_selector("[data-component-name='avo/index/table_row_component'][data-resource-id='#{other_course.to_param}']")
     end
 
     it "finds the record when searching with a partial multi-word query" do
       visit "admin/resources/courses"
 
-      click_resource_search_input
       write_in_search("Ruby on")
-      select_first_result_in_search
 
-      expect(page).to have_current_path("/admin/resources/courses/#{course.to_param}")
+      expect(page).to have_text("Displaying 1 item")
+      expect(page).to have_selector("[data-component-name='avo/index/table_row_component'][data-resource-id='#{course.to_param}']")
+      expect(page).not_to have_selector("[data-component-name='avo/index/table_row_component'][data-resource-id='#{other_course.to_param}']")
     end
   end
 end
