7d54fea8326b3044306bfe20f240017f1a60a35e
Adrian Marin <adrian@adrianthedev.com>
2026-03-05 11:33:17 +0200

    refactor: panel header tweaks

--- app/javascript/js/controllers/fields/panel_refresh_controller.js
+++ app/javascript/js/controllers/fields/panel_refresh_controller.js
@@ -5,6 +5,10 @@ export default class extends Controller {
     const frame = this.context.scope.element.closest('turbo-frame')
     this.element.querySelector('svg').classList.add('animate-spin')
     if (frame) {
+      frame.classList.add('opacity-50')
+      frame.addEventListener('turbo:frame-load', () => {
+        frame.classList.remove('opacity-50')
+      }, { once: true })
       frame.reload()
     } else {
       console.error(
