eaa3fdacdfd050db6e5e9b21c717b62f4908eb95
Paul Bob <paul.ionut.bob@gmail.com>
2026-03-09 16:32:34 +0200

    update styles

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,11 +4,3 @@ if defined?(Pagy::I18n)
     Pagy::I18n.pathnames << Avo::Engine.root.join("lib", "generators", "avo", "templates", "locales", "pagy")
   end
 end
-
-arrow_left_svg = File.read(Avo::Icons.root.join("assets", "svgs", "tabler", "outline", "arrow-narrow-left.svg")).gsub(/\s+/, " ").strip
-arrow_right_svg = File.read(Avo::Icons.root.join("assets", "svgs", "tabler", "outline", "arrow-narrow-right.svg")).gsub(/\s+/, " ").strip
-
-Pagy::I18n::DATA.each_value do |(data, _)|
-  data["pagy.prev"] = arrow_left_svg
-  data["pagy.next"] = arrow_right_svg
-end
