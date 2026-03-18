485f05494744d25364e89a045ee5c9283d220b45
Paul Bob <paul.ionut.bob@gmail.com>
2026-03-18 20:44:57 +0200

    chore: integrate Mapbox GL and update styles
    - Added `mapbox-gl` dependency to `package.json` and updated `yarn.lock`.
    - Imported Mapbox GL CSS in `avo.base.css` for styling.
    - Updated JavaScript to utilize Mapkick with Mapbox GL.
    - Adjusted badge viewer component styles for better layout.
    - Implemented a new controller for handling CSP violation reports.
    - Configured routes and content security policy for CSP reporting.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "hey", to: "home#index"
+  post "csp-violation-report-endpoint", to: "csp_reports#create"
 
   resources :posts
 
