c2abb7baf09dfe6d6bf729348598754f560a5b06
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-03-26 21:55:26 +0200

    change icons from world to world-map and from outline/school into filled/school

--- spec/dummy/app/avo/resources/user.rb
+++ spec/dummy/app/avo/resources/user.rb
@@ -78,7 +78,7 @@ class Avo::Resources::User < Avo::BaseResource
   end
 
   def actions
-    action Avo::Actions::ToggleInactive, icon: "tabler/outline/world"
+    action Avo::Actions::ToggleInactive, icon: "tabler/outline/world-map"
     action Avo::Actions::ToggleAdmin
     divider label: "Other actions"
     action Avo::Actions::Sub::DummyAction
