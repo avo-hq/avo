9efb2212f47222d4d63ac0a1c84d325a938285c6
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-03-26 17:22:51 +0200

    fix tests

--- spec/system/avo/action_filter_spec.rb
+++ spec/system/avo/action_filter_spec.rb
@@ -7,17 +7,10 @@ RSpec.feature "Actions", type: :system do
     open_panel_action(action_name: "Dummy action")
     run_action
 
-    base_classes = ".max-w-lg.w-full.shadow-lg.px-4.py-3.rounded-sm.relative.border.text-white.pointer-events-auto"
-
-    error_classes = "#{base_classes}.bg-red-400.border-red-600"
-    success_classes = "#{base_classes}.bg-green-500.border-green-600"
-    warning_classes = "#{base_classes}.bg-orange-400.border-orange-600"
-    info_classes = "#{base_classes}.bg-blue-400.border-blue-600"
-
-    expect(page).to have_selector error_classes
-    expect(page).to have_selector success_classes
-    expect(page).to have_selector warning_classes
-    expect(page).to have_selector info_classes
+    expect(page).to have_selector ".alert.alert--danger"
+    expect(page).to have_selector ".alert.alert--success"
+    expect(page).to have_selector ".alert.alert--warning"
+    expect(page).to have_selector ".alert.alert--info"
 
     expect(page).to have_text "Success response ✌️"
     expect(page).to have_text "Warning response ✌️"
