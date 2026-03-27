e2903019a87204c7f9c09bbaed461824f3721824
Adrian Marin <adrian@adrianthedev.com>
2026-03-27 15:48:12 +0200

    wip

---
+++ app/components/avo/sidebar/heading_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Avo::Sidebar::HeadingComponent < Avo::BaseComponent
+  prop :title
+  prop :icon
+  prop :collapsable, default: false
+  prop :collapsed, default: false
+  prop :key
+end
