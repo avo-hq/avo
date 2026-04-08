ff96e50ede537120fba3530670fba52e519e8c5c
Mihai Munteanu <mihai_munteanu_90@yahoo.com>
2026-04-08 07:22:12 +0300

    clean the avo remove the avo-advance feature

--- spec/dummy/app/avo/scopes/project_done.rb
+++
@@ -1,6 +0,0 @@
-class Avo::Scopes::ProjectDone < Avo::Advanced::Scopes::BaseScope
-  self.name = "Done"
-  self.description = "Projects that are done"
-  self.scope = -> { query.where(stage: "done") }
-  self.visible = -> { true }
-end
--- spec/dummy/app/avo/scopes/project_on_hold.rb
+++
@@ -1,6 +0,0 @@
-class Avo::Scopes::ProjectOnHold < Avo::Advanced::Scopes::BaseScope
-  self.name = "On hold"
-  self.description = "Projects that are on hold"
-  self.scope = -> { query.where(stage: "on hold") }
-  self.visible = -> { true }
-end
