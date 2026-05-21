90aa02c1d6ad60038044db1758479c168c6dc4e6
Adrian Marin <adrian@adrianthedev.com>
2026-05-21 20:53:06 +0300

    fix test

--- lib/avo/test_helpers.rb
+++ lib/avo/test_helpers.rb
@@ -348,11 +348,13 @@ module Avo
     def click_row_action(record, control:)
       row = find("[data-record-id='#{record.to_param}']")
 
-      if row.has_css?(".grid-card__actions-row .button--bare", wait: 0)
-        row.find(".grid-card__actions-row .button--bare").click
+      if row.has_css?(".grid-card__actions-row button[popovertarget]", wait: 0)
+        row.find(".grid-card__actions-row button[popovertarget]").click
       end
 
-      find("a[data-control='#{control}'][data-resource-id='#{record.to_param}']").click
+      within(row) do
+        find("a[data-control='#{control}'][data-resource-id='#{record.to_param}']").click
+      end
     end
 
     private
