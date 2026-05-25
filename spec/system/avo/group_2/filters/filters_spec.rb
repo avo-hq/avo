ef7beda3400eb0c4287afe8526400ec603735494
Adrian Marin <adrian@adrianthedev.com>
2026-05-25 16:49:53 +0300

    tests

--- spec/system/avo/group_2/filters/filters_spec.rb
+++ spec/system/avo/group_2/filters/filters_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe "Filters", type: :system do
       expect(page).to have_unchecked_field "Unfeatured"
       expect(page).to have_text "Featured post"
       expect(page).to have_text "Unfeatured post"
-      expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+      expect(page).to have_css(".button--disabled", text: "Reset filters")
     end
 
     it "changes the query" do
@@ -87,7 +87,7 @@ RSpec.describe "Filters", type: :system do
       expect(page).to have_unchecked_field "Featured"
       expect(page).to have_unchecked_field "Unfeatured"
       expect(current_url).not_to include "encoded_filters="
-      expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+      expect(page).to have_css(".button--disabled", text: "Reset filters")
     end
   end
 
@@ -111,7 +111,7 @@ RSpec.describe "Filters", type: :system do
       expect(page).to have_checked_field "Has Members"
       expect(page).not_to have_text "Without Members"
       expect(page).to have_text "With Members"
-      expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+      expect(page).to have_css(".button--disabled", text: "Reset filters")
     end
 
     it "changes the query and reset" do
@@ -137,7 +137,7 @@ RSpec.describe "Filters", type: :system do
       expect(page).to have_checked_field "Has Members"
       expect(page).not_to have_text "Without Members"
       expect(page).to have_text "With Members"
-      expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+      expect(page).to have_css(".button--disabled", text: "Reset filters")
     end
   end
 
@@ -156,7 +156,7 @@ RSpec.describe "Filters", type: :system do
         expect(page).to have_select "published status", selected: "Published or unpublished", options: ["Published or unpublished", "Published", "Unpublished"]
         expect(page).to have_text "Published post"
         expect(page).to have_text "Unpublished post"
-        expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+        expect(page).to have_css(".button--disabled", text: "Reset filters")
       end
 
       it "changes the query" do
@@ -196,7 +196,7 @@ RSpec.describe "Filters", type: :system do
         expect(page).to have_text "Unpublished post"
         expect(page).to have_select "avo_filters_published_status", selected: "Published or unpublished", options: ["Published or unpublished", "Published", "Unpublished"]
         expect(current_url).not_to include "encoded_filters="
-        expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+        expect(page).to have_css(".button--disabled", text: "Reset filters")
 
         select "Unpublished", from: "avo_filters_published_status"
         wait_for_loaded
@@ -216,7 +216,7 @@ RSpec.describe "Filters", type: :system do
         expect(page).to have_select "avo_filters_published_status", selected: "Published or unpublished", options: ["Published or unpublished", "Published", "Unpublished"]
         expect(page).to have_text "Published post"
         expect(page).to have_text "Unpublished post"
-        expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+        expect(page).to have_css(".button--disabled", text: "Reset filters")
       end
     end
   end
@@ -235,7 +235,7 @@ RSpec.describe "Filters", type: :system do
 
         expect(page).to have_text "Status"
         expect(page).to have_select "avo_filters_status", selected: [], options: ["draft", "published", "archived"]
-        expect(page).to have_css(".filters-card__reset.filters-card__reset--disabled", text: "Reset filters")
+        expect(page).to have_css(".button--disabled", text: "Reset filters")
       end
 
       it "changes the query" do
