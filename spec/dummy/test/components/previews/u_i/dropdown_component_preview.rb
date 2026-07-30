2c8fdc6f803e2e003638c7e04614c1dbb2219780
Adrian Marin <adrian@adrianthedev.com>
2026-07-31 00:18:08 +0300

    Add footer slot to DropdownComponent (#4682)

--- spec/dummy/test/components/previews/u_i/dropdown_component_preview.rb
+++ spec/dummy/test/components/previews/u_i/dropdown_component_preview.rb
@@ -42,6 +42,11 @@ module UI
     def searchable
       render_with_template(template: "u_i/dropdown_component_preview/searchable")
     end
+
+    # Popover dropdown with a footer row pinned under the (filterable) list
+    def with_footer
+      render_with_template(template: "u_i/dropdown_component_preview/with_footer")
+    end
     # @!endgroup
   end
 end
