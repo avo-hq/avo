46d3edaff1709a265484fdaf456768779ae1d479
Adrian Marin <adrian@adrianthedev.com>
2026-09-22 22:30:04 +0300

    fix: build a working model class for each array resource (#4810)

--- spec/features/avo/array_resource_spec.rb
+++ spec/features/avo/array_resource_spec.rb
@@ -36,4 +36,50 @@ RSpec.feature "ArrayResource", type: :feature do
       expect(name).to eq User.first.name
     end
   end
+
+  describe "model class" do
+    # YARD, a development dependency, adds Module#class_name. Host apps don't have it, and it
+    # hides a missing class_name on the generated model class, so remove it for these examples.
+    around do |example|
+      yard_class_name = Module.instance_method(:class_name) if Module.method_defined?(:class_name)
+      Module.send(:remove_method, :class_name) if yard_class_name
+
+      example.run
+    ensure
+      Module.send(:define_method, :class_name, yard_class_name) if yard_class_name
+    end
+
+    it "returns the records from .all" do
+      expect(Avo::Resources::Movie.model_class.all.map(&:name)).to include "The Shawshank Redemption"
+    end
+
+    it "returns the resource class name" do
+      expect(Avo::Resources::Movie.model_class.class_name).to eq "Movie"
+    end
+
+    it "builds records that are instances of model_class" do
+      resource = Avo::Resources::Movie.new
+      records = resource.fetch_records
+
+      expect(records).to all(be_a(resource.model_class))
+      expect(records.first.class.class_name).to eq "Movie"
+      expect(records.first.class.all.map(&:id)).to eq records.map(&:id)
+    end
+
+    it "is not shared between array resources" do
+      Avo::Resources::Attendee.new.fetch_records
+
+      expect(Avo::Resources::Attendee.model_class).to eq User
+      expect(Avo::Resources::Movie.model_class).not_to eq User
+    end
+
+    it "renders the show page when the policy scope calls scope.all" do
+      # Pundit's default Scope#resolve is `scope.all`
+      allow_any_instance_of(Avo::Services::AuthorizationService).to receive(:apply_policy) { |_service, query| query.all }
+
+      visit avo.resources_movie_path(28)
+
+      expect(page).to have_text "La La Land"
+    end
+  end
 end
